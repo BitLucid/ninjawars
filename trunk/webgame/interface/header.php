07c4232f73ad8b35bdbf75b1e4555be4018f86bb
Roy <tchalvakspam@gmail.com>
2009-04-15 23:14:41 -0400

    All iframes: now using ob_start() output buffer.

--- trunk/webgame/interface/header.php
+++ trunk/webgame/interface/header.php
@@ -1,6 +1,6 @@
 <?php
 require_once(substr(__FILE__,0,(strpos(__FILE__, 'webgame/')))."webgame/lib/base.inc.php"); // *** Absolute path include of everything.
-
+ob_start();
 // General utility objects.
 $filter = new Filter(); // *** Creates the filters for later use.
 $sql = new DBAccess();
