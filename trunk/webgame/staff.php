1c0e82295077aba32db52b27ba69aa6d191b68ad
Roy <tchalvakspam@gmail.com>
2009-04-27 02:56:39 -0400

    Staff:Clarification of github repository and older developers.

--- trunk/webgame/staff.php
+++ trunk/webgame/staff.php
@@ -9,58 +9,67 @@ include "interface/header.php";
 // TODO: Add in mention of Kultcher, RobertoSuave, Chris (not sure what his pseudonym is).
 ?>
 
-<span class="brownHeading">Staff</span>
+<p class="title">Staff</p>
 
+<div>
 <p>
-The preferred method of contacting us with problems or bugs with the game is via the <a href="http://ninjawars.proboards19.com">Ninjawars Forum</a>
+The preferred method of contacting us with problems or bugs with the game is via the <a href="http://ninjawars.proboards19.com">Ninjawars Forum</a>.  
+For other issues, including confirmation problems, email us at: <a href="mailto:<?php echo SUPPORT_EMAIL;?>"><?php echo SUPPORT_EMAIL;?></a>
 </p>
-<p style="border-bottom:thin grey dashed;padding-bottom:2em">
-For other issues, including confirmation problems, email us at: <a href="mailto:ninjawarsTchalvak@gmail.com">ninjawarsTchalvak@gmail.com</a>
-</p>
+</div>
+
+<div class='developers'>
+<div class='subtitle'>Developers</div>
 
-<p>
-<p style="color:#cc3333;font-weight:bold">Developers
-<br /><br />
-
-<a href="mailto:ninjawarsTchalvak@gmail.com">Tchalvak / Roy Ronalds</a> - Programming Lead
-- <a href="http://www.myspace.com/toastersquid">on Myspace</a> - <a href="http://www.facebook.com/s.php?q=Roy+Ronalds&init=q">on Facebook</a> - on AIM: Tchalvak
-<img src="images/tchalvak.jpg">
+<div style="border:thin red dashed">
+<a href="mailto:ninjawarsTchalvak@gmail.com">Tchalvak / Roy Ronalds</a> - Programmer and Maintainer
+<span style="color:#cc3333;font-weight:bold">
+- <a href="http://www.myspace.com/toastersquid">on Myspace</a>
+- <a href="http://www.facebook.com/s.php?q=Roy+Ronalds&init=q">on Facebook</a> 
+- on AIM: Tchalvak
+</span>
 <p>Hailing from the icy steppes of upstate New York, Roy got his first computer at the ripe old age of 14, and has been addicted to computers ever since.  He's worked in IT fixing computers, and eventually decided that the web is the future, got in on the action, and hasn't looked back.  Since 2003, he's been working in php and webdesign, which he got started on from working on ninjawars.net. </p>
-
 <p>Roy is almost done with a BS degree in Biochemistry from SUNY Geneseo.  He says it was an accident.  Luckily he also took programming classes along the way, and liked them.</p>
-
 <p>When he's able to overcome his attachment to instantaneous communication and exchange of ideas (i.e. the internet), he truly enjoys swimming and walking dogs, even if they are someone else's dogs.</p>
+<img src="images/tchalvak.jpg">
+</div>
+<br />
+<div style="border:thin red dashed">
+<span style="color:white">Beagle / Al Vazquez</span> - Previously Programming Lead and Current Server Administrator
+</div>
 
-
-<br /><br /><br /><br />
-<!--<a href="mailto:">--><span style="color:white">Beagle / Al Vazquez</span><!--</a>--> - Previous Programming Lead and Current Server Administrator
-</p><!-- End of the red text -->
+</div><!-- End of the Developers section -->
 <hr />
-<b>Credits</b>
-<br /><br />
-
-NinjaLord / John Facey, II - Creator of NW<br />
-<a href="interview.php">Interview with John on www.bbgdev.com</a><br /><br />
-
-Evolym Fragile/Davinel - Flash Banner
-
-<br /><br />
-
-Magatsu - Shop Graphics
-
-<br /><br />
-
-Alegion - Koi photograph Background - <a href='http://alegion.deviantart.com/'>alegion.deviantart.com/</a>
-
-<hr />
+<div class='other-credits'>
+<div class='subtitle'>Other Credits</div>
 <p>
-Want to join the Ninjawars team? Email <a href="mailto:ninjawarsTchalvak@gmail.com">ninjawarsTchalvak@gmail.com</a>,
-or you can contribute to the source code directly at: 
-<a target='_blank' href="http://github.com/tchalvak/ninjawars/tree/master">http://github.com/tchalvak/ninjawars/tree/master</a> .
+NinjaLord / John Facey, II - Founder & Original Developer of NinjaWars - <a href="interview.php">Interview with John by www.bbgdev.com</a>
 </p>
 <p>
-
+Other developers & planners at various points:
+chrismonster, kultcher, sparky, & suavisimo
 </p>
+<p>
+Evolym Fragile/Davinel - the Flash Banner
+<p>
+Magatsu - the Shop Graphics
+</p>
+Alegion - the Koi photograph Background - <a href='http://alegion.deviantart.com/'>alegion.deviantart.com/</a>
+</p>
+</div>
+
+<hr />
+<div>
+<div class='subtitle'>Contributing to ninjawars</div>
+<p>
+Ninjawars is open source, with the source code downloadable and able to be openly contributed back to at:
+<p>
+<a target='_blank' href="http://github.com/tchalvak/ninjawars/tree/master">http://github.com/tchalvak/ninjawars/tree/master</a>
+</p>
+<p>Want to get involved?  You can hack away at the code on github and suggest changes or ask any questions on the forum.</p>
+</div>
+<div class='license'>
+<div class='subtitle'>License</div>
 <p>
 <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/us/">
 <img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/3.0/us/88x31.png" />
@@ -73,7 +82,7 @@ or you can contribute to the source code directly at:
   <a xmlns:cc="http://creativecommons.org/ns#" href="http://ninjawars.net/webgame/staff.php" rel="cc:morePermissions">http://ninjawars.net/webgame/staff.php</a>.
 
 </p>
-
+</div>
 <?php
 include "interface/footer.php";
 ?>
