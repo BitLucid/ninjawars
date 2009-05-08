44a7a4c29d8ace6ec83f7d90fac93d8240cf39db
Roy <tchalvakspam@gmail.com>
2009-05-08 14:44:20 -0400

    Chat: uses render functions instead, now.

---
+++ trunk/webgame/lib/specific/lib_chat.php
@@ -0,0 +1,78 @@
+<?php
+/*
+ * Chat helper functions.
+ *
+ * @package messages
+ * @subpackage chat
+ */
+
+// Functions for rendering chat widgets
+
+// Just return a div with an active member count.
+function render_active_members($sql){
+    $members = $sql->QueryItem("select count(*) from ppl_online where member = true AND activity > (now() - CAST('30 minutes' AS interval))");
+    $membersTotal = $sql->QueryItem("select count(*) from ppl_online where member = true");
+    return
+        "<div class='active-members-count'>
+            Active Members:  ".($members?$members : '0')." / ".($membersTotal?$membersTotal : '0')."
+        </div>";
+}
+
+/**
+ * Render the div full of chat messages.
+ * @param $chatlength Essentially the limit on the number of messages.
+**/
+function render_chat_messages($sql, $chatlength, $show_elipsis=null){
+    // Eventually there might be a reason to abstract out get_chats();
+    $sql->Query("SELECT send_from, message FROM chat ORDER BY id DESC LIMIT $chatlength");// Pull messages
+    $chats = $sql->fetchAll();
+    $message_rows = '';
+    $messageCount = $sql->QueryItem("select count(*) from chat");
+    if (!isset($show_elipsis) && $messageCount>$chatlength){
+	$show_elipsis = true;
+    }
+    $res = "<div id='chatMessages' style='float:;margin-top:0em'>";
+    foreach($chats AS $messageData) {
+	// *** PROBABLY ALSO A SPEED PROBLEM AREA.
+	$message_rows .= "[<a href='player.php?player={$messageData['send_from']}'
+	     target='main'>{$messageData['send_from']}</a>]: {$messageData['message']}<br />\n";
+    }
+    $res .= $message_rows;
+    if ($show_elipsis){ // to indicate there are more chats available
+	$res .= ".<br />.<br />.<br />";
+    }
+    $res .= "</div>";
+    return $res;
+}
+
+// Render the "refresh chat periodically" js.
+function render_chat_refresh(){
+    ob_start();
+    ?>
+
+    <script type="text/javascript">
+    function refreshpage()
+    {
+      parent.mini_chat.location="mini_chat.php";
+    }
+    setInterval("refreshpage()",300*1000);
+    </script>
+    <?php
+    $res = ob_get_contents();
+    ob_end_clean();
+    return $res;
+}
+
+/**
+ * Display the chat input form.
+**/
+function render_chat_input(){
+    return
+    "<form id=\"post_msg\" action=\"mini_chat.php\" method=\"post\" name=\"post_msg\" style=\"margin-top:0em;margin-bottom:0.5em\">\n
+    <input id=\"message\" type=\"text\" size=\"20\" maxlength=\"490\" name=\"message\" class=\"textField\" />\n
+    <input id=\"command\" type=\"hidden\" value=\"postnow\" name=\"command\" />
+    <input type=\"submit\" value=\"Send\" class=\"formButton\" />\n
+    </form>\n";
+}
+
+?>
--- trunk/webgame/mini_chat.php
+++ trunk/webgame/mini_chat.php
@@ -1,81 +1,44 @@
 <?php
+require_once(substr(__FILE__, 0, (strpos(__FILE__, 'webgame/')))."webgame/resources/resources.php");
+require_once(LIB_ROOT."specific/lib_chat.php"); // Require all the chat helper and rendering functions.
+
 $private    = false;
 $alive      = false;
 $page_title = "Mini Chat";
 $quickstat  = false;
 
 include "interface/header.php";
-?>
 
-<script type="text/javascript">
-function refreshpage()
-{
-  parent.mini_chat.location="mini_chat.php";
-}
-setInterval("refreshpage()",300*1000);
-</script>
 
-<?php
 $default_limit = 22;
 $chatlength = in('chatlength', $default_limit, 'toInt');
 $filteredMessage = in('message', null, 'forChat'); // *** Sanitize any message
 $command = in('command');
 $sentMessage = in('message');
 $sent = false;
+$username = get_username();
+$input_form = ($username? render_chat_input() : '');
+
+// Take in a chat and record it to the database.
+if ($username) {
+	if ($command == "postnow" && $filteredMessage) {
+	  sendChat($username,'ChatMsg',$filteredMessage); // ChatMsg is deprecated.
+	}
+}
+
+
+echo render_chat_refresh(); // Write out the js to refresh.
 
 echo "<div id='mini-chat'>";
-if (is_logged_in()) {
-	if ($command == "postnow" && $filteredMessage) {
-	  sendChat($username,'ChatMsg',$filteredMessage);
-	  $sent = true;
-	  $command = "";
-	  $sentMessage = "";
-	}
-	echo "<form id=\"post_msg\" action=\"mini_chat.php\" method=\"post\" name=\"post_msg\" style=\"margin-top:0em;margin-bottom:0.5em\">\n";
-	echo "<input id=\"message\" type=\"text\" size=\"20\" maxlength=\"490\" name=\"message\" class=\"textField\" />\n";
-	echo "<input id=\"command\" type=\"hidden\" value=\"postnow\" name=\"command\" />";
-	echo "<input type=\"submit\" value=\"Send\" class=\"formButton\" />\n";
-	if($sent){
-	    echo "<span style=font-size:85%>Sent.</span></br />\n";
-	}
-	echo "</form>\n";// *** Makes the "sent" notice appear within the form.
-} else {
-    // *** Not logged in.
-	echo "<p class='notice'>Not currently logged in to chat.</p>";
-}
 
-$show_elipsis = false;
-$messageCount = $sql->QueryItem("select count(*) from chat");
-if ($messageCount>$chatlength){
-	$show_elipsis = true;
-}
+echo $input_form;
 
-$members = $sql->QueryItem("select count(*) from ppl_online where member = true AND activity > (now() - CAST('30 minutes' AS interval))");
-$membersTotal = $sql->QueryItem("select count(*) from ppl_online where member = true");
+echo render_active_members($sql);
 
-echo "<div style='border-top:thin dotted teal;'>";
-echo "Active Members:  ".($members?$members : '0')." / ".($membersTotal?$membersTotal : '0')." \n";
-echo "</div>";
+echo render_chat_messages($sql, $chatlength);
 
-echo "<div id='chatMessages' style='float:;margin-top:0em'>";
-$sql->Query("SELECT send_from, message FROM chat ORDER BY id DESC LIMIT $chatlength");// Pull messages
-/*
-* SPEED PROBLEM AREA.
-* This query could probably use further limitations to speed up the chat page load time.
-*/
-$chats = $sql->fetchAll();
-foreach($chats AS $messageData) {
-	// *** PROBABLY ALSO A SPEED PROBLEM AREA.
-	$from = $messageData['send_from'];
-	// *** Since the message only gets displayed once, just echo it directly.
-	echo "[<a href=\"player.php?player=$from\" target=\"main\">$from</a>]: ".$messageData['message']."<br />\n";
-}
-if ($show_elipsis){ // to indicate there are more chats available
-	echo ".<br />.<br />.<br />";
-}
-/*if ($chatlength != 360)
-{
-	echo "<br /><a href=\"mini_chat.php?chatlength=360\" style=font-size:90%>View remaining messages.</a><br />\n";
-}*/
-echo "</div></div>";
+echo "</div>"; // End of mini_chat div.
+
+include "interface/footer.php";
+
 ?>
