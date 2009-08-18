0b15b2df65727fca16ae585f39192920bc5867db
Roy <roy.ronalds@gmail.com>
2009-08-18 09:40:47 -0400

    Deity: Level plays a part in healing.

--- deploy/cron/deity_halfhour.php
+++ deploy/cron/deity_halfhour.php
@@ -4,7 +4,6 @@ require_once(LIB_ROOT."specific/lib_deity.php"); // Deity-specific functions
 
 $regen_rate           = 1;
 $turn_regen_threshold = 100;
-$maximum_turns        = 300;
 $maximum_heal         = 150;
 $maxtime              = '6 hours'; // *** Max time a person is kept online without being active.
 $score                = get_score_formula();
@@ -16,7 +15,6 @@ chat_timer();
 
 $sql->Update("UPDATE players SET turns = 0 WHERE turns < 0"); // if anyone has less than 0 turns, set it to 0
 $sql->Update("UPDATE players SET turns = turns+$regen_rate WHERE turns < ".$turn_regen_threshold);  // add turns at the regen rate for anyone below the threshold
-$sql->Update("UPDATE players SET turns = ".$maximum_turns." WHERE turns > ".$maximum_turns); // removes turns if anyone is over the max
 $sql->Update("UPDATE players SET bounty = 0 WHERE bounty < 0"); // if anyone has negative bounty, set it to 0
 
 $sql->Query("DELETE FROM ppl_online WHERE activity < (now() - interval '".$maxtime."')");
@@ -25,8 +23,9 @@ $out_display['Inactive Browsers Deactivated'] = $sql->a_rows;
 
 // *** HEAL CODE ***
 
-$sql->Update("UPDATE players SET health = numeric_smaller(health+8, $maximum_heal)
+$sql->Update("UPDATE players SET health = numeric_smaller(health+8+cast(floor(level/10) AS int), $maximum_heal)
 	     WHERE health BETWEEN 1 AND $maximum_heal AND NOT cast(status&".POISON." AS bool)");
+// Higher levels now heal faster.
 
 $logMessage = "DEITY_HALFHOUR STARTING: ".date(DATE_RFC1036)."\n";
 
