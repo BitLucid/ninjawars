7fd409d8953187da97dfed280deefbd9403310b9
Roy <roy.ronalds@gmail.com>
2009-08-31 12:47:10 -0400

    Resources: updated my sample resources.

--- docs/tchalvakSAMPLEresources.php
+++ docs/tchalvakSAMPLEresources.php
@@ -46,3 +46,30 @@ define('COMBAT_ROOT', SERVER_ROOT.'combat/');  // *** For all the in-game combat
 define('SKILL_ROOT', SERVER_ROOT.'skill/');  // *** For all the in-game skill objects.
 define('ITEM_ROOT', SERVER_ROOT.'item/');  // *** For all the in-game item/inventory objects.
 ?>
+<?php
+/*
+* resources.php template. This file must be included whenever a script is run. It defines constants used throughout the application
+*/
+define('DATABASE_HOST', "localhost");		// *** The host to connect to for the database
+define('DATABASE_USER', "tchalvak");		// *** The user that should connect to the database
+define('DATABASE_NAME', "ninjawars");		// *** The name of the database to connect to
+define('OFFLINE', false);				// *** Controls if remote or local resources are used
+define('DEBUG', true);					// *** Shorter debugging constant name, set as false on live.
+define('DEBUG_ALL_ERRORS', true);	// *** Only will turn on if debug is also on.
+define('SERVER_ROOT', "/home/tchalvak/ninjawars/deploy/");		// *** The root deployment directory of the game
+define('WEB_ROOT', "http://localhost/tchalvak/ninjawars/deploy/www/");			// *** The base URL used to access the game
+define('ADMIN_EMAIL', "ninjawarsTchalvak@gmail.com");		// *** For logs/emailed errors.
+define('SUPPORT_EMAIL', "ninjawarsTchalvak@gmail.com");	// *** For public questions.
+define('SUPPORT_EMAIL_FORMAL_NAME', "Ninjawars Tchalvak");
+define('SYSTEM_MESSENGER_EMAIL', "noreply@ninjawars.net");
+define('SYSTEM_MESSENGER_NAME', "Automated Ninjawars Messenger");
+
+
+// For location-specific derived-or-not constants.
+define('COMPILED_TEMPLATE_PATH', SERVER_ROOT.'templates/compiled/'); // *** This folder must have write permissions.
+define('LOGS', SERVER_ROOT.'cron/'); // *** For all custom logging
+define('CONNECTION_STRING', 'pgsql:dbname='.DATABASE_NAME.';user='.DATABASE_USER);
+
+// Seperate, tracked file for derived constants, that changes as they change.
+require(SERVER_ROOT."derived_constants.php");
+?>
