9246d17191a96856ea88872e4a36c3fe107d7fb7
Al Vazquez <beagle@localhost.(none)>
2009-09-11 14:25:52 -0400

    Almost completed the clan table database migration

--- scripts/dbScripts/clan_table_creation.sql
+++ scripts/dbScripts/clan_table_creation.sql
@@ -5,3 +5,6 @@ insert into clan (clan_name, _creator_player_id) select clan_long_name, player_i
 create table clan_player (_clan_id int not null references clan(clan_id) on update cascade on delete cascade, _player_id int not null references players(player_id) on update cascade on delete cascade, member_level int not null default 0);
 
 insert into clan_player (_clan_id, _player_id, member_level) select clan_id, member.player_id AS member_id, CASE WHEN leader.player_id = member.player_id THEN 1 ELSE 0 END AS leader_id FROM clan JOIN players leader ON _creator_player_id = leader.player_id JOIN players member ON member.clan = leader.uname WHERE member.confirmed = 1 order by clan_id;
+
+alter table players drop column clan;
+alter table players drop column clan_long_name;
