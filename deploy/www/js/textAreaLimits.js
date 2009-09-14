265ea68f51e1dda92e7d4c0ce06438bc7fdbbf46
Roy <tchalvakspam@gmail.com>
2009-09-14 19:47:14 -0400

    Stats: Tightened up charSuggest js.

--- deploy/www/js/textAreaLimits.js
+++ deploy/www/js/textAreaLimits.js
@@ -1,17 +1,15 @@
 $(document).ready(function() {
-    $(function(){
-        $('#player-profile-area').keyup(function(){
-            textAreaLimits('player-profile-area', 500, 'characters-left');
-        })
-    });
+    charSuggest('player-profile-area', 500, 'characters-left');
 });
 
+function charSuggest(textareaid, limit, infoid){
+    $('#'+textareaid).keyup(function(){
+        var textArea = $('#'+textareaid);
+        var text = textArea.val();
+        var textlength = text.length;
+        var info = $('#'+infoid);
+        var newText = limit+" character limit, "+textlength+" characters used.";
+        info.text(newText);
+    });
 
-function textAreaLimits(textareaid, limit, infoid){
-    var textArea = $('#'+textareaid);
-    var text = textArea.val();
-    var textlength = text.length;
-    var info = $('#'+infoid);
-    var newText = limit+" character limit, "+textlength+" characters used.";
-    info.text(newText);
 }
