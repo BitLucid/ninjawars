9c1165143b9df5bebdd1ef2ee5ea5791962a7a29
Roy <tchalvakspam@gmail.com>
2009-09-22 00:38:06 -0400

    Messages: js confirm added to delete link.

--- deploy/www/js/messageDeleteConfirm.js
+++ deploy/www/js/messageDeleteConfirm.js
@@ -1,16 +1,6 @@
-$('#delete-messages').click(function(){
-    $(this).css({'color':'red'});
-  var answer = confirm('Delete all messages?');
-  return answer // answer is a boolean
-}); 
-
-    $(document).ready(function(){
-        $('#delete-message').css({'color':'red','background-color':'teal'});
-    });
-/*
-    $('#delete-messages a').click(function () {
-        var answer = confirm("Delete all messsages?");
-        alert('was here'+answer);
-        return answer;
-    });
-    */
+$(document).ready(function (){
+    $('#delete-messages a').click(function(){
+      var answer = confirm('Delete all messages?');
+      return answer // answer is a boolean
+    }).hover(function () {$(this).css({'background-color':'red'})}, function () {$(this).css({'background-color':'inherit'})}); 
+});
