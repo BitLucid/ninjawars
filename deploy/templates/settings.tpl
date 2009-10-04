7c598ed78fd80e59613f1f3b59fa18ba029f97c0
Roy <tchalvakspam@gmail.com>
2009-10-03 22:57:51 -0400

    Settings: Adding a settings-switching page for later.

---
+++ deploy/templates/settings.tpl
@@ -0,0 +1,3 @@
+<h2>Settings</h2>
+
+No settings yet.
---
+++ deploy/www/settings.php
@@ -0,0 +1,17 @@
+<?php
+$alive      = true;
+$private    = true;
+$quickstat  = "player";
+$page_title = "Settings";
+
+include SERVER_ROOT."interface/header.php";
+
+$settings = get_settings();
+//var_dump($settings);
+
+$parts = get_certain_vars(get_defined_vars(), array());
+echo render_template('settings.tpl', $parts);
+
+include SERVER_ROOT."interface/footer.php";
+
+?>
