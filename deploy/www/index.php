10d9037fec54ddab6d18a4bf8a02eca38c417ecc
Roy <roy.ronalds@gmail.com>
2009-11-25 10:13:13 -0500

    Index: level 1 sees tutorial, levels 2-4 see villagers, after sees ninja list.

--- deploy/www/index.php
+++ deploy/www/index.php
@@ -27,6 +27,7 @@ if ($logout) { // on logout, kill the session and don't redirect.
 	    exit(); // Login redirect to prevent the refresh postback problem.
 	}
 }
+$is_not_logged_in = !$is_logged_in;
 $username = get_username();
 $user_id = get_user_id();
 
@@ -40,7 +41,6 @@ $header = render_html_for_header('Live By the Sword', 'main-body', $is_index=tru
 // render_html_for_header Writes out the html,head,meta,title,css,js.
 
 $version = 'NW Version 1.7.1 2009.11.22';
-$is_not_logged_in = !$is_logged_in;
 
 
 // Display main iframe page unless logged in.
@@ -49,6 +49,8 @@ if ($is_logged_in) {
     $level = getLevel($username);
     $main_src = 'list_all_players.php';
     if($level==1){
+    	$main_src = 'tutorial.php';
+    } elseif ($level<6){
     	$main_src = 'attack_player.php';
     }
 }
@@ -61,6 +63,5 @@ if(!$is_logged_in){
 }
 
 echo render_footer(null, true); // Skip quickstats.
-// Um, need to render footer here.
 
 ?>
