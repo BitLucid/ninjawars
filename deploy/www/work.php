ab4cf5848574cd0355a5388216a7fbb3e17452a4
Roy <tchalvakspam@gmail.com>
2009-12-17 12:08:51 -0500

    Work: Added google ad for testing.

--- deploy/www/work.php
+++ deploy/www/work.php
@@ -64,6 +64,22 @@ echo "<input id=\"workButton\" type=\"submit\" value=\"Turns\" name=\"workButton
 echo "</div>\n";
 echo "</form>\n";
 echo "</p>";
+?>
+<!-- Google Ad -->
+<script type="text/javascript"><!--
+google_ad_client = "pub-9488510237149880";
+/* 300x250, created 12/17/09 */
+google_ad_slot = "9563671390";
+google_ad_width = 300;
+google_ad_height = 250;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+
+<?php
+
 
 include SERVER_ROOT."interface/footer.php";
 ?>
