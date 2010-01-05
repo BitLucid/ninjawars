43fb233351cd4b4ce01834a47bef4227e2f90cb9
Roy <tchalvakspam@gmail.com>
2010-01-05 17:17:16 -0500

    Commands: Annoying clan_function bugs.

--- deploy/lib/common/commands.php
+++ deploy/lib/common/commands.php
@@ -731,7 +731,7 @@ function setClanLongName($who,$clan_long_name)
 function getClan($p_playerID) {
 	global $sql,$status_array, $player_id;
 
-	$clan = $sql->QueryItem("SELECT _clan_id FROM clan_player WHERE _player_id = $p_playerID");
+	$clan = $sql->QueryItem("SELECT _clan_id FROM clan_player WHERE _player_id = '".sql($p_playerID)."'");
 
 	if ($p_playerID == $player_id) {
 		$_SESSION['clan'] = $clan;
@@ -761,7 +761,9 @@ function getClanLeader($p_clanID)
 function getClanLongName($p_playerID) {
 	global $sql, $status_array;
 
-	$clan_long_name = $sql->QueryItem("SELECT clan_name FROM clan JOIN clan_player WHERE _player_id = $p_playerID");
+	$clan_long_name = $sql->QueryItem(
+	    "SELECT clan_name FROM clan JOIN clan_player on clan_id = _clan_id 
+	        WHERE _player_id = '".sql($p_playerID)."'");
 
 	if (getStatus(getPlayerName($p_playerID))){
 		return $clan_long_name;
