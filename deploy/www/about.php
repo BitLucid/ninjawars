81b8cf8f3b722754c6bffae3dc01e34175c9ded8
Roy <tchalvakspam@gmail.com>
2010-01-26 14:30:21 -0500

    About: Uses idiomatic page_render.

--- deploy/www/about.php
+++ deploy/www/about.php
@@ -1,14 +1,9 @@
 <?php
-$alive      = false;
-$private    = false;
-$quickstat  = false;
-$page_title = "About NinjaWars";
 
-include SERVER_ROOT."interface/header.php";
+init(); // Initializes a full environment.
 
 $progression = render_template('progression.tpl', array('WEB_ROOT'=>WEB_ROOT, 'IMAGE_ROOT'=>IMAGE_ROOT));
 
-echo render_template('about.tpl', array($progression));
+echo render_page('about.tpl', 'About NinjaWars', array($progression), $options=array('quickstat'=>false, 'private'=>false, 'alive'=>false)); 
 
-echo render_footer();
 ?>
