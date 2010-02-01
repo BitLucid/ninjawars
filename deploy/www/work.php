29d73d1793a879fff03fd001b15e48950253560a
Roy <tchalvakspam@gmail.com>
2010-02-01 00:59:34 -0500

    Work page: Uses render_page now.

--- deploy/www/work.php
+++ deploy/www/work.php
@@ -4,79 +4,35 @@ $alive      = true;
 $quickstat  = "player";
 $page_title = "Working in the Village";
 
-include SERVER_ROOT."interface/header.php";
-?>
+init();
 
-<h1>Working in the Village</h1>
-
-<?php
 $work_multiplier = 30;
-
-$description2 = "<div class=\"description\">\n".
-                "<div style='margin-bottom: 10px;'>On your way back from the fields, you pass by a few young children ".
-	          "chasing grasshoppers in the tall grass.</div>\n".
-                "<div>The foreman hands you a small pouch of gold as he says ".
-                "\"Care to put a little more work in? I'll keep paying you.\"</div>\n".
-                "</div>\n";
-
-$description1 = "<div class=\"description\">\n".
-	                "<div style='margin-bottom: 10px;'>On your way to the foreman's office, you pass by several workers ".
-                "drenched in sweat from working in the heat all day.</div>\n".
-                "<div>The foreman barely looks up at you as he busies himself with paperwork ".
-                "and a cigarette. \"So, how much work can we expect from you?\"</div>\n".
-                "</div>\n";
-
-$description = $description1;
+$worked = null;
+$new_gold = null;
+$not_enough_energy = null;
+$use_second_description = null;
 
 $worked = intval(in('worked'));
 
-if ($worked > 0)
-{
+if ($worked > 0){
 	$turns = getTurns($username);
 	$gold  = getGold($username);
 
-	if ($worked > $turns)
-	{
-		$description .= "<div>You have chosen to do more work than turns you have.</div>\n";
-	}
-	else
-	{
+	if ($worked > $turns){
+	    $not_enough_energy = true;
+	} else {
 		$new_gold  = $worked * $work_multiplier;   // *** calc amount worked ***
 
 		$gold  = addGold($username, $new_gold);
 		$turns = subtractTurns($username, $worked);
-
-		$description = $description2."<div style='margin-bottom: 10px;'>You have worked for $worked turns and earned $new_gold gold.</div>\n";
+		
+		$use_second_description = true;
 	}
 }
 
-echo $description;
-echo "<p>";
-echo "<div>You can earn money by working in the Village</div>\n";
-echo "<div>Village work will exchange turns for gold.</div>\n";
-echo "<div>The current work exchange rate: 1 Turn = ".$work_multiplier." Gold.</div>\n";
-echo "<div>Work in the Village?</div>\n";
-echo "<form id=\"work\" action=\"work.php\" method=\"post\" name=\"work\">\n";
-echo "<div>\n";
-echo "<input id=\"worked\" type=\"text\" size=\"3\" maxlength=\"3\" name=\"worked\" class=\"textField\">\n";
-echo "<input id=\"workButton\" type=\"submit\" value=\"Turns\" name=\"workButton\" class=\"formButton\">\n";
-echo "</div>\n";
-echo "</form>\n";
-echo "</p>";
-?>
-<!-- Google Ad -->
-<script type="text/javascript"><!--
-google_ad_client = "pub-9488510237149880";
-/* 300x250, created 12/17/09 */
-google_ad_slot = "9563671390";
-google_ad_width = 300;
-google_ad_height = 250;
-//-->
-</script>
-<script type="text/javascript"
-src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-</script>
-
-<?php
-include SERVER_ROOT."interface/footer.php";
+echo render_page('work.tpl', 
+        'Working in the Village', 
+        get_certain_vars(get_defined_vars(), array()), 
+        $options=array('quickstat'=>'player', 'private'=>false, 'alive'=>true)); 
+        
 ?>
