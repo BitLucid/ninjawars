1f8175fa5db5a6a5e373b8135019f258ed8e6baf
Roy <tchalvakspam@gmail.com>
2010-01-31 23:57:51 -0500

    Skills page: Some quick cleanup, template comes next.

--- deploy/www/skills.php
+++ deploy/www/skills.php
@@ -23,54 +23,66 @@ $class = getClass($username);
 $status_array = getStatus($username);
 
 $row = $sql->data;
-echo "<p>You are a level $level, $class Ninja.</p>\n";
 $status_output_list = render_status_section();
+$no_skills = true;
+$stealth = $skillsListObj->hasSkill('Stealth');
+$stealth_turn_cost = $skillsListObj->getTurnCost('Stealth');
+$unstealth_turn_cost = $skillsListObj->getTurnCost('Unstealth');
+$chi = $skillsListObj->hasSkill('Chi');
+$speed = $skillsListObj->hasSkill('Chi');
+$hidden_resurrect = $skillsListObj->hasSkill('hidden resurrect');
+
+echo "<p>You are a level $level, $class Ninja.</p>\n";
 echo "<p>Your status is: ".$status_output_list."</p>";
 echo "<div id='skills-list'>";
-$no_skills = true;
 
-if ($skillsListObj->hasSkill('Stealth')) {
+if ($stealth) {
 	echo "<div id='stealth-skills'>";
 	echo "<p>By selecting Stealth you will go into a mode where enemies can not directly attack you for a short time.";
 	echo "<a href=\"about.php#skills\">(help)</a></p><p>";
 	echo "<form action=\"skills_mod.php\" method=\"post\">\n";
 	echo "<div>\n";
 	echo "<input type=\"submit\" name=\"command\" value=\"Stealth\" class=\"formButton\">\n";
-	echo "</select> Turn Cost: ".$skillsListObj->getTurnCost('Stealth')." to Stealth.\n";
+	echo "</select> Turn Cost: ".$stealth_turn_cost." to Stealth.\n";
 	echo "</div>\n";
 	echo "</form></p><p>";
 
 	echo "<form action=\"skills_mod.php\" method=\"post\">\n";
 	echo "<div>\n";
 	echo "<input type=\"submit\" name=\"command\" value=\"Unstealth\" class=\"formButton\">\n";
-	echo "Turn Cost: ".$skillsListObj->getTurnCost('Unstealth')." to Unstealth.\n ";
+	echo "Turn Cost: ".$unstealth_turn_cost." to Unstealth.\n ";
 	echo "</div>\n";
 	echo "</form></p>";
 	echo "</div>";
 	$no_skills = false;
 }
 
-if ($skillsListObj->hasSkill('Chi')) {
-    echo "<p id='chi-skill'>Your Chi skill increases the benefits of healing at the shrine.</p>";
+if ($chi) {
+    echo "<p id='chi-skill'>Chi: Your Chi skill increases the benefits of healing at the shrine.</p>";
 }
 
-if ($skillsListObj->hasSkill('speed')) {
-    echo "<p id='speed-skill'>Due to your speed, you gain back turns at a faster rate.</p>";
+if ($speed) {
+    echo "<p id='speed-skill'>Speed: Due to your speed, you gain back turns at a faster rate.</p>";
 } // +1 every hour, so 5 per hour instead of 4.
 
 
 /*
 if ($skillsListObj->hasSkill('midnight heal')) {
-    echo "<p id='midnight-heal-skill'>When resurrected you will come back with more health than other ninja.</p>";
-}  THIS IS NOT CURRENTLY THE CASE*/
+    echo "<p id='midnight-heal-skill'>Midnight Heal: When resurrected you will come back with more health than other ninja.</p>";
+}  THIS IS NOT CURRENTLY THE CASE TODO NEEDS A FIX*/
 
-if ($skillsListObj->hasSkill('hidden resurrect')) {
-    echo "<p id='hidden-resurrect-skill'>When you are resurrected you will return already hidden and stealthed.</p>";
+if ($hidden_resurrect) {
+    echo "<p id='hidden-resurrect-skill'>Hidden Resurrect: When you are resurrected you will return already hidden and stealthed.</p>";
 }
 
 if($no_skills){
 	echo "<p id='no-skills'>You do not have any skills you can use on yourself.</p>\n";
 }
+
+
+
+
+
 ?>
 </div>
 <div id='search-for-ninja'>
