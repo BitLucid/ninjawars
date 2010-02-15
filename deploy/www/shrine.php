d9b5f635ab1dc780e7cad51bab2fea62f8f868be
Roy <tchalvakspam@gmail.com>
2010-02-15 11:32:16 -0500

    Shrine: Moved a check to only happen when logged on to squash notices.

--- deploy/www/shrine.php
+++ deploy/www/shrine.php
@@ -7,12 +7,12 @@ $freeResKillLimit  = 25;
 $lostTurns         = 10; // *** Default turns lost when the player has no kills.
 $startingKills     = 0;
 $userLevel         = 0;
-$at_max_health = ($players_health >= (150 + (($players_level - 1) * 25)));
 $poisoned = getStatus($username) && isset($status_array['Poisoned']) && $status_array['Poisoned'];
 
 if(isset($username)){
 	$startingKills     = getKills($username);
 	$userLevel         = getLevel($username);
+    $at_max_health = ($players_health >= (150 + (($players_level - 1) * 25)));
 
 	// *** A True or False as to whether resurrection will be free.
 	$freeResurrection = ($userLevel < $freeResLevelLimit && $startingKills < $freeResKillLimit);
