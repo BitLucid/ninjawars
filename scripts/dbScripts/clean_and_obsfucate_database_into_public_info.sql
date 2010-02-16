b9c54f45be3ec4b4a0ffcb9846aad0305d15e436
Roy <tchalvakspam@gmail.com>
2010-02-15 22:27:31 -0500

    Sql: clean & obsfucate script updated and renamed.

--- scripts/dbScripts/clean_and_obsfucate_database_into_public_info.sql
+++ scripts/dbScripts/clean_and_obsfucate_database_into_public_info.sql
@@ -2,9 +2,7 @@ update players set uname = (substring(uname, 1, 1) || player_id) where uname != 
 update clan set clan_name = (substring(clan_name, 1, 1) || clan_id);
 update players set pname = 'password';
 update players set email = player_id || 'example@example.com';
-update players set ip = '127.0.0.33';
-update players set gold=(player_id/2), kills=(player_id/100);
-update inventory set amount=10 where amount != 0 and amount != 1;
+update players set gold=(player_id/2), kills=(player_id/100), messages='', ip='127.0.0.33';
 update past_stats set stat_result='no-one' where stat_type='Yesterday''s Vicious Killer';
 truncate chat;
 truncate events;
@@ -15,3 +13,4 @@ truncate dueling_log;
 truncate levelling_log;
 truncate players_backup;
 truncate players_flagged;
+grant all on chat, players, events, messages, dueling_log, levelling_log, flags, past_stats, clan, clan_player, player_rank, players_backup, players_flagged, ppl_online, rankings, settings, time to developers;
