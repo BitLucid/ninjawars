8c8ef1fcd58ceff3e83848530b0ff35d13587ef3
Roy <tchalvakspam@gmail.com>
2010-03-02 15:37:09 -0500

    Js: Minor commenting out of minor visibility toggle.

--- deploy/www/js/nw.js
+++ deploy/www/js/nw.js
@@ -403,13 +403,7 @@ $(document).ready(function() {
 		chainedUpdate(); // Start the periodic index update.
        	
        /* Collapse the following parts of the index */
-        $("#links-menu").toggle();
-        
-        /* Expand the chat when it's clicked. */
-        //$("#expand-chat").click(function () {
-            //$("#mini-chat-frame-container").removeClass('chat-collapsed').addClass('chat-expanded');/*.height(780);*/
-            //$(this).hide();  // Hide the clicked section after expansion.
-        //});
+        //$("#links-menu").toggle();
         
         // Click to view more chat messages than you were looking at before.
         $('#expand-chat').click(function (){
