b541088f2d3a907d0d7c6f07f0540a20ca2d9d1a
Roy <tchalvakspam@gmail.com>
2010-06-04 15:18:08 -0400

    Accounts: Whoops, errant distinct in the account table creation query.

--- deploy/db/migrations/20100504_account_table.sql
+++ deploy/db/migrations/20100504_account_table.sql
@@ -14,6 +14,6 @@ create table account_players (
 );
 
 -- Still a problem with duplicate accounts/identities here.
-insert into accounts (account_identity, active_email, phash, type) select distinct lower(email) as email, lower(email) as active_email, crypt(pname, gen_salt('bf')) as phash, 0 from players;
+insert into accounts (account_identity, active_email, phash, type) select lower(email) as email, lower(email) as active_email, crypt(pname, gen_salt('bf')) as phash, 0 from players;
 -- Just a simple hack to deal with lowercasing of emails.
 insert into account_players (_account_id, _player_id) select account_id, player_id from accounts join players on account_identity = lower(email);
