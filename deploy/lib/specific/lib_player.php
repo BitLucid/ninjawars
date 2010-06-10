f0cfb95c7ecafc745a102c504dc75616e692b11c
Roy <tchalvakspam@gmail.com>
2010-06-10 14:37:56 -0400

    Accounts: Created (not-quite-complete) accounts lib.

--- deploy/lib/specific/lib_player.php
+++ deploy/lib/specific/lib_player.php
@@ -1,5 +1,6 @@
 <?php
 require_once(LIB_ROOT."specific/lib_status.php");
+require_once(LIB_ROOT."common/lib_accounts.php");
 // lib_player.php
 
 // Defines for avatar options.
@@ -268,53 +269,11 @@ function display_clan_options($player_info, $viewing_player_obj) {
 	}
 }
 
-// Check whether the player is the leader of their clan.
-function is_clan_leader($player_id) {
-	return (($clan = get_clan_by_player_id($player_id)) && $player_id == get_clan_leader_id($clan->getID()));
+
+function render_player_link($username) {
+    return "<a href='player.php?player=".htmlentities(urlencode($username))."'>".htmlentities($username)."</a>";
 }
 
-function get_rank($username) {
-	DatabaseConnection::getInstance();
-	$statement = DatabaseConnection::$pdo->prepare("SELECT rank_id FROM rankings WHERE uname = :player");
-	$statement->bindValue(':player', $username);
-	$statement->execute();
-
-	$rank = $statement->fetchColumn();
-
-	return ($rank > 0 ? $rank : 1); // Make rank default to 1 if no valid ones are found.
-}
-
-// Gives the blacklisted emails, should eventually be from a table.
-function get_blacklisted_emails() {
-	return array('@hotmail.com', '@hotmail.co.uk', '@msn.com', '@live.com', '@aol.com', '@aim.com', '@yahoo.com', '@yahoo.co.uk');
-}
-
-// Gives whitelisted emails, make a table eventually.
-function get_whitelisted_emails() {
-	return array('@gmail.com');
-}
-
-// Return 1 if the email is a blacklisted email, 0 otherwise.
-function preconfirm_some_emails($email) {
-	// Made the default be to auto-confirm players.
-	$res = 1;
-	$blacklisted_by = get_blacklisted_emails();
-	$whitelisted_by = get_whitelisted_emails();
-
-	foreach ($blacklisted_by AS $loop_domain) {
-		if (strpos(strtolower($email), $loop_domain)) {
-			return 1;
-		}
-	}
-
-	foreach ($whitelisted_by AS $loop_domain) {
-		if (strpos(strtolower($email), $loop_domain)) {
-			return 0;
-		}
-	}
-
-	return $res;
-}
 
 function render_class_select($current) {
 	DatabaseConnection::getInstance();
@@ -344,6 +303,7 @@ function render_class_select($current) {
     return $res;
 }
 
+
 function display_signup_form($enteredName, $enteredEmail, $enteredClass, $enteredReferral) {
 	// ************************* START OF SIGNUP FORM ********************************
 	$class_select = render_class_select($enteredClass);
@@ -359,219 +319,23 @@ function display_signup_form($enteredName, $enteredEmail, $enteredClass, $entere
 } // *** End of function display_signup_form().
 
 
-function create_player($send_name, $params=array()) {
+
+
+// Check whether the player is the leader of their clan.
+function is_clan_leader($player_id) {
+	return (($clan = get_clan_by_player_id($player_id)) && $player_id == get_clan_leader_id($clan->getID()));
+}
+
+function get_rank($username) {
 	DatabaseConnection::getInstance();
-
-    $send_email  = $params['send_email'];
-    $send_pass   = $params['send_pass'];
-    $send_class  = $params['send_class'];
-    $preconfirm  = (int) $params['preconfirm'];
-    $confirm     = (int) $params['confirm'];
-    $referred_by = $params['referred_by'];
-
-	// Create the initial player row.
-	$playerCreationQuery= "INSERT INTO players
-		 (uname, pname, health, strength, gold, messages, kills, turns, confirm, confirmed,
-		  email, _class_id, level,  status, member, days, ip, bounty, created_date)
-		 VALUES
-		 (:username, :pass, '150', '5', '100', '', '0', '180', :confirm, :preconfirm,
-		 :email, (SELECT class_id FROM class WHERE class_name = :class), '1', '1', '0', '0', '', '0', now())";
-	//  ***  Inserts the choices and defaults into the player table. Status defaults to stealthed. ***
-	$statement = DatabaseConnection::$pdo->prepare($playerCreationQuery);
-	$statement->bindValue(':username', $send_name);
-	$statement->bindValue(':pass', $send_pass);
-	$statement->bindValue(':confirm', $confirm);
-	$statement->bindValue(':preconfirm', $preconfirm);
-	$statement->bindValue(':email', $send_email);
-	$statement->bindValue(':class', $send_class);
+	$statement = DatabaseConnection::$pdo->prepare("SELECT rank_id FROM rankings WHERE uname = :player");
+	$statement->bindValue(':player', $username);
 	$statement->execute();
 
-	$headers  = "MIME-Version: 1.0\r\n";
-	$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
-	$headers .= "From: ".SYSTEM_MESSENGER_NAME." <".SYSTEM_MESSENGER_EMAIL.">\r\n";
-	$headers .= "Reply-To: ".SUPPORT_EMAIL_FORMAL_NAME." <".SUPPORT_EMAIL.">\r\n";
+	$rank = $statement->fetchColumn();
 
-	//  ***  Sends out the confirmation email to the chosen email address.  ***
-	$_to = "$send_email";
-	$_subject = "NinjaWars Account Sign Up";
-	$_body = render_template('signup_email_body.tpl', array(
-	        'send_name'       => $send_name
-			, 'confirm'       => $confirm
-			, 'send_class'    => $send_class
-			, 'SUPPORT_EMAIL' => SUPPORT_EMAIL
-		)
-	);
-
-	$_from = $headers;
-	// *** Create message object.
-	$message = new Nmail($_to, $_subject, $_body, $_from);
-    if (DEBUG) {$message->dump = true;}
-	$sent = false; // By default, assume failure.
-	$sent = $message->send();
-
-	// TODO: Need an in-game error logging.
-    return $sent;
+	return ($rank > 0 ? $rank : 1); // Make rank default to 1 if no valid ones are found.
 }
 
-function confirm_player($player_name, $confirmation=0, $autoconfirm=false) {
-	DatabaseConnection::getInstance();
 
-	// Preconfirmed or the email didn't send, so automatically confirm the player.
-	$require_confirm = ($autoconfirm ? '' : "and confirm = :confirmation");
-
-	$up = "update players set confirmed = 1, confirm='55555' where uname = :player ".$require_confirm;
-
-	$statement = DatabaseConnection::$pdo->prepare($up);
-	$statement->bindValue(':player', $player_name);
-
-	if ($require_confirm) {
-		$statement->bindValue(':confirmation', $confirmation);
-	}
-
-	$up_res = $statement->execute();
-
-    return ($autoconfirm ? true : $up_res);
-}
-
-function validate_signup($enteredName, $enteredEmail, $enteredClass, $enteredReferral, $enteredPass) {
-	$successful = false;
-	DatabaseConnection::getInstance();
-
-	$send_name   = trim($enteredName);
-	$send_pass   = $enteredPass;
-	$send_class  = $enteredClass;
-	$send_email  = trim($enteredEmail);
-	$referred_by = $enteredReferral;
-
-	echo "Your responses:<br> Name - $send_name,<br>
-		 Password - ".((isset($send_pass) ? "***yourpassword***" : "NO PASSWORD")).",<br>
-		 Class - $send_class,<br>
-		 Email - $send_email,<br>
-		 Site Referred By - $referred_by<br><br>\n";
-
-	//  *** Requirement checking Section  ***
-
-	if ($send_name != "" && $send_pass != "" && $send_email != "" && $send_class != "") {  //When everything is non-blank.
-		$check_name  = 0;
-		$check_email = 0;
-
-		$statement = DatabaseConnection::$pdo->prepare("SELECT uname FROM players WHERE lower(uname) = :username");
-		$statement->bindValue(':username', strtolower($send_name));
-		$statement->execute();
-		$check_name = $statement->fetch();
-
-		$statement = DatabaseConnection::$pdo->prepare("SELECT email FROM players WHERE lower(email) = :email");
-		$statement->bindValue(':email', strtolower($send_email));
-		$statement->execute();
-		$check_email = $statement->fetch();
-
-        // Validate the username symbols!
-		$username_error = validate_username($send_name);
-
-		if ($username_error) {
-			echo $username_error;
-		} else {  //when all the name requirement errors didn't trigger.
-			$send_name = trim($send_name);  // Just cuts off any white space at the end.
-			$filter = new Filter();
-			$send_name = $filter->toUsername($send_name); // Filter any un-whitelisted characters.
-			echo "Phase 1 Complete: Name passes requirements.<hr>\n";
-
-			// Validate the password!
-			$password_error = false && validate_password($send_pass);
-
-			if ($password_error) {
-				echo $password_error;
-			} else {
-				$send_pass = trim($send_pass); // *** Trims any extra space off of the password.
-				echo "Phase 2 Complete: Password passes requirements.<hr>\n";
-
-				if (FALSE) { // CURRENTLY NO BLOCKED EMAIL SERVICES strstr($send_email, "@") == "@aol.com" || strstr($send_email, "@") == "@netscape.com" || strstr($send_email, "@") == "@aim.com"
-				    //Throws error if email from blocked domain.
-					echo "Phase 3 Incomplete: We cannot currently accept @aol.com, @netscape.com, or @aim.com email addresses.";
-				} elseif (!eregi("^[[:alnum:]][a-z0-9_.-]*@[a-z0-9.-]+\.[a-z]{2,4}$", trim($send_email))) {
-					echo "Phase 3 Incomplete: The email address (".htmlentities($send_email).")
-					    must contain an @ symbol and a domain name to be valid.";
-				} else if (!$check_name && !$check_email && $send_name != "SysMsg" && $send_name != "NewUserList") {
-				    //Uses previous query to make sure name and email aren't duplicates.
-					echo "Phase 3 Complete: Username and Email are unique.<br><hr>\n";
-
-					$statement = DatabaseConnection::$pdo->query('SELECT class_name FROM class WHERE class_active');
-					$statement->execute();
-
-					$legalClasess = array();
-					while ($classData = $statement->fetch()) {
-						$legalClasses[] = $classData['class_name'];
-					}
-
-					if (!in_array($send_class, $legalClasses)) {
-						echo "Phase 4 Incomplete: No proper class was specified.<br>";
-					} else {
-						echo "Phase 4 Complete: Class was specified.<br><hr>";
-
-						// *** Signup is successful at this point  ***
-						$preconfirm = 0;
-						$preconfirm = preconfirm_some_emails($send_email);
-
-						if (!$preconfirm) { /* not blacklisted by, so require a normal email confirmation */
-							echo "Phase 5: When you receive an email from SysMsg,
-							 it will describe how to activate your account.<br><br>\n";
-						}
-
-						// The preconfirmation message occurs later.
-						$confirm = rand(1000,9999); //generate confirmation code
-
-						// Use the function from lib_player
-						$player_params = array(
-						    'send_email'    => $send_email
-							, 'send_pass'   => $send_pass
-							, 'send_class'  => $send_class
-							, 'preconfirm'  => $preconfirm
-							, 'confirm'     => $confirm
-							, 'referred_by' => $referred_by
-						);
-
-						$successful = create_player($send_name, $player_params); // Create the player.
-
-						if (!$successful) {
-						    echo "There was a problem with creating a player account.  Please contact us as below: ";
-						} else {
-    						if (!$preconfirm) {
-    							//  *** Continues the page display ***
-    							echo "Confirmation email has been sent to <b>".$send_email."</b>.  <br>
-    				  					Be sure to also check for the email in any \"Junk Mail\" or \"Spam\" folders.
-    				  					Delivery typically takes less than 15 minutes.";
-    						} else {
-    						    // Use the confirm function from lib_player.
-    						    confirm_player($send_name, false, true); // name, no confirm #, just autoconfirm.
-    							echo "<p>Account with the login name \"".$send_name
-    							."\" is now confirmed!  Please login on the login bar of the ninjawars.net page.</p>";
-    						}
-
-    						echo "<p>Only one account per person is allowed.</p>";
-						}
-
-						echo "If you require help use the forums at <a href='".WEB_ROOT."forum/'>"
-						    .WEB_ROOT."forum/</a> or email: ".SUPPORT_EMAIL;
-					}	// *** End of class checking.
-				} else {	// Default, displays when the username or email are not unique.
-					$what = ($check_email != 0 ? "Email" : "Username");
-
-					echo "Phase 3 Incomplete: That $what is already in use. Please choose a different one.\n";
-				}
-			}
-		}
-	} else {  //  ***  Response for when nothing was submitted.  ***
-		echo "Phase 1 Incomplete: You did not correctly fill out all the necessary information.\n";
-	}
-
-	//This is the final signup return section, which only shows if a successful
-	//insert and confirmation number has -not- been acheived.
-
-	echo "<br><br>";
-	return $successful;
-} // *** End of validate_signup() function.
-
-function render_player_link($username) {
-    return "<a href='player.php?player=".htmlentities(urlencode($username))."'>".htmlentities($username)."</a>";
-}
 ?>
