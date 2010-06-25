02a4483f9b3a8b72eecd9b974dbc78a7b6baec51
Roy <tchalvakspam@gmail.com>
2010-06-25 13:58:31 -0400

    Input: Don't filter is no request comes in, just return the default.

--- deploy/lib/common/lib_input.php
+++ deploy/lib/common/lib_input.php
@@ -7,13 +7,15 @@
  */
 
 function in($var_name, $default_val=null, $filter_method='toText'){
-    $result = isset($_REQUEST[$var_name])? $_REQUEST[$var_name] : $default_val;
+    $result = null;
+    $req = isset($_REQUEST[$var_name])? $_REQUEST[$var_name] : null;
     $filter = new Filter();
-    // Check for the appropriate method.
-    if($filter_method && method_exists($filter, $filter_method)){
-        $result = $filter->$filter_method($result);
+    // Check for the appropriate method, only filter if something came in to start with.
+    if($filter_method && method_exists($filter, $filter_method) && $req){
+        return $filter->$filter_method($req);
+    } else {
+        return $default_val;
     }
-    return $result;
 }
 
 ?>
