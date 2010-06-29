43d1b3db3abfe0507ae4037dbb6a45fa14a1cab8
Al Vazquez <beagle@netrunner.(none)>
2010-06-29 03:17:48 -0400

    Parameterized a query for safety. Whitespace changes

--- deploy/lib/common/lib_auth.php
+++ deploy/lib/common/lib_auth.php
@@ -9,7 +9,9 @@ function authenticate($p_login, $p_pass) {
 	if ($login != '' && $pass != '') {
 		// Allow login via username or email.
 		$sql = "SELECT account_id, account_identity, uname, player_id
-			FROM accounts join account_players on account_id=_account_id join players on player_id = _player_id
+			FROM accounts
+			JOIN account_players ON account_id = _account_id
+			JOIN players ON player_id = _player_id
 			WHERE (lower(active_email) = :login OR lower(uname) = :login) AND phash = crypt(:pass, phash)";
 		return query_row($sql, array(':login'=>$login, ':pass'=>$pass));
 	} else {
@@ -19,10 +21,11 @@ function authenticate($p_login, $p_pass) {
 
 /**
  * Login the user and delegate the setup if login is valid.
-**/
+ **/
 function login_user($p_user, $p_pass) {
 	$success = false;
 	$error   = 'That password/username combination was incorrect.';
+
 	if ($data = authenticate($p_user, $p_pass)) {
 		SESSION::commence(); // Start a session on a successful login.
 		$_COOKIE['username'] = $data['uname']; // May want to keep this for relogin easing purposes.
@@ -43,7 +46,7 @@ function login_user($p_user, $p_pass) {
 
 // Sets the last logged in date equal to now.
 function update_last_logged_in($ninja_id) {
-	$up = "update accounts set last_login = now() where account_id in (select _account_id from account_players where _player_id = :ninja_id )";
+	$up = "UPDATE accounts SET last_login = now() WHERE account_id IN (SELECT _account_id FROM account_players WHERE _player_id = :ninja_id)";
 	return query($up, array(':ninja_id'=>array($ninja_id, PDO::PARAM_INT)));
 }
 
@@ -71,7 +74,7 @@ function get_account_ip() {
 
 /**
  * @return boolean Check whether someone is logged into their account.
-**/
+ **/
 function is_logged_in() {
 	return !!get_logged_in_account_id();
 }
@@ -80,7 +83,7 @@ function is_logged_in() {
 /**
  * Just do a check whether the input username and password is valid
  * @return boolean
-**/
+ **/
 function is_authentic($p_user, $p_pass) {
 	// Note that authenticate is happily side-effect-less.
 	return (boolean)authenticate($p_user, $p_pass);
@@ -88,7 +91,7 @@ function is_authentic($p_user, $p_pass) {
 
 /**
  * Logout function.
-**/
+ **/
 function logout_user($echo=false, $redirect='index.php') {
 	$msg = 'You have been logged out.';
 	nw_session_destroy();
@@ -117,7 +120,7 @@ function validate_password($send_pass) {
 	$error = null;
 	$filter = new Filter();
 
-	if (strlen($password_to_hash)<7 || strlen($password_to_hash)>500) {  //  Throws error if password has html elements.
+	if (strlen($password_to_hash) < 7 || strlen($password_to_hash) > 500) {
 		$error = "Phase 2 Incomplete: Passwords must be at least 7 characters long.<hr>\n";
 	}
 
@@ -146,16 +149,16 @@ function validate_username($send_name) {
 }
 
 /*
-Potential regex for a username.
+   Potential regex for a username.
  * A username must start with a lower-case or upper-case letter
  * A username can contain only letters, numbers or underscores
  * A username must be between 8 and 24 characters
  * A username cannot end in an underscore
- 
-function valid_name($username) {
-	return preg_match("#^[a-z][\da-z_]{6,22}[a-z\d]\$#i", $username);
-}
-*/
+
+ function valid_name($username) {
+ return preg_match("#^[a-z][\da-z_]{6,22}[a-z\d]\$#i", $username);
+ }
+ */
 
 // Takes in a potential login name and saves it over multiple logins.
 function nw_session_start($potential_username = '') {
@@ -163,8 +166,7 @@ function nw_session_start($potential_username = '') {
 
 	if (!isset($_COOKIE['user_cookie']) || $_COOKIE['user_cookie'] != $potential_username) {
 		// Refresh cookie if the username isn't set in it yet.
-		$result['cookie_created'] =
-			createCookie("user_cookie", $potential_username, (time()+60*60*24*365), "/", WEB_ROOT); // *** 365 days ***
+		$result['cookie_created'] = createCookie("user_cookie", $potential_username, (time()+60*60*24*365), "/", WEB_ROOT); // *** 365 days ***
 	} else {
 		$result['cookie_existed'] = true;
 	}
@@ -180,25 +182,25 @@ function nw_session_destroy() {
 /**
  * Returns display:none style information depending on the current state.
  * Used primarily on the index page.
-**/
+ **/
 function display_when($state) {
-	$on = '';
-	$off = "style='display:none;'";
+	$on  = '';
+	$off = "style='display: none;'";
 
-	switch (true) {
-		case $state == 'logged_in':
+	switch ($state) {
+		case 'logged_in':
 			return (is_logged_in() ? $on : $off);
-		break;
-		case $state == 'logged_out':
+			break;
+		case 'logged_out':
 			return (is_logged_in() ? $off : $on);
-		break;
-		case $state == 'logout_occurs':
+			break;
+		case 'logout_occurs':
 			$logout = in('logout');
 			return (isset($logout) ? $on : $off);
-		break;
-		case $state == 'login_failed':
+			break;
+		case 'login_failed':
 			return (in('action') == 'login' && !is_logged_in() ? $on : $off);
-		break;
+			break;
 		default:
 			if (DEBUG) {
 				throw Exception('improper display_when() argument');
@@ -207,7 +209,7 @@ function display_when($state) {
 			}
 
 			return $off;
-		break;
+			break;
 	}
 }
 
@@ -219,7 +221,7 @@ function get_username($ninja_id=null) {
 // Returns a ninja name from a ninja id.
 function get_ninja_name($ninja_id=null) {
 	static $self;
- 
+
 	if (!$ninja_id) {
 		if ($self) {
 			// Self info requested
@@ -315,11 +317,11 @@ function createCookie($name, $value='', $maxage=0, $path='', $domain='', $secure
 	// Prevent "headers already sent" error with utf8 support (BOM)
 	//if ( utf8_support ) header('Content-Type: text/html; charset=utf-8');
 	$header_string = 'Set-Cookie: '.rawurlencode($name).'='.rawurlencode($value)
-					.(empty($domain) ? '' : '; Domain='.$domain)
-					.(empty($maxage) ? '' : '; Max-Age='.$maxage)
-					.(empty($path) ? '' : '; Path='.$path)
-					.(!$secure ? '' : '; Secure')
-					.(!$HTTPOnly ? '' : '; HttpOnly');
+		.(empty($domain) ? '' : '; Domain='.$domain)
+		.(empty($maxage) ? '' : '; Max-Age='.$maxage)
+		.(empty($path)   ? '' : '; Path='.$path)
+		.(!$secure       ? '' : '; Secure')
+		.(!$HTTPOnly     ? '' : '; HttpOnly');
 	header($header_string, false);
 	assert(isset($domain));
 	return true;
@@ -330,21 +332,23 @@ function createCookie($name, $value='', $maxage=0, $path='', $domain='', $secure
  */
 function membership_and_combat_stats($update_past_stats=false) {
 	DatabaseConnection::getInstance();
-	$vk = DatabaseConnection::$pdo->query('SELECT uname FROM levelling_log WHERE killsdate = cast(now() AS date) group by uname, killpoints order by killpoints DESC LIMIT 1');
+	$vk = DatabaseConnection::$pdo->query('SELECT uname FROM levelling_log WHERE killsdate = cast(now() AS date) GROUP BY uname, killpoints ORDER BY killpoints DESC LIMIT 1');
 	$todaysViciousKiller = $vk->fetchColumn();
 
 	if ($todaysViciousKiller == '') {
 		$todaysViciousKiller = 'None';
 	} elseif ($update_past_stats) {
-		DatabaseConnection::$pdo->query('UPDATE past_stats SET stat_result = \''
-			.$todaysViciousKiller
-			.'\' WHERE id = 4'); // 4 is the ID of the vicious killer stat.
+		$update = DatabaseConnection::$pdo->prepare('UPDATE past_stats SET stat_result = :visciousKiller WHERE id = 4'); // 4 is the ID of the vicious killer stat.
+		$update->bindValue(':visciousKiller', $todaysViciousKiller);
+		$update->execute();
 	}
 
 	$stats['vicious_killer'] = $todaysViciousKiller;
-	$pc = DatabaseConnection::$pdo->query("Select count(player_id) FROM players WHERE confirmed = 1");
+
+	$pc = DatabaseConnection::$pdo->query("SELECT count(player_id) FROM players WHERE confirmed = 1");
 	$stats['player_count'] = $pc->fetchColumn();
-	$po = DatabaseConnection::$pdo->query("select count(*) from ppl_online where member = true");
+
+	$po = DatabaseConnection::$pdo->query("SELECT count(*) FROM ppl_online WHERE member = true");
 	$stats['players_online'] = $po->fetchColumn();
 
 	return $stats;
