ee83230ccff896fbbc8d496052a610f8b6134d59
Al Vazquez <beagle@netrunner.(none)>
2010-06-29 03:17:24 -0400

    in() now returns the original value if filter method requested is null

--- deploy/lib/common/lib_input.php
+++ deploy/lib/common/lib_input.php
@@ -7,15 +7,20 @@
  */
 
 function in($var_name, $default_val=null, $filter_method='toText'){
-    $result = null;
-    $req = isset($_REQUEST[$var_name])? $_REQUEST[$var_name] : null;
-    $filter = new Filter();
-    // Check for the appropriate method, only filter if something came in to start with.
-    if($filter_method && method_exists($filter, $filter_method) && $req){
-        return $filter->$filter_method($req);
-    } else {
-        return $default_val;
-    }
+	$result = null;
+	$req    = (isset($_REQUEST[$var_name]) ? $_REQUEST[$var_name] : null);
+	$filter = new Filter();
+
+	// Check for the appropriate method, only filter if something came in to start with.
+	if ($req) {
+		if ($filter_method && method_exists($filter, $filter_method)) {
+			return $filter->$filter_method($req);
+		} else {
+			return $req;
+		}
+	} else {
+		return $default_val;
+	}
 }
 
 ?>
