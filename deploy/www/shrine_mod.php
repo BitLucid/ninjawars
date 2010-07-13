922eee6fa9aca5307d11db43e2536747b85a3bf4
Al Vazquez <beagle@netrunner.(none)>
2010-07-12 22:22:31 -0400

    Shrine now uses player object more. Display omission bugs in shrine effects fixed.

--- deploy/www/shrine_mod.php
+++ deploy/www/shrine_mod.php
@@ -49,40 +49,47 @@ if ($restore == 1) {	//  *** RESURRECTION SECTION ***
 
 	if ($startingHealth > 0) {
 		$error = 'You are not dead.';
-	} else if ($startingKills > 1 || $freeResurrection) {	// If you're dead, and a newbie, or dead and have kills.
-		//  *** FREE RESURRECTION DETERMINATION ***
-
-		if (!($freeResurrection)) { // 1 kill point cost for resurrection above level 5 or 25 kills.
-			subtractKills($username, 1);
+	} else {
+		if ($startingKills > 1 || $freeResurrection) {	// If you're dead, and a newbie, or dead and have kills.
+			$turn_taking_resurrect = false; // Template display variable.
 			$kill_taking_resurrect = false;
+			//  *** FREE RESURRECTION DETERMINATION ***
+
+			if (!$freeResurrection) { // 1 kill point cost for resurrection above level 5 or 25 kills.
+				$player->vo->kills = subtractKills($username, 1);
+				$kill_taking_resurrect = true;
+			}
+		} elseif ($startingTurns > 0) { // Dead and no killpoints left, and not a newbie.
+			$turn_taking_resurrect = true; // Template display variable.
+		    $kill_taking_resurrect = false;
+
+			if ($startingTurns < $lostTurns && $startingTurns > 0) { // *** From 9 to 1 turns.
+				$lostTurns = $startingTurns;
+				$final_turns = $player->vo->turns = subtractTurns($username, $lostTurns); // *** Takes away necessary turns.
+			}
+		} else { // *** No kills, no turns, and too high of a level.
+	    	$error = 'You have no kills or turns, so you must wait to regain turns before you can return to life.';
 		}
 
-		$player->death();
+		if ($kill_taking_resurrect || $turn_taking_resurrect) {
+			$player->death();
 
-		$returning_health = ($has_chi ? 150 : 100);
-		setHealth($username, $returning_health);
-		$final_turns = (string) getTurns($username);
-		$final_kills = getKills($username);
-		
-		// *** FREE STEALTHING FOR BLACK CLASS UPON NON-FREE RESURRECTION
-		if ($players_class == "Black" && (!$freeResurrection)) {
-			addStatus($username, STEALTH);
+			if ($kill_taking_resurrect) {
+				// *** FREE STEALTHING FOR BLACK CLASS UPON NON-FREE RESURRECTION
+				if ($players_class == "Black" && (!$freeResurrection)) {
+					addStatus($username, STEALTH);
+				}
+
+				$returning_health = ($has_chi ? 150 : 100);
+			} else {
+				$returning_health = 100;
+			}
+
+			$finalHealth = $player->vo->health = setHealth($username, $returning_health);
+			$final_turns = $player->vo->turns;
+			$final_kills = $player->vo->kills;
+			$fully_healed = false;
 		}
-	} elseif ($startingTurns > 0) { // Dead and no killpoints left, and not a newbie.
-		if ($startingTurns < $lostTurns && $startingTurns > 0) { // *** From 9 to 1 turns.
-			$lostTurns = $startingTurns;
-		}
-
-		$player->death();
-
-		subtractTurns($username, $lostTurns); // *** Takes away necessary turns.
-		setHealth($username, 100);
-		$final_turns = (string) getTurns($username);
-		
-		$turn_taking_resurrect = true; // Template display variable.
-	    $kill_taking_resurrect = false;
-	} else { // *** No kills, no turns, and too high of a level.
-	    $error = 'You have no kills or turns, so you must wait to regain turns before you can return to life.';
 	}
 } // *** end of resurrection ***
 
@@ -95,7 +102,7 @@ if ($healed == 1 || $max_heal == 1) {  //If the user tried to heal themselves.
 	    $heal_points = $startingGold;
 	}
 	
-	$current_health = getHealth($username);
+	$current_health = $player->vo->health;
 
 	if ($current_health >= $max_health) {
 	    $error = 'You are currently at full health.';
@@ -109,10 +116,9 @@ if ($healed == 1 || $max_heal == 1) {  //If the user tried to heal themselves.
 
     				subtractGold($username,$heal_points);
     				// Having chi increases the amount healed in all cases.
-    				addHealth($username,($has_chi? round($heal_points*1.5) : $heal_points));
-    				$finalHealth = getHealth($username);
+    				$player->vo->health = $finalHealth = addHealth($username, ($has_chi? round($heal_points*1.5) : $heal_points));
     				
-    				$fully_healed = ($finalHealth>=$max_health); // Let the user know whether they're fully healed.
+    				$fully_healed = ($finalHealth >= $max_health); // Let the user know whether they're fully healed.
     				
     			} else {
     			    $error = 'You do not have enough gold for that much healing.';
@@ -127,13 +133,13 @@ if ($healed == 1 || $max_heal == 1) {  //If the user tried to heal themselves.
 } else if ($poisoned == 1) {	//  *** POISON SECTION ***
 	$poison_cure_requested = true;
 
-	if (getHealth($username) > 0) {
+	if ($player->vo->health > 0) {
 		$cost = 50;  //  the cost of curing poison is set here.
 
 		if ($startingGold >= $cost) {
-			if (getStatus($username) && $status_array['Poison']) {
-				subtractGold($username,$cost);
-				subtractStatus($username,POISON);
+			if ($player->hasStatus(POISON)) {
+				subtractGold($username, $cost);
+				subtractStatus($username, POISON);
 			} else {
 			    $error = 'You are not ill.';
 			}
