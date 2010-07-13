1350b733ab5ca97dd2dcd803e5eb73a195002224
Al Vazquez <beagle@netrunner.(none)>
2010-07-12 22:16:42 -0400

    Removed unused function for nw_error_reporting. Whitespace changes to infrequently touched files

--- deploy/lib/func/assignment_functions.php
+++ deploy/lib/func/assignment_functions.php
@@ -3,28 +3,28 @@
  * Return first non-null argument.
 **/
 function first_value() {
-    $arg_list = func_get_args();
-    foreach($arg_list as $l_arg){
-        if(!is_null($l_arg)){
-            return $l_arg;
-        }
-    }
-    return null;
-}
+	$arg_list = func_get_args();
+	foreach ($arg_list as $l_arg) {
+		if (!is_null($l_arg)) {
+			return $l_arg;
+		}
+	}
 
+	return null;
+}
 
 /**
  * Much more easy-going, just:
  * Return first true-like argument.
 **/
 function whichever() {
-    $arg_list = func_get_args();
-    foreach($arg_list as $l_arg){
-        if($l_arg != false){
-            return $l_arg;
-        }
-    }
-    return null;    
-}
+	$arg_list = func_get_args();
+	foreach ($arg_list as $l_arg) {
+		if ($l_arg != false) {
+			return $l_arg;
+		}
+	}
 
+	return null;
+}
 ?>
