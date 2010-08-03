e5c3701d02609e68512e9180e5c7fd0e3bf75803
Al Vazquez <al.vazquez@gmail.com>
2010-08-02 20:23:36 -0400

    Updated some code in player.php to reduce database trips. Some whitespace changes to lib_input

--- deploy/lib/common/lib_input.php
+++ deploy/lib/common/lib_input.php
@@ -8,48 +8,50 @@
 // Input function that by default LEAVES INPUT COMPLETELY UNFILTERED
 // To not filter some input, you have to explicitly pass in null for the third parameter,
 // e.g. in('some_url_parameter', null, null)
-function in($var_name, $default_val=null, $filter_callback=null){
-    $result = isset($_REQUEST[$var_name])? $_REQUEST[$var_name] : $default_val;
-    // Check that the filter function sent in exists.
-    if($filter_callback && function_exists($filter_callback)){
-        $result = $filter_callback($result);
-    }
+function in($var_name, $default_val=null, $filter_callback=null) {
+	$result = (isset($_REQUEST[$var_name]) ? $_REQUEST[$var_name] : $default_val);
+	// Check that the filter function sent in exists.
+	if ($filter_callback && function_exists($filter_callback)) {
+		$result = $filter_callback($result);
+	}
+
     return $result;
 }
 
 // Casts to an integer anything that can be cast that way non-destructively, otherwise null.
-function sanitize_to_int($dirty){
-	if($dirty == (int) $dirty){ // Cast anything that can be non-destructively cast.
+function sanitize_to_int($dirty) {
+	if ($dirty == (int) $dirty) { // Cast anything that can be non-destructively cast.
 		$res = (int) $dirty;
 	} else {
 		$res = null;
 	}
+
 	return $res;
 }
 
 // Strip everything except alphanumeric, underscore, and dash
-function sanitize_to_word($dirty){
-	return preg_replace("[^A-Za-z0-9_\-]", "", (string) $dirty);	
+function sanitize_to_word($dirty) {
+	return preg_replace("[^A-Za-z0-9_\-]", "", (string) $dirty);
 }
 
 // Strip everything except words, digits, spaces, _, -, ., @, :, and slash for urls /
-function sanitize_to_text($dirty){
-    // Allows words, digits, spaces, _, -, ., @, :, and slash for urls /
+function sanitize_to_text($dirty) {
+	// Allows words, digits, spaces, _, -, ., @, :, and slash for urls /
 	return preg_replace("/[^\w\d\s_\-\.\@\:\/]/", "", (string) $dirty);
 }
 
-function sanitize_to_email($dirty){
+function sanitize_to_email($dirty) {
 	return filter_var($dirty, FILTER_SANITIZE_EMAIL);
 }
 
 // Restrict an option to certain possibilities, e.g. for an orderby string, the possibilities would be an array of column names.
-function restrict_to($original, $possibilities=array(), $default=null){
-	foreach ($possibilities as $possibility){
-		if ($original == $possibility){
+function restrict_to($original, $possibilities=array(), $default=null) {
+	foreach ($possibilities as $possibility) {
+		if ($original == $possibility) {
 			return $original;
 		}
 	}
+
 	return $default;  // If the original doesn't match, just return the default.
 }
-
 ?>
--- deploy/www/player.php
+++ deploy/www/player.php
@@ -26,31 +26,38 @@ if (!$target_player_obj || !$target_player_obj->player_id || !$target_player_obj
 		$template = 'no-player.tpl';
 		$parts    = array();
 	} else {
-		$score         = get_score_formula();
-		$user_id       = get_user_id();
-		$username      = get_username();
+		$viewing_player_obj = new Player(get_user_id());
+
+		$score = get_score_formula();
+
+		if ($viewing_player_obj && $viewing_player_obj->vo) {
+			$user_id  = $viewing_player_obj->vo->player_id;
+			$username = $viewing_player_obj->vo->uname;
+			$self     = ($username && $username == $player_info['uname']); // Record whether this is a self-viewing.
+		} else {
+			$self = false;
+		}
+
 		$message       = in('message');
 
 		$player      = $target = $player_info['uname']; // reset the target and target_id vars.
 		$target_id   = $player_info['player_id'];
-		$self        = (get_username() && get_username() == $player_info['uname']); // Record whether this is a self-viewing.
-	
+
 		if ($message) {
 		    send_message($user_id, $target_id, $message);
 		    // "message sent" notice will be displayed by the template itself.
 		}
-	
-		$viewing_player_obj = new Player(get_username());
+
 		$viewers_clan       = ($viewing_player_obj instanceof Player && $viewing_player_obj->vo ? get_clan_by_player_id($viewing_player_obj->vo->player_id) : null);
-	
+
 		// Attack Legal section
 		$params          = array('required_turns'=>0, 'ignores_stealth'=>true); // 0 for unstealth.
 		$AttackLegal     = new AttackLegal($username, $target, $params);
 		$attack_allowed  = $AttackLegal->check();
 		$attack_error    = $AttackLegal->getError();
-	
+
 		// TODO: Add the "player since" date to the player profile/info.
-	
+
 		DatabaseConnection::getInstance();
 		$statement = DatabaseConnection::$pdo->prepare("SELECT rank_id FROM rankings WHERE uname = :player");
 		$statement->bindValue(':player', $player_info['uname']);
@@ -65,23 +72,23 @@ if (!$target_player_obj || !$target_player_obj->player_id || !$target_player_obj
 		$level_category          = level_category($player_info['level']);
 
 		$gravatar_url            = generate_gravatar_url($target_player_obj);
-	
+
 		if ($username && !$attack_error && !$self) { // They're not dead or otherwise unattackable.
 			// Attack or Duel
-	
+
 			$skillDAO = new SkillDAO();
-	
-			$combat_skills = $skillDAO->getSkillsByTypeAndClass($viewing_player_obj->vo->_class_id, 'combat', $viewing_player_obj->vo->level)->fetchAll(); 
+
+			$combat_skills = $skillDAO->getSkillsByTypeAndClass($viewing_player_obj->vo->_class_id, 'combat', $viewing_player_obj->vo->level)->fetchAll();
 			    // *** todo When Smarty3 is released, remove fetch all and change template to new foreach-as syntax ***
-	
+
 			$item_use_section  = render_item_use_on_another($target);
 			$skill_use_section = render_skills($target, $viewing_player_obj);
 		}	// End of the there-was-no-attack-error section
-	
+
 		$set_bounty_section     = '';
 		$communication_section  = '';
 		$player_clan_section    = '';
-	
+
 		$clan = get_clan_by_player_id($player_info['player_id']);
 
 		// Player clan and clan members
@@ -99,9 +106,9 @@ if (!$target_player_obj || !$target_player_obj->player_id || !$target_player_obj
 				$same_clan = $render_clan_options = false;
 			}
 		}
-	
+
 		// Send the info to the template.
-	
+
 		$template = 'player.tpl';
 		$parts = get_certain_vars(get_defined_vars(), array('combat_skills', 'player_info', 'self', 'rank_spot', 'level_category', 'gravatar_url', 'status_list', 'clan'));
 	}
