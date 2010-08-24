f6d3dcd1b3814505ae14a19d9b4ca15d0475bac5
Roy <tchalvakspam@gmail.com>
2010-08-24 17:14:05 -0400

    Inventory migration: Had to drop the constraints on the deprecated item_name column in inventory.

--- deploy/db/migrations/20100811_inventory_string_removal.sql
+++ deploy/db/migrations/20100811_inventory_string_removal.sql
@@ -1,1 +1,3 @@
 alter table inventory rename column item to item_type_string_backup;
+alter table inventory drop constraint inventory_item_fkey;
+alter table inventory ALTER COLUMN item_type_string_backup DROP NOT NULL;
