9f71114013b259f017ad9361a68222c9f617f813
Roy <tchalvakspam@gmail.com>
2010-10-03 03:01:30 -0400

    Splash: login templatized.  Login.php and Logout.php pages created.

--- deploy/www/logout.php
+++ deploy/www/logout.php
@@ -1,12 +1,8 @@
 <?php
-$alive      = false;
-$private    = false;
-$quickstat  = false;
-$page_title = "Log out";
 
-include SERVER_ROOT."interface/header.php";
+init($private=false, $alive=false);
 
-logout_user($echo=true, $redirect=false); // From lib_auth (for authenticate)
+logout_user($echo=false, $redirect='login.php?logged_out=1'); // logout_user lives in lib_auth
+// Logout immediately redirects to login.php to display a "you are now logged out" message.
 
-include SERVER_ROOT."interface/footer.php";
 ?>
