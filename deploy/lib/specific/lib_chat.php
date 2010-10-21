586d80a3e384693a1dd8d273a8b08e73dd533ee7
Al Vazquez <beagle@netrunner.(none)>
2010-10-20 22:40:10 -0400

    send_chat now rejects blank messages. Village button uses onclick to refresh instead of the non-functioning href it was using.

--- deploy/lib/specific/lib_chat.php
+++ deploy/lib/specific/lib_chat.php
@@ -16,21 +16,23 @@ function send_chat($user_id, $msg) {
 
 	$msg = trim($msg);
 
-	$statement = DatabaseConnection::$pdo->prepare("SELECT message FROM chat WHERE sender_id = :sender ORDER BY date DESC LIMIT 1");
-	$statement->bindValue(':sender', $user_id);
-	$statement->execute();
-	$prevMsg = trim($statement->fetchColumn());
-
-	if ($prevMsg != $msg) {
-		$statement = DatabaseConnection::$pdo->prepare("INSERT INTO chat (chat_id, sender_id, message, date) VALUES (default, :sender, :message, now())");
+	if ($msg) {
+		$statement = DatabaseConnection::$pdo->prepare("SELECT message FROM chat WHERE sender_id = :sender ORDER BY date DESC LIMIT 1");
 		$statement->bindValue(':sender', $user_id);
-		$statement->bindValue(':message', $msg);
 		$statement->execute();
+		$prevMsg = trim($statement->fetchColumn());
 
-		return true;
-	} else {
-		return false;
+		if ($prevMsg != $msg) {
+			$statement = DatabaseConnection::$pdo->prepare("INSERT INTO chat (chat_id, sender_id, message, date) VALUES (default, :sender, :message, now())");
+			$statement->bindValue(':sender', $user_id);
+			$statement->bindValue(':message', $msg);
+			$statement->execute();
+
+			return true;
+		}
 	}
+
+	return false;
         
 	// could add channels later.
 }
