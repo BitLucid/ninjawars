8ac7ae85b33bbc35d6b048ed1ef3a89472eaf68b
Roy <tchalvakspam@gmail.com>
2010-11-19 02:31:21 -0500

    /interface/ footer.php and header.php deleted.

--- deploy/interface/footer.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Displays the ending html stuff, and potentially the quickstats js refresh.
-display_template('footer.tpl', array("quickstat"=>(isset($quickstat) ? $quickstat : null)));
-?>
