4fb4ae5dcf51c71fb3a7ab8906227f1dba0004d2
Al Vazquez <beagle@netrunner.(none)>
2011-01-05 10:50:20 -0500

    New index creation script added to make DB more efficient, especially for ordering by date

---
+++ deploy/db/migrations/20110105_indeces.sql
@@ -0,0 +1,4 @@
+CREATE INDEX chat_date_index ON chat(date);
+CREATE INDEX messages_date_index ON messages(date);
+CREATE INDEX events_date_index ON events(date);
+CREATE INDEX dueling_log_date_index ON dueling_log(date);
