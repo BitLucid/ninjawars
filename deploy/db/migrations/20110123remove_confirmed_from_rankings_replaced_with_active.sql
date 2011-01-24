1768ffe2caf7a6cab910d22add9e0f9727a78ebe
Roy <tchalvakspam@gmail.com>
2011-01-23 21:34:28 -0500

    Sql: Fixed dependency for rankings view that prevented rename of confirmed to active.

---
+++ deploy/db/migrations/20110123remove_confirmed_from_rankings_replaced_with_active.sql
@@ -0,0 +1,13 @@
+drop view rankings;
+create view rankings as SELECT player_rank.rank_id, players.player_id, player_rank.score, players.uname, class.class_name, players.level, 
+        CASE
+            WHEN players.health = 0 THEN 0
+            ELSE 1
+        END::boolean AS alive, players.days
+   FROM player_rank
+   JOIN players ON players.player_id = player_rank._player_id
+   JOIN class ON class.class_id = players._class_id
+  WHERE players.active = 1
+  ORDER BY player_rank.rank_id;
+
+alter table players rename column confirmed to active;
---
+++ deploy/db/migrations/20110124rename_undeadlocked_confirm_column.sql
@@ -0,0 +1,1 @@
+alter table players rename column confirmed to active;
