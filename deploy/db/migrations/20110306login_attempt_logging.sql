8cac951af81a54db6853075929a9a6ae2e6d5465
Roy <tchalvakspam@gmail.com>
2011-03-17 22:56:52 -0400

    SQL: Added some grants needed for the login_attempts table.

--- deploy/db/migrations/20110306login_attempt_logging.sql
+++ deploy/db/migrations/20110306login_attempt_logging.sql
@@ -1,1 +1,3 @@
 create table login_attempts (attempt_id serial, username text, ua_string text, ip text, successful int, additional_info text, attempt_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+grant all on login_attempts_attempt_id_seq to developers;
+grant all on login_attempts to developers;
