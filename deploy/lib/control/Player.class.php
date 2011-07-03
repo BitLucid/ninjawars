62472b1a8761957bd77ab76ff9a1a0b640e46eff
Al Vazquez <beagle@netrunner.(none)>
2011-07-03 02:04:43 -0400

    Attempting to solve error caused by uninitialized PlayerVO in Player object. Also whitespace fixes in Player.class.php

--- deploy/lib/control/Player.class.php
+++ deploy/lib/control/Player.class.php
@@ -37,23 +37,25 @@ class Player
 			if (!($this->vo = $dao->get($this->player_id))) {
 				$this->vo = new PlayerVO();
 			}
+		} else {
+			$this->vo = new PlayerVO();
 		}
 	}
 
 	public function __toString() {
 		return $this->vo->uname;
 	}
-	
-	public function name(){
+
+	public function name() {
 		return $this->vo->uname;
 	}
-	
-	public function id(){
-	    return $this->vo->player_id;
+
+	public function id() {
+		return $this->vo->player_id;
 	}
-	
-	public function level(){
-	    return $this->vo->level;
+
+	public function level() {
+		return $this->vo->level;
 	}
 
 	// Save the Player state.
@@ -65,7 +67,7 @@ class Player
 	// Actively pulls the latest status data from the db.
 	protected function queryStatus() {
 		$id = $this->id();
-		if($id){
+		if ($id) {
 			return query_item("SELECT status FROM players WHERE player_id = :player_id", 
 				array(':player_id'=>array($id, PDO::PARAM_INT)));
 		} else {
@@ -131,8 +133,8 @@ class Player
 			return $str;
 		}
 	}
-	
-	
+
+
 
 	public function speed() {
 		$speed = $this->vo->speed;
@@ -142,7 +144,7 @@ class Player
 			return $str;
 		}
 	}
-	
+
 	public function stamina() {
 		$stat = $this->vo->stamina;
 		if ($this->hasStatus(POISON)) {
@@ -151,16 +153,16 @@ class Player
 			return $stat;
 		}
 	}
-	
+
 	public function ki() {
 		return $this->vo->ki;
 	}
-	
-	public function add_ki($amount){
+
+	public function add_ki($amount) {
 		query('update players set ki = ki + :amount where player_id = :id', array(':amount'=>$amount, ':id'=>$this->id()));
 	}
-	
-	public function subtract_ki($amount){
+
+	public function subtract_ki($amount) {
 		query('update players set ki = case when (ki - :amount) < 1 then 0 else ki - :amount2 end where player_id = :id', array(':amount'=>$amount, ':amount2'=>$amount, ':id'=>$this->id()));
 	}
 
@@ -170,7 +172,7 @@ class Player
 
 	public function hasStatus($p_status) {
 		$status = valid_status($p_status);
-		if($status){
+		if ($status) {
 			return (bool)($this->getStatus()&$status);
 		} else {
 			return false;
@@ -181,47 +183,48 @@ class Player
 		// Set to make active the primary indicator, lets deity fully determine who to make inactive or not.
 		return !!$this->vo->active;
 	}
-	
-	public function isAdmin(){
-	    $name = strtolower($this->name());
-	    if ($name == 'tchalvak' || $name == 'beagle' || $name == 'suavisimo') {
-	        return true;
-	    }
 
-	    return false;
+	public function isAdmin() {
+		$name = strtolower($this->name());
+		if ($name == 'tchalvak' || $name == 'beagle' || $name == 'suavisimo') {
+			return true;
+		}
+
+		return false;
 	}
 
 	public function death() {
 		$this->resetStatus();
 		$this->subtractHealth($this->health());
 	}
-	
+
 	public function email() {
 		$account = account_info_by_char_id($this->id());
 		return $account['active_email'];
 	}
-	
-	public function turns(){
+
+	public function turns() {
 		return $this->vo->turns;
 	}
 
-	public function changeTurns($amount){
+	public function changeTurns($amount) {
 		return change_turns($this->id(), $amount);
 	}
 
-    // Pull the data of the player obj as an array.
-    public function data($specific = null) {
-    	static $data;
-    	if(!$data){
-    		$data = add_data_to_player_row($this->as_array());
-    		// Cache this data over the live of the player object.
-    	}
-    	if($specific){
-    		return $data[$specific];
-    	} else {
-	        return $data;
-        }
-    }
+	// Pull the data of the player obj as an array.
+	public function data($specific = null) {
+		static $data;
+		if (!$data) {
+			$data = add_data_to_player_row($this->as_array());
+			// Cache this data over the live of the player object.
+		}
+
+		if ($specific) {
+			return $data[$specific];
+		} else {
+			return $data;
+		}
+	}
 
 	public function as_vo() {
 		return $this->vo;
@@ -234,9 +237,9 @@ class Player
 	public function getClan() {
 		DatabaseConnection::getInstance();
 		$statement = DatabaseConnection::$pdo->prepare("SELECT clan_id, clan_name 
-				FROM clan 
-				JOIN clan_player ON clan_id = _clan_id 
-				WHERE _player_id = :player");
+			FROM clan 
+			JOIN clan_player ON clan_id = _clan_id 
+			WHERE _player_id = :player");
 		$statement->bindValue(':player', $this->player_id);
 		$statement->execute();
 		if ($data = $statement->fetch()) {
@@ -246,78 +249,78 @@ class Player
 			return null;
 		}
 	}
-	
+
 	// Pull the class identity for a character.
 	public function class_identity() {
-	    return char_class_identity($this->id());
-	}
-	
-	// Pull the class display name for a character.
-	public function class_display_name() {
-	    return char_class_name($this->id());
+		return char_class_identity($this->id());
 	}
 
-    // Complex wrapper that allows for robust healing with a limit of the max health.	
+	// Pull the class display name for a character.
+	public function class_display_name() {
+		return char_class_name($this->id());
+	}
+
+	// Complex wrapper that allows for robust healing with a limit of the max health.	
 	public function heal($amount) {
 		$hurt = $this->hurt_by();
 		// Heal at most the amount hurt, or the amount requested, pick whichever is smallest.
 		$heal = min($hurt, $amount);
-	    return $this->addHealth($heal);
-	}
-	
-	// Simple wrapper for changeHealth
-	public function addHealth($amount) {
-	    return $this->changeHealth($amount);
-	}
-	
-	// Simple wrapper for subtractive action.
-	public function subtractHealth($amount) {
-	    return $this->changeHealth((-1*(int)$amount));
-	}
-	
-	// To subtract just send in a negative integer.
-	public function changeHealth($add_amount) {
-    	$amount = (int)$add_amount;
-    	// Only change on positive or negative changes, not zero.
-    	if (abs($amount) > 0) {
-        	$id = $this->id();
-            // Set health = 0 when it's less than zero, otherwise modify it.
-    	    $up = "UPDATE players SET health = 
-    		   CASE WHEN health + :amount < 0 THEN 0 ELSE health + :amount2 END 
-    		   WHERE player_id  = :player_id";
-    		query($up, array(':player_id'=>array($id, PDO::PARAM_INT),
-    		    ':amount'=>$amount, ':amount2'=>$amount));
-    		$this->vo->health = $amount;
-    	}
-    	return $this->health(); // Return the current health.
+		return $this->addHealth($heal);
 	}
 
-    // Pull the current health.	
+	// Simple wrapper for changeHealth
+	public function addHealth($amount) {
+		return $this->changeHealth($amount);
+	}
+
+	// Simple wrapper for subtractive action.
+	public function subtractHealth($amount) {
+		return $this->changeHealth((-1*(int)$amount));
+	}
+
+	// To subtract just send in a negative integer.
+	public function changeHealth($add_amount) {
+		$amount = (int)$add_amount;
+		// Only change on positive or negative changes, not zero.
+		if (abs($amount) > 0) {
+			$id = $this->id();
+			// Set health = 0 when it's less than zero, otherwise modify it.
+			$up = "UPDATE players SET health = 
+				CASE WHEN health + :amount < 0 THEN 0 ELSE health + :amount2 END 
+				WHERE player_id  = :player_id";
+			query($up, array(':player_id'=>array($id, PDO::PARAM_INT),
+				':amount'=>$amount, ':amount2'=>$amount));
+			$this->vo->health = $amount;
+		}
+		return $this->health(); // Return the current health.
+	}
+
+	// Pull the current health.	
 	public function health() {
-	    $id = $this->id();
-        $sel = "SELECT health from players where player_id = :id";
+		$id = $this->id();
+		$sel = "SELECT health from players where player_id = :id";
 		return query_item($sel, array(':id'=>array($id, PDO::PARAM_INT)));
 	}
-	
+
 	// Return the amount below the max health (or zero).
-	public function hurt_by(){
+	public function hurt_by() {
 		return max(0, 
 			($this->max_health() - $this->health())
-			);
+		);
 	}
-	
+
 	// This char's max health
 	public function max_health() {
-	    return max_health_by_level($this->level());
+		return max_health_by_level($this->level());
 	}
 
 	// Return the current percentage health.
 	public function health_percent() {
-	    return health_percent($this->health(), $this->level());
+		return health_percent($this->health(), $this->level());
 	}
 
-	
-	public function ip(){
+
+	public function ip() {
 		return account_info_by_char_id($this->id(), 'ip');
 	}
 }
