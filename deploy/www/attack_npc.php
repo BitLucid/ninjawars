c846753d124f02231d2a8ce432ec6cd1e9116da0
Roy <tchalvakspam@gmail.com>
2011-07-19 14:24:48 -0400

    List_all_players and attack_npc: Fix broken redirection urls.

--- deploy/www/attack_npc.php
+++ deploy/www/attack_npc.php
@@ -2,7 +2,7 @@
 // Deprecated script for ferretting out continued bad url usages.
 error_log('Deprecated linking to attack_npc url performed from referrer: '.$_SERVER['HTTP_REFERER']);
 $query = $_SERVER['QUERY_STRING'];
-$new_url = 'npc.php'.$query;
+$new_url = 'npc.php?'.$query;
 permanent_redirect($new_url);
 
 ?>
