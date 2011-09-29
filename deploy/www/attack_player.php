ea3f478122ec2a8741af58cbb553430705ecc8fc
Roy <tchalvakspam@gmail.com>
2011-09-29 17:47:54 -0400

    Attack_player: Squash notice.

--- deploy/www/attack_player.php
+++ deploy/www/attack_player.php
@@ -1,6 +1,6 @@
 <?php
 // Deprecated script for backwards compatibility.
-error_log('Deprecated linking to attack_player.php url performed from referrer: '.$_SERVER['HTTP_REFERER']);
+error_log('Deprecated linking to attack_player.php url performed from referrer: '.@$_SERVER['HTTP_REFERER']);
 $query = $_SERVER['QUERY_STRING'];
 $new_url = 'map.php'.$query;
 permanent_redirect($new_url);
