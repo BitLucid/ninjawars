a9cbc2ae427f3a4576681954ca0fbd37da6e6475
Roy <roy.ronalds@gmail.com>
2012-05-26 23:20:19 -0400

    Docs: Grant docs, minor tweak to grant syntax.

--- docs/grant_developers_roles_kzqai.sql
+++ docs/grant_developers_roles_kzqai.sql
@@ -1,5 +1,5 @@
 create role developers;
 GRANT ALL ON *.* TO GROUP developers;
-GRANT ALL ON DATABASE ninjawars to developers;
+GRANT ALL ON DATABASE ninjawars to group developers;
 
 create role kzqai SUPERUSER LOGIN in group developers;
