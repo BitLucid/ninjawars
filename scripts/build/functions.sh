fc9afdf553e3e0430a56a220e085fdbb0b8c8407
Taufan Aditya <toopay@taufanaditya.com>
2013-05-26 02:13:10 +0700

    Fix attemp

--- scripts/build/functions.sh
+++ scripts/build/functions.sh
@@ -130,9 +130,9 @@ function set_webserver {
 	echo '127.0.0.1       nw.local' >> /etc/hosts
 	FULL_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 	DIR=`echo $FULL_SCRIPT_DIR | sed 's/scripts\/build//'`
-	sed "s/__DIR__/$DIR/" $FULL_SCRIPT_DIR/tpl/nw.local > /etc/apache2/sites-available/nw.local
+	sed "s/__DIR__/$DIR/" "$FULL_SCRIPT_DIR/tpl/nw.local" > "/etc/apache2/sites-available/nw.local"
 	sudo a2ensite nw.local
 	sudo service apache2 restart
-	sed "s/__DBUSER__/$1/;s/__DBNAME__/$2/" $FULL_SCRIPT_DIR/tpl/resources.php > "$DIRdeploy/resources.php"
+	sed "s/__DBUSER__/$1/;s/__DBNAME__/$2/" "$FULL_SCRIPT_DIR/tpl/resources.php" > "$DIRdeploy/resources.php"
 	say_ok "Web-server configured!"
 }
