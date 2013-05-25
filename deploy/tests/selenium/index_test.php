278406392c5ceb43f780a9ec7f0fd56980600eda
Taufan Aditya <toopay@taufanaditya.com>
2013-05-25 22:59:51 +0700

    Typo in selenium test

--- deploy/tests/selenium/index_test.php
+++ deploy/tests/selenium/index_test.php
@@ -2,7 +2,7 @@
 
 class index_test extends PHPUnit_Extensions_Selenium2TestCase {
 
-	public function set_up()
+	public function setUp()
 	{
 		$this->setBrowser('firefox');
 		
@@ -10,7 +10,7 @@ class index_test extends PHPUnit_Extensions_Selenium2TestCase {
 		$this->setBrowserUrl('http://127.0.0.1/ninjawars/deploy/www/index.php');
 	}
 	
-	public function test_title()
+	public function testTitle()
 	{
 		// Change below url to your servername, eg : $this->url('http://nw.local/');
 		$this->url('http://127.0.0.1/ninjawars/deploy/www/index.php');
