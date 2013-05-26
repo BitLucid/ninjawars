66e1dc281a63b0ad2b958f50247639fa9c51d92c
Taufan Aditya <toopay@taufanaditya.com>
2013-05-26 15:38:35 +0700

    Validate database creation, symlink to gitignore #209

--- scripts/build/install.sh
+++ scripts/build/install.sh
@@ -40,7 +40,12 @@ ensure_system
 
 say_loud "Creating database"
 
-psql -c "create database $DBNAME;" -U $DBUSER
+DATABASE_CREATED=$(psql -c "create database $DBNAME;" -U $DBUSER | grep "CREATE DATABASE")
+if [ "CREATE DATABASE" != "$DATABASE_CREATED" ]; then
+	say_error $DBUSER" was not a valid Postgres user, create "$DBUSER" first!"
+	quit_gracefully
+fi
+
 
 say_loud "Checking for project depedencies"
 
@@ -51,5 +56,8 @@ set_composer
 set_build $DBUSER $DBNAME
 vendor/bin/propel-gen
 vendor/bin/propel-gen insert-sql
+
+# TODO : need more clean way to import gitignore.
+ln -s docs/gitignoreSAMPLE .gitignore
 
 say_loud "Complete!"
