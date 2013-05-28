98d9954d60968c23c7f698e110cdd3c63b855b45
Taufan Aditya <toopay@taufanaditya.com>
2013-05-29 01:57:01 +0700

    Return to current dir after setup apache sites

--- scripts/build/functions.sh
+++ scripts/build/functions.sh
@@ -151,6 +151,7 @@ function set_webserver {
 	sudo a2ensite nw.local
 	sudo a2enmod rewrite
 	sudo service apache2 restart
+	cd $DIR
 	sed "s,__DBUSER__,$1,;s,__DBNAME__,$2," $FULL_SCRIPT_DIR/tpl/resources.php > $DIR"deploy/resources.php"
 	mkdir -p $DIR"deploy/templates/compiled"
 	sudo chown www-data $DIR"deploy/templates/compiled"
