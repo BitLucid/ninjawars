51b78e69cce48001bb5b197e4edabb278beb56c2
Roy Ronalds <tchalvakspam@gmail.com>
2013-07-11 15:24:38 -0400

    Main: Changed title on main page to represent it better and differentiate it from the index page.

--- deploy/www/main.php
+++ deploy/www/main.php
@@ -6,7 +6,6 @@ init($private=false, $alive=false);
 $show_faqs = in('show_faqs');
 
 $page = 'main';
-$pages = array('main'=>array('title'=>'Live by the Sword', 'template'=>'main.tpl'));
+$pages = array('main'=>array('title'=>'Start Playing Ninjawars', 'template'=>'main.tpl'));
 
 display_static_page($page, $pages, $vars=array('user_id'=>self_char_id(), 'show_faqs'=>$show_faqs), $options=array());
-?>
