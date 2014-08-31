03341c8f6b9af6a39b9e86d99d0c3db3689aa2b9
Roy Ronalds <tchalvakspam@gmail.com>
2014-08-31 01:50:59 -0400

    Stats: Cleaned up the new in-progress IC stats stuff somewhat.

--- deploy/www/stats.php
+++ deploy/www/stats.php
@@ -18,10 +18,26 @@ $instincts = in('instincts');
 $beliefs = in('beliefs');
 $traits = in('traits');
 
+$dev = (bool) DEBUG;
+
+
+// Update a player's OOC profile
+function update_profile($char_id, $new_profile){
+	DatabaseConnection::getInstance();
+	$statement = DatabaseConnection::$pdo->prepare('UPDATE players SET messages = :profile WHERE player_id = :char_id');
+	$statement->bindValue(':profile', $new_profile);
+	$statement->bindValue(':char_id', $char_id);
+	$statement->execute();
+	return true;
+}
 
 
 $username = self_name();
-$user_id  = self_char_id();
+$char_id  = self_char_id();
+
+// Password and email changing systems exist in account.php (& account.tpl).
+
+$char         = new Player($char_id);
 
 $profile_changed    = false;
 $profile_max_length = 500; // Should match the limit in limitStatChars.js - ajv: No, limitStatChars.js should be dynamically generated with this number from a common location -
@@ -30,24 +46,11 @@ $successMessage = null;
 if ($changedetails == 1) {
     // Limit the profile length.
 	if ($newprofile != '') {
-		DatabaseConnection::getInstance();
-		$statement = DatabaseConnection::$pdo->prepare('UPDATE players SET messages = :profile WHERE player_id = :player');
-		$statement->bindValue(':profile', $newprofile);
-		$statement->bindValue(':player', $user_id);
-		$statement->execute();	// todo - test for success
-
-		$profile_changed = true;
+		$profile_changed = update_profile($char_id, $newprofile);
 	} else {
 		$error = 'Cannot enter a blank profile.';
 	}
 }
-
-// Password and email changing systems exist in account.php (& account.tpl).
-
-
-// Password and email changing systems exist in account.php (& account.tpl).
-
-$char         = new Player($user_id);
 
 if($changedetails){
 	$changed = false;
@@ -61,15 +64,16 @@ if($changedetails){
 	}
 
 	if($changed){
-		$char->save();
-		debug($char);die();
+		$char->save(); // Ha, Ha, Ha
 	}
 }
-$description = 'This is a description here and all';
-$goals = 'Kill ninja of the ramen clan';
-$beliefs = 'I believe in a one true ninja god';
-$instincts = 'When I hear whistling, I duck';
-$traits = 'Hardy, nervous, meaty, silent';
+if(DEBUG){
+	$description = 'This is a description here and all';
+	$goals = 'Kill ninja of the ramen clan';
+	$beliefs = 'I believe in a one true ninja god';
+	$instincts = 'When I hear whistling, I duck';
+	$traits = 'Hardy, nervous, meaty, silent';
+}
 $player           = self_info();
 //$player['created_date']=$player['created_date']? date("c", strtotime($player['created_date'])) : null;
 $class_theme      = class_theme($char->class_identity());
@@ -77,13 +81,13 @@ $level_category   = level_category($player['level']);
 $status_list      = get_status_list();
 $gravatar_url     = generate_gravatar_url($player['player_id']);
 $gurl             = $gravatar_url;
-$rank_display     = get_rank($user_id); // rank display.
+$rank_display     = get_rank($char_id); // rank display.
 $profile_editable = $player['messages'];
 
-$parts = get_certain_vars(get_defined_vars(), array('player', 'level_category', 'status_list', 'description', 'goals', 'beliefs', 'instincts', 'traits'));
+$parts = get_certain_vars(get_defined_vars(), array('player', 'level_category', 'status_list', 'description', 'goals', 'beliefs', 'instincts', 'traits', 'dev'));
 
 // Set the parts array's player clan if any is found.
-if ($parts['player_clan'] = get_clan_by_player_id($user_id)) {
+if ($parts['player_clan'] = get_clan_by_player_id($char_id)) {
     // Set the char clan name and id for later usage.
 	$parts['clan_name'] = $parts['player_clan']->getName();
 	$parts['clan_id']   = $parts['player_clan']->getID();
