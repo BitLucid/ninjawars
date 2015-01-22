1726905a19a4954684bbb13f28119887741c404d
Roy Ronalds <tchalvakspam@gmail.com>
2015-01-22 11:24:59 -0500

    ninjamaster: Additions to view a certain character by username.

--- deploy/templates/ninjamaster.tpl
+++ deploy/templates/ninjamaster.tpl
@@ -1,23 +1,59 @@
+<style>
+.float-right{
+	float:right;clear:both;
+}
+.headed{
+	border-top:1px brown solid;border-left:1px brown solid;
+}
+.char-inventory{
+	height:1.3em;
+}
+.char-info-header{
+	border-bottom:1px brown solid;color:#ADD8E6;
+}
+#admin-actions table caption{
+	text-align:center;
+}
+</style>
+
+<div id='admin-actions'>
+
 <h1>Admin Actions</h1>
+
+<section class='centered glassbox'>
+	<form action='' method='post'>
+		View character @<input id='char-name' type='text' placeholder='character' value='{$char_name}' required=required>
+		<div><input type='Submit' value='Match'></div>
+	</form>
+</section>
 
 {if $char_infos}
 <!-- View the details of some ninja -->
 {foreach from=$char_infos item='char_info'}
+<div id='clear' class='float-right block'>
+	<a href='/ninjamaster'>Clear</a>
+</div>
 <table>
-	<tr><thead>Specific Character's info for <strong class='char-name'>{$char_info.uname}</strong></thead></tr>
+	<caption>Specific Character's info for <strong class='char-name'>{$char_info.uname}</strong></caption>
+	<thead>
+		{foreach from=$char_info key='name' item='stat'}<th class='char-info-header'>{$name}</th>{/foreach}
+	</thead>
 	<tr>
 	{foreach from=$char_info key='name' item='stat'}
-		<td style='border:1px brown solid'>{$name}</td><td> {$stat}</td>
+		<td>{$stat}</td>
 	{/foreach}
 	</tr>
+	<tfoot>
+		<tr><td>Out-of-Character profile: {$message}</td></tr>
+	</tfoot>
 </table>
-<table>
-	<tr><thead>Inventory for <strong class='char-name'>{$char_info.uname}</strong></thead></tr>
+<table class='char-inventory'>
+	<caption>Inventory for <strong class='char-name'>{$char_info.uname}</strong>:</caption>
 	{foreach from=$char_inventory key='name' item='item'}
-		<tr style='height:1.3em'>
+		<tr class='info'>
 		<td>&#9734;</td>
 		{foreach from=$item key='column' item='data'}
-			<td style='border-top:1px brown solid;border-left:1px brown solid'>{$column}</td><td> {$data}</td>
+			<td class='headed'>{$column}</td><td> {$data}</td>
 		{/foreach}
 		</tr>
 	{/foreach}
@@ -42,3 +78,6 @@
 	<a href='/ninjamaster/?view={$dupe.player_id}' class='char-name'>{$dupe.uname}</a> :: IP {$dupe.ip}<br>
 	{/foreach}
 {/if}
+
+
+</div><!-- End of #admin-actions -->
--- deploy/www/ninjamaster/index.php
+++ deploy/www/ninjamaster/index.php
@@ -1,68 +1,91 @@
 <?php
 require_once(LIB_ROOT."control/lib_inventory.php"); // The inventory functions.
 
-// Secure the script
-$char_id = get_char_id();
-$self = null;
-if($char_id){
-	$self = new Player($char_id);
-}
-if($self && $self->isAdmin()){
-// Admin possibilities start here.
+class AdminViews{
 
-function high_rollers(){
-	// Select first few max kills from players.
-	// Max turns.
-	// Max gold.
-	// Max kills
-	// etc.
-	$res = array();
-	$res['gold'] = query_array('select player_id, uname, gold from players order by gold desc limit 10');
-	$res['turns'] = query_array('select player_id, uname, turns from players order by turns desc limit 10');
-	$res['kills'] = query_array('select player_id, uname, kills from players order by kills desc limit 10');
-	$res['health'] = query_array('select player_id, uname, health from players order by health desc limit 10');
-	$res['ki'] = query_array('select player_id, uname, ki from players order by ki desc limit 10');
-	return $res;
-}
-
-
-function duped_ips(){
-	return query_array('select uname, player_id, ip from players where ip in (SELECT ip FROM players WHERE active = 1 GROUP  BY ip HAVING count(*) > 1 ORDER BY count(*) ASC limit 30) order by ip');
-}
-
-// Return one or many
-function split_char_infos($ids){
-	if(is_numeric($ids)){
-		return array(char_info($ids));
-	} else {
+	public static function high_rollers(){
+		// Select first few max kills from players.
+		// Max turns.
+		// Max gold.
+		// Max kills
+		// etc.
 		$res = array();
-		$ids = explode(',', $ids);
-		foreach($ids as $id){
-			$res[$id] = char_info($id);
-		}
+		$res['gold'] = query_array('select player_id, uname, gold from players order by gold desc limit 10');
+		$res['turns'] = query_array('select player_id, uname, turns from players order by turns desc limit 10');
+		$res['kills'] = query_array('select player_id, uname, kills from players order by kills desc limit 10');
+		$res['health'] = query_array('select player_id, uname, health from players order by health desc limit 10');
+		$res['ki'] = query_array('select player_id, uname, ki from players order by ki desc limit 10');
 		return $res;
 	}
+
+	public static function duped_ips(){
+		return query_array('select uname, player_id, ip from players where ip in (SELECT ip FROM players WHERE active = 1 GROUP  BY ip HAVING count(*) > 1 ORDER BY count(*) ASC limit 30) order by ip');
+	}
+
+
+	// Return one or many
+	public static function split_char_infos($ids){
+		if(is_numeric($ids)){
+			return array(char_info($ids));
+		} else {
+			$res = array();
+			$ids = explode(',', $ids);
+			foreach($ids as $id){
+				$res[$id] = char_info($id);
+			}
+			return $res;
+		}
+	}
+
+	public static function char_inventory($char_id){
+		return inventory_counts($char_id);
+	}
+
+
 }
 
-	$dupes = duped_ips();
 
-	$stats = high_rollers();
+
+
+
+
+
+// Redirect for any non-admins.
+$char_id = self_char_id();
+$self = null;
+if(positive_int($char_id)){
+	$self = new Player($char_id);
+}
+if($self instanceof Player && $self->isAdmin()){
+// Admin possibilities start here.
+
+	$dupes = AdminViews::duped_ips();
+
+	$stats = AdminViews::high_rollers();
+
+	$char_name = in('char-name');
+	debug($char_name, $_REQUEST);
+
+	if(is_string($char_name) && trim($char_name)){
+		$view_char = get_char_id_by_name($char_name);
+	}
 
 	// If a request is made to view a character's info, show it.
-	$view_char = in('view');
-	$char_infos = null;
-	$char_inventory = null;
+	$view_char = in('view')? in('view') : @$view_char;
+	$char_infos = $char_inventory = $message = null;
 	if($view_char){
-		$char_infos = split_char_infos($view_char);
-		$char_inventory = inventory_counts($view_char);
-		//debug($char_inventory);
+		$char_infos = AdminViews::split_char_infos($view_char);
+		$char_inventory = AdminViews::char_inventory($view_char);
+		$message = $char_infos['messages'];
+		unset($char_infos['messages']);
 	}
 
 
 	display_page(
 		'ninjamaster.tpl'	// *** Main Template ***
 		, 'Admin Actions' // *** Page Title ***
-		, array('stats'=>$stats, 'char_infos'=>$char_infos, 'dupes'=>$dupes, 'char_inventory'=>$char_inventory) // *** Page Variables ***
+		, ['stats'=>$stats, 'char_infos'=>$char_infos, 'dupes'=>$dupes, 'char_inventory'=>$char_inventory,
+			'char_name'=>$char_name, 'message'=>$message] // *** Page Variables ***
 	);
 
 
@@ -74,4 +97,3 @@ function split_char_infos($ids){
 
 
 
-?>
