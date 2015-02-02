a7f986e6391be7934f689ef51b0fa74ee54fbce8
Roy Ronalds <tchalvakspam@gmail.com>
2015-02-02 11:18:41 -0500

    Resources: Build resource gets better defaults.

--- deploy/resources.build.php
+++ deploy/resources.build.php
@@ -3,17 +3,17 @@
 * resources.php template. The live version of this file must be included whenever a script is run. 
 * It defines constants used throughout the application.  Constants for tracked files 
 */
-define('DATABASE_USE_PASSWORD', false); // *** Whether to specify password to pdo at all. Generally true only on live
-define('DATABASE_USE_PORT', false); // *** Whether to specify port to pdo at all. Generally true only on live
+define('DATABASE_USE_PASSWORD', true); // *** Whether to specify password to pdo at all. Generally true only on live
+define('DATABASE_USE_PORT', true); // *** Whether to specify port to pdo at all. Generally true only on live
 define('DATABASE_HOST', "servo");		// *** The host to connect to for the database, localhost by default
 define('DATABASE_PORT', "xxxy");		// *** The port number to connect on.
 define('DATABASE_USER', "__DB_USER__");		// *** The user that should connect to the database
 define('DATABASE_PASSWORD', "__DB_PASS__");		// *** The password for the database connection, trust on dev
 define('DATABASE_NAME', "nw");		// *** The name of the database to connect to, nw on dev
 define('OFFLINE', false);				// *** Controls if remote or local resources are used
-define('DEBUG', true);					// *** Shorter debugging constant name, set as false on live.
-define('PROFILE', true);				// *** Whether or not to do performance profiling
-define('DEBUG_ALL_ERRORS', true);	// *** Second debugging level, e.g. email debugging, only works when debug is also on.
+define('DEBUG', false);					// *** Shorter debugging constant name, set as false on live.
+define('PROFILE', false);				// *** Whether or not to do performance profiling
+define('DEBUG_ALL_ERRORS', false);	// *** Second debugging level, e.g. email debugging, only works when debug is also on.
 define('SERVER_ROOT', realpath(__DIR__).'/');		// *** The root deployment directory of the game
 // Generally for the install purposes the SERVER_ROOT should correspond to /srv/ninjawars/deploy/ 
 define('WEB_ROOT', "http://nw.remote/");			// *** The base URL used to access the game, http://www.ninjawars.net on live
@@ -29,7 +29,7 @@ define('ALERTS_EMAIL', SUPPORT_EMAIL);
 define('FACEBOOK_APP_ID', '30479872633'); // Non-confidential id for the facebook app
 define('FACEBOOK_APP_SECRET', 'mooMooIAmACow'); // Secret! string for facebook login auth stuff.
 
-define('TRAP_ERRORS', false); // Whether to use the global error handler & oops page.
+define('TRAP_ERRORS', true); // Whether to use the global error handler & oops page, true on live.
 
 define('TEMPLATE_LIBRARY_PATH', SERVER_ROOT.'vendor/smarty/smarty/distribution/libs/Smarty.class.php'); // Path to Smarty 3
 
