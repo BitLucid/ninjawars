2a1e7342666309637310d4e60b1d6de4b82171e5
Roy Ronalds <tchalvakspam@gmail.com>
2015-03-17 09:48:57 -0400

    Status: Fixed up the last commit message to sync with the latest github api.  Still have to make it use oauth correctly.

--- deploy/www/js/staffPage.js
+++ deploy/www/js/staffPage.js
@@ -1,19 +1,34 @@
 function loadLastCommitMessage(){
-    var login = 'tchalvak' // your username
-        
-    $.getJSON('https://api.github.com/repos/bitlucid/ninjawars/commits/master?callback=?', 
-    	function(data) {
-    		if(!data.data){
-    			console.log('No github commit api data');
-    			console.log(data);
-    			return;
-    		}
-				// Load latest commit message.
-			$('#latest-commit-section').find('#latest-commit')
-			.html(data.data.commit.message)
-			.append("<div id='commit-author'>--"+data.data.commit.author.name+"</div>")
-			.show().end()
-			.find('#latest-commit-title').show();        
-		}
+    var owner = 'BitLucid';
+    var repo = 'ninjawars';
+    var githubUrl = 'https://api.github.com/repos/'+owner+'/'+repo+'/commits/HEAD?callback=?&limit=2';
+    var placeCommit = function(data) {
+        if(!data.data || !data.data.commit){
+            console.log('No github commit api data');
+            console.log(data);
+            return;
+        }
+        // Load latest commit message.
+        $('#latest-commit-section').find('#latest-commit')
+            .html(data.data.commit.message)
+            .append("<div id='commit-author'>--"+data.data.commit.author.name+"</div>")
+            .show().end()
+            .find('#latest-commit-title').show();        
+    };
+
+    function setHeader(xhr) {
+        var oauthToken = ''; // Public url read access.
+        xhr.setRequestHeader('Authorization', 'token '+oauthToken);
+      };
+
+    // https://api.github.com/repos/BitLucid/ninjawars/commits/HEAD
+    $.ajax({
+          url: githubUrl,
+          type: 'GET',
+          dataType: 'json',
+          success: placeCommit,
+          error: $.noop,
+          beforeSend: setHeader
+        }
     );
 }
