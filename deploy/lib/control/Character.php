142cb7b3352634a40d81fffbf9e7a655cd149ee0
Roy Ronalds <tchalvakspam@gmail.com>
2015-04-02 22:11:10 -0400

    Npcs: Commit the errant Character interface file.

---
+++ deploy/lib/control/Character.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Npcs and Players should both implement this interface.
+ *
+ *
+**/
+interface Character{
+	function strength();
+	function speed();
+	function stamina();
+	function max_damage(Character $char=null);
+	function damage(Character $char=null);
+	function health();
+}
