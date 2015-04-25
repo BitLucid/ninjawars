c8d0023256d08373a6d19d2f75917c2e7eb3dc74
Roy Ronalds <tchalvakspam@gmail.com>
2015-04-25 11:26:26 -0400

    Check: Also includes base.inc.php and vendors/autoload.php

--- deploy/check.php
+++ deploy/check.php
@@ -10,6 +10,9 @@ assert('' !== WEB_ROOT);
 assert('http:///' !== WEB_ROOT);
 $out = ob_get_contents();
 ob_end_clean();
+// How about some includes
+require(SERVER_ROOT.'core/base.inc.php');
+require_once(VENDOR_ROOT.'autoload.php');
 // Reverse return values
 if($out){
     return 1;
