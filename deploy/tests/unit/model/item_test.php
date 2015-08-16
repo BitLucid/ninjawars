77288a5389103e803d4b733335c4cd38e3e4fbab
Roy Ronalds <tchalvakspam@gmail.com>
2015-08-16 00:32:46 -0400

    Tests: Moved item tests into proper location.

--- deploy/tests/unit/model/item_test.php
+++ deploy/tests/unit/model/item_test.php
@@ -66,7 +66,44 @@ class Item_Test extends PHPUnit_Framework_TestCase {
 		$this->assertGreaterThan(0, $this->itemRandomDamageSum(new Item('tetsubo')));
 	}
 
-	public function testShurikenHasSomeMaxDamage(){
+    public function testThatAShurikenHasSliceEffect(){
+        $item = new Item('shuriken');
+        $this->assertGreaterThan(0, $item->hasEffect('slice'));
+    }
+
+    public function testThatAShurikenIsUsableOnOthers(){
+        $item = new Item('shuriken');
+        $this->assertTrue($item->isOtherUsable());
+    }
+
+    public function testThatShurikenDoesNotIgnoreStealth(){
+        $item = new Item('shuriken');
+        $this->assertFalse($item->ignoresStealth());
+    }
+
+    public function testThatShurikensArentSelfUsable(){
+        $item = new Item('shuriken');
+        $this->assertFalse($item->isSelfUsable());
+    }
+
+    public function testThatCaltropsIdeallyCauseNegativeTurnChange(){
+        $item = new Item('caltrops');
+        $this->assertLessThan(0, $item->getMaxTurnChange());
+    }
+
+    public function testBuffItemsIgnoreStealth(){
+        foreach(['mirror', 'prayerwheel', 'shell', 'lantern'] as $ident){
+            $item = new Item($ident);
+            $this->assertTrue($item->ignoresStealth());
+        }
+    }
+
+    public function testMeitoNamedKatanaIgnoresStealth(){
+        $item = new Item('meito');
+        $this->assertTrue($item->ignoresStealth());
+    }
+
+	public function testShurikenHasSomeMaxDamageWhenTargetted(){
 		$shuriken = new Item('shuriken');
 		$this->assertGreaterThan(0, $shuriken->getMaxDamage(new Player()));
 	}
