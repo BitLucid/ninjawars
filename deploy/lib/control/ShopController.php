55a574b1e0af3d567758969283cee062e0a80fb5
Roy Ronalds <tchalvakspam@gmail.com>
2015-10-12 00:52:57 -0400

    ShopController: Moved into it's own file.

---
+++ deploy/lib/control/ShopController.php
@@ -0,0 +1,101 @@
+<?php
+namespace app\Controller;
+
+
+class ShopController { // extends Controller
+	public static $alive   = true;  // *** must be alive to access the shop ***
+	public static $private = false; // *** do not need to be logged in ***
+
+	protected $itemCosts   = [];
+	protected $sessionData = [];
+
+	public function __construct() {
+		$this->itemCosts   = item_for_sale_costs();
+		$this->sessionData = [
+			'username'         => self_name(),
+			'char_id'          => self_char_id(),
+			'is_logged_in'     => is_logged_in(),
+			'quantity_setting' => get_setting('items_quantity'),
+		];
+	}
+
+	public function index() {
+		$parts = array(
+			'quantity'  => $this->sessionData['quantity_setting'],
+			'view_part' => 'index',
+		);
+
+		$this->render($parts);
+	}
+
+	public function buy() {
+		$in_quantity       = in('quantity');
+		$in_item           = in('item');
+
+		$gold = get_gold($this->sessionData['char_id']);
+
+		$current_item_cost = 0;
+		$no_funny_business = false;
+
+		// Pull the item info from the database
+		$item_costs        = item_for_sale_costs();
+		$item              = getItemByID(item_id_from_display_name($in_item));
+
+		if ($item) {
+			$purchaseOrder = new PurchaseOrder();
+
+			// Determine the quantity from input, or settings, or as a fallback, default of 1.
+			$purchaseOrder->quantity = whichever(positive_int($in_quantity), $this->sessionData['quantity_setting'], 1);
+			$purchaseOrder->item     = $item;
+
+			$potential_cost    = (isset($item_costs[$purchaseOrder->item->identity()]['item_cost']) ? $item_costs[$purchaseOrder->item->identity()]['item_cost'] : null);
+			$current_item_cost = first_value($potential_cost, 0);
+			$current_item_cost = $current_item_cost * $purchaseOrder->quantity;
+
+			if (!$this->sessionData['char_id'] || !$purchaseOrder->item || $purchaseOrder->quantity < 1) {
+				$no_funny_business = true;
+			} else if ($gold >= $current_item_cost) { // Has enough gold.
+				try {
+					add_item($this->sessionData['char_id'], $purchaseOrder->item->identity(), $purchaseOrder->quantity);
+					subtract_gold($this->sessionData['char_id'], $current_item_cost);
+				} catch (Exception $e) {
+					$invalid_item = $e->getMessage();
+					error_log('Invalid Item attempted :'.$invalid_item);
+					$no_funny_business = true;
+				}
+			}
+		}
+
+		set_setting('items_quantity', $purchaseOrder->quantity);
+
+		$parts = array(
+			'current_item_cost' => $current_item_cost,
+			'quantity'          => $purchaseOrder->quantity,
+			'item_text'         => ($purchaseOrder->quantity > 1 ? $item->getPluralName() : $item->getName()),
+			'no_funny_business' => $no_funny_business,
+			'view_part'         => 'buy',
+		);
+
+		$this->render($parts);
+	}
+
+	public function render($p_parts) {
+		$p_parts['gold']         = get_gold($this->sessionData['char_id']);
+		$p_parts['item_costs']   = $this->itemCosts;
+		$p_parts['is_logged_in'] = $this->sessionData['is_logged_in'];
+
+		display_page(
+			'shop.tpl' // *** Main Template ***
+			, 'Shop'          // *** Page Title ***
+			, $p_parts        // *** Page Variables ***
+			, array(          // *** Page Options ***
+				'quickstat' => 'viewinv'
+			)
+		);
+	}
+}
+
+class PurchaseOrder {
+	public $quantity;
+	public $item;
+}
