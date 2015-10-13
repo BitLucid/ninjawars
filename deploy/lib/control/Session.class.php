cb35db311ed88bac410fb01c67bf2b39c6697d8c
Roy Ronalds <tchalvakspam@gmail.com>
2015-10-13 17:23:23 -0400

    Session: Session is now a wrapped static for unit testing.

--- deploy/lib/control/Session.class.php
+++ deploy/lib/control/Session.class.php
@@ -2,6 +2,7 @@
 // *** The session class from the comments in http://us3.php.net/session_start, providing static methods that abstract common session usage patterns. ***
 class SESSION
 {
+	private static $session = null;
 	public function __construct()
 	{
 		self::commence();
@@ -10,17 +11,27 @@ class SESSION
 	// *** Starts the session whenever a method is called for. ***
 	public static function commence()
 	{
-		if (session_id() == '')
-		{
-			session_start();
-			$_SESSION['ready'] = TRUE;
+		if(!isset(static::$session['ready']) || !static::$session['ready']){
+			if (session_id() == ''){
+				session_start();
+				static::$session = $_SESSION;
+				static::$session['ready'] = TRUE;
+			}
 		}
+	}
+
+	/**
+	 * Inject a new session of arbitrary data, scary.
+	**/
+	public static function inject($session){
+		static::$session = $session;
+		static::$session['ready'] = TRUE;
 	}
 
 	public static function set($field, $val)
 	{
 		self::commence();
-		$_SESSION[$field] = $val;
+		static::$session[$field] = $val;
 	}
 
 	// *** Set a session id if it doesn't exist yet. ***
@@ -38,33 +49,34 @@ class SESSION
 	public static function un_set($field)
 	{
 		self::commence();
-		unset($_SESSION[$field]);
+		unset(static::$session[$field]);
 	}
 
 	public static function destroy()
 	{
 		self::commence();
-		unset($_SESSION);
-		session_destroy();
+		static::$session = null;
+		if(session_id() === null){
+			session_destroy();
+		}
 		return true;
 	}
 
 	public static function get($field)
 	{
 		self::commence();
-		return (isset($_SESSION[$field]) ? $_SESSION[$field] : null);
+		return (isset(static::$session[$field]) ? static::$session[$field] : null);
 	}
 
 	public static function has_values()
 	{
 		self::commence();
-		return (count($_SESSION) > 0);
+		return (count(static::$session) > 0);
 	}
 
 	public static function is_set($field)
 	{
 		self::commence();
-		return isset($_SESSION[$field]);
+		return isset(static::$session[$field]);
 	}
 }
-?>
