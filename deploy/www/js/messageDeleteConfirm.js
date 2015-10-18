a43a6a6e77cde8df500c8ecf0f8fcfe3eb0fc5b7
Al Vazquez <al.vazquez@gmail.com>
2015-10-18 13:43:47 -0400

    Fix style/clarity in messageDeleteConfirm.js

--- deploy/www/js/messageDeleteConfirm.js
+++ deploy/www/js/messageDeleteConfirm.js
@@ -1,6 +1,12 @@
-$(document).ready(function (){
-    $('#delete-messages a').click(function(){
-      var answer = confirm('Delete all messages?');
-      return answer // answer is a boolean
-    }).hover(function () {$(this).css({'background-color':'red'})}, function () {$(this).css({'background-color':'inherit'})}); 
+$(document).ready(function () {
+	$('#delete-messages a').click(function() {
+		return confirm('Delete all messages?'); // *** boolean return value ***
+	}).hover(
+		function () {
+			$(this).css({'background-color':'red'});
+		},
+		function () {
+			$(this).css({'background-color':'inherit'});
+		}
+	);
 });
