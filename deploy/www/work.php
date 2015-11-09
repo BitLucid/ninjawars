529fd9196e05e0fa34f7f8026e2cc1b015801fbe
Roy Ronalds <tchalvakspam@gmail.com>
2015-11-09 12:31:57 -0500

    Work: Refactor the work page into a TODO controller.

--- deploy/www/work.php
+++ deploy/www/work.php
@@ -7,6 +7,112 @@ if ($error = init($private, $alive)) {
 	display_error($error);
 } else {
 
+/**
+ * The controller for effects of a work request and the default index display of the page and initial form
+**/
+class WorkController{
+
+	/**
+	 * Take in a url parameter of work and try to convert it to gold
+	 * 
+	**/
+	public function requestWork(){
+		// Initialize variables to pass to the template.
+		$work_multiplier        = 30;
+		$worked                 = null;
+		$new_gold               = null;
+		$not_enough_energy      = null;
+		$use_second_description = null;
+		$recommended_to_work    = null;
+		$is_logged_in           = is_logged_in();
+
+
+		$worked = intval(in('worked')); // An amount of work requested
+
+		// TODO: refactor & move the rest of the code from the body of work.php and get it working without error here
+		// For now, just throw a debug error:
+		debug('ERROR: TODO: Fill in the functionality of the requestWork action');
+
+		$response = [];
+		$response['parts'] = [
+			'recommended_to_work'=>$recommended_to_work, 
+			'work_multiplier'=>$work_multiplier,
+			'is_logged_in'=>$is_logged_in,
+			'gold_display'=>$gold_display,
+			'worked'=>$worked,
+			'new_gold'=>$new_gold,
+			'not_enough_energy'=>$not_enough_energy,
+			'use_second_description'=>$use_second_description
+			];
+		$response['template'] = 'work.tpl';
+		$response['title'] = 'Working in the Village';
+		$response['options'] = ['quickstat' => 'player'];
+		return $response;
+	}
+
+	/**
+	 * Get the last turns worked by a pc, and pass it to display the default page with form
+	**/
+	public function index(){
+		// Initialize variables to pass to the template.
+		$work_multiplier        = 30;
+		$worked                 = null;
+		$new_gold               = null;
+		$not_enough_energy      = null;
+		$use_second_description = null;
+		$is_logged_in           = is_logged_in();
+
+
+		// Fill out some of the variables.
+		$char_id = self_char_id();
+		$player_char = new Player($char_id);
+		$last_worked = get_setting('turns_worked');
+		$recommended_to_work = $last_worked? $last_worked : 10;
+		$gold  = get_gold($char_id); // Get the current/final gold.
+		$gold_display = number_format($gold);
+
+		$response = [];
+		$response['parts'] = [
+			'recommended_to_work'=>$recommended_to_work, 
+			'work_multiplier'=>$work_multiplier,
+			'is_logged_in'=>$is_logged_in,
+			'gold_display'=>$gold_display,
+			'worked'=>$worked,
+			'new_gold'=>$new_gold,
+			'not_enough_energy'=>$not_enough_energy,
+			'use_second_description'=>$use_second_description
+			];
+		$response['template'] = 'work.tpl';
+		$response['title'] = 'Working in the Village';
+		$response['options'] = ['quickstat' => 'player'];
+		return $response;
+	}
+
+}
+
+$command = in('command');
+
+$controller = new WorkController();
+
+// Switch between the different controller methods.
+switch(true){
+	case($_SERVER['REQUEST_METHOD'] == 'POST' && $command='request_work'):
+		$response = $controller->requestWork();
+	break;
+	case($command = 'index'):
+	default:
+		$command = 'index';
+		$response = $controller->index();
+	break;
+}
+
+// Display the page with the template, title or header vars, template parts, and page options
+display_page($response['template'], $response['title'], $response['parts'], $response['options']);
+
+
+} // End of the no-error area
+/*
+
 $work_multiplier        = 30;
 $worked                 =
 $new_gold               =
@@ -55,3 +161,5 @@ display_page(
 	)
 );
 }
+
+*/
