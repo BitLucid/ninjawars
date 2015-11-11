161c7ab693bbda4583111256f8493a240521d01c
Ner Atillo <atilloregner@gmail.com>
2015-11-11 22:39:07 +0800

    Village: refactor village page

--- deploy/www/village.php
+++ deploy/www/village.php
@@ -1,63 +1,40 @@
 <?php
-$private = false;
-$alive   = false;
+require_once(CORE.'control/VillageController.php');
 
-if ($error = init($private, $alive)) {
+use app\Controller\VillageController;
+
+if ($error = init(VillageController::PRIV, VillageController::ALIVE)) {
 	display_error($error);
-} else {
-
-require_once(LIB_ROOT."control/lib_chat.php"); // Require all the chat helper and rendering functions.
-
-$default_limit = 200;
-$field_size    = 40;
-$chatlength    = in('chatlength', $default_limit, 'toInt');
-$message       = in('message', null, 'no filter'); // Essentially no filtering.
-$view_all      = in('view_all');
-$command       = in('command');
-$sentMessage   = in('message');
-$sent          = false;
-$user_id       = self_char_id();
-$target        = $_SERVER['PHP_SELF'];
-$channel       = 1;
-
-$chatlength = min(3000, max(30, $chatlength)); // Min 30, max 3000
-
-// Take in a chat and record it to the database.
-if ($user_id) {
-	if ($command == "postnow" && $message) {
-		send_chat($user_id, $message);
-		redirect('/village.php');
-	}
+	die();
 }
 
-// Output section.
+$command = in('command');
+$controller = new VillageController();
 
-$message_count = get_chat_count();
-$chats = get_chats(($view_all? null : $chatlength)); // Limit by chatlength unless a request to view all came in.
-$chats = $chats->fetchAll();
-$more_chats_to_see = (count($chats)<$message_count? true : null);
+// Switch between the different controller me/**thods.
+switch(true){
+	case($_SERVER['REQUEST_METHOD'] == 'POST' && self_char_id()
+		&& $command=='postnow'):
+		$controller->postnow();
+		exit();
+		break;
+	case($command = 'index'):
+	default:
+		$command = 'index';
+		$response = $controller->index();
+		break;
+}
 
-$parts = get_certain_vars(get_defined_vars(), array('chats'));
+// TODO: register plugin time_ago globally and call display_page function instead?
+$template = prep_page($response['template'], $response['title'], $response['parts'], $response['options']);
 
 function get_time_ago($p_params, &$tpl) {
 	return time_ago($p_params['ago'], $p_params['previous_date']);
 }
-
-$template = prep_page(
-	'village.tpl'
-	, 'Chat Board'
-	, $parts
-	, array (
-		'quickstat' => false
-	)
-);
 
 //$template->register_function('time_ago', 'get_time_ago');
 $template->registerPlugin("function","time_ago", "get_time_ago");
 
 $template->fullDisplay();
 
-
-
-} // End of player-is-live so no error block
 
