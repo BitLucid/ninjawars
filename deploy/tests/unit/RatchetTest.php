3e898748813ddda545867be62ee5dcc0bdaa4935
Roy Ronalds <tchalvakspam@gmail.com>
2015-11-12 20:20:34 -0500

    Tests:Ratchet: Set trigger to update ratchet if it, drops enough.

--- deploy/tests/unit/RatchetTest.php
+++ deploy/tests/unit/RatchetTest.php
@@ -14,11 +14,13 @@ class RatchetTest extends PHPUnit_Framework_TestCase {
 	public function testWwwScriptsLimit(){
 		$it = new RegexIterator(new DirectoryIterator(ROOT.'www/'), "/\\.php\$/i");
 		$this->assertLessThanOrEqual(static::MAX_WWW_SCRIPTS, iterator_count($it));
+		$this->assertGreaterThanOrEqual(round(static::MAX_WWW_SCRIPTS*0.95), iterator_count($it)); // Change ratchet if this trips
 	}
 
 	public function testNinjamasterScriptsLimit(){
 		$it = new RegexIterator(new DirectoryIterator(ROOT.'www/ninjamaster/'), "/\\.php\$/i");
 		$this->assertLessThanOrEqual(static::MAX_NINJAMASTER_SCRIPTS, iterator_count($it));
+		$this->assertGreaterThanOrEqual(static::MAX_NINJAMASTER_SCRIPTS, iterator_count($it)); // Change ratchet if this trips
 	}
 	
 }
