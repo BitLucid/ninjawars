631c8a9d8b88f461396d528c63ddf8aad9934120
Roy Ronalds <tchalvakspam@gmail.com>
2015-12-15 11:28:02 -0500

    Enemies: Now updates quickstats. ConsiderController: Split out of the enemies.php script.

---
+++ deploy/lib/control/ConsiderController.php
@@ -0,0 +1,117 @@
+<?php
+namespace app\combat;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+
+
+/**
+ * Display ninja & monsters to potentially pick fights with
+**/
+class ConsiderController{
+
+	const ENEMY_LIMIT = 20;
+
+	public function __construct(){
+	}
+
+	public function index(){
+		return $this->render($this->configure());
+	}
+
+	private function configure(){
+		$char = new \Player(self_char_id());
+		// Array that simulates database display information for switching out for an npc database solution.
+		$npcs = array(
+			  array('name'=>'Peasant',        'identity'=>'peasant', 'image'=>'fighter.png')
+			, array('name'=>'Thief',          'identity'=>'thief', 'image'=>'thief.png')
+			, array('name'=>'Merchant',       'identity'=>'merchant', 'image'=>'merchant.png')
+			, array('name'=>"Guard",          'identity'=>'guard', 'image'=>'guard.png')
+			, array('name'=>'Samurai',        'identity'=>'samurai', 'image'=>'samurai.png')
+		);
+
+
+		$char_name = $char->name();
+
+		$peers = nearby_peers($char->id());
+
+		$active_ninjas = get_active_players(5, true); // Get the currently active ninjas
+
+		$char_info = self_info();
+
+		// Generic/abstracted npcs
+		$other_npcs = \NpcFactory::npcsData();
+
+		$enemy_list = get_current_enemies();
+		$enemy_count = rco($enemy_list);
+		$recent_attackers = get_recent_attackers($char);
+
+		if (self::ENEMY_LIMIT <= $enemy_count) {
+			$max_enemies = true;
+		}
+
+		return [
+			'logged_in'=>(bool)$char->id(),
+			'enemy_list'=>$enemy_list,
+			'enemy_count'=>$enemy_count,
+			'char_name'=>$char->name(),
+			'npcs'=>$npcs,
+			'other_npcs'=>$other_npcs,
+			'char_info'=>$char_info,
+			'active_ninjas'=>$active_ninjas,
+			'recent_attackers'=>$recent_attackers,
+			'enemy_list'=>$enemy_list,
+			'peers'=>$peers];
+	}
+
+	/**
+	 * Search for enemies to remember.
+	**/
+	public function search(){
+		$enemy_match = in('enemy_match');
+		$found_enemies = $enemy_match? get_enemy_matches($enemy_match) : null;
+		$parts = $this->configure();
+		// Add some additional parts
+		$parts = array_merge($parts, [
+			 'found_enemies'=>$found_enemies,
+			 'enemy_match'=>$enemy_match
+			]);
+		return $this->render($parts);
+	}
+
+	/**
+	 * Render the parts, since the template is always currently the same.
+	 **/
+	public function render($parts){
+		return [
+			'template'=>'enemies.tpl',
+			'title'=>'Fight',
+			'parts'=>$parts,
+			'options'=>['quickstat'=>false]
+			];
+	}
+
+	/**
+	 * Add an enemy to pc's list if valid.
+	**/
+	public function addEnemy(){
+		$add_enemy    = in('add_enemy', null, 'toInt');
+		if (is_numeric($add_enemy) && $add_enemy != 0) {
+			add_enemy($add_enemy);
+		}
+		return new RedirectResponse('enemies.php');
+	}
+
+	/**
+	 * Take an enemy off a pc's list.
+	**/
+	public function deleteEnemy(){
+		$remove_enemy = in('remove_enemy', null, 'toInt');
+		if (is_numeric($remove_enemy) && $remove_enemy != 0) {
+			remove_enemy($remove_enemy);
+		}
+		return new RedirectResponse('enemies.php');
+	}
+
+
+}
