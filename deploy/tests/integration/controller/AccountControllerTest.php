414d2d42331f32bb216a2c0cbdeec07809791281
Al Vazquez <al.vazquez@gmail.com>
2015-12-19 10:54:03 -0500

    Enable existing AccountController test stubs

--- deploy/tests/integration/controller/AccountControllerTest.php
+++ deploy/tests/integration/controller/AccountControllerTest.php
@@ -1,29 +1,28 @@
 <?php
 require_once(CORE.'control/AccountController.php');
+
 use Symfony\Component\HttpFoundation\Request;
 use app\environment\RequestWrapper;
 use app\Controller\AccountController as AccountController;
-
+use Symfony\Component\HttpFoundation\Session\Storage\MockArraySessionStorage;
 
 class AccountControllerTest extends PHPUnit_Framework_TestCase {
-
-
-	public function setUp(){
+	public function setUp() {
+		nw\SessionFactory::init(new MockArraySessionStorage());
+		nw\SessionFactory::getSession()->set('player_id', 128274);
 	}
-	
-	public function tearDown(){
+
+	public function tearDown() {
     }
 
-    public function testAccountControllerCanInstantiateWithoutError(){
+    public function testAccountControllerCanInstantiateWithoutError() {
         $controller = new AccountController();
         $this->assertInstanceOf('app\Controller\AccountController', $controller);
     }
-    
-    public function testAccountControllerIndexRuns(){
+
+    public function testAccountControllerIndexRuns() {
         $controller = new AccountController();
-        $this->markTestIncomplete('Need to be able to mock or inject session for AccountControllerTest.');
         $response = $controller->index();
         $this->assertNotEmpty($response);
     }
 }
-
