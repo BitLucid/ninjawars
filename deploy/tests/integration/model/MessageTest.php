1a79790102507f032a80945f9aec943ef915098a
Al Vazquez <al.vazquez@gmail.com>
2015-12-27 15:56:29 -0500

    Simplify and cleanup Message model tests

--- deploy/tests/integration/model/MessageTest.php
+++ deploy/tests/integration/model/MessageTest.php
@@ -4,91 +4,101 @@ require_once(CORE.'data/Message.php');
 
 use app\data\Message;
 
-
 class TestMessage extends PHPUnit_Framework_TestCase {
+    private $char_id;
+    private $char_id_2;
+    private $messageData;
 
+    function setUp() {
+        $this->char_id = TestAccountCreateAndDestroy::char_id(true);
+        $this->char_id_2 = TestAccountCreateAndDestroy::char_id_2(true);
 
-	function setUp(){
-		$this->char_id = TestAccountCreateAndDestroy::char_id($confirm=true);
-		$this->char_id_2 = TestAccountCreateAndDestroy::char_id_2($confirm=true);
-		$this->message = Message::create(['message'=>'Random phpunit Test Message', 'send_to'=>$this->char_id, 'send_from'=>$this->char_id_2, 'unread'=>1, 'type'=>null]);
-		$this->delete_this_message = null;
-	}
-	
-	function tearDown(){
-		TestAccountCreateAndDestroy::destroy();
-		$this->message->delete();
+        $this->messageData = [
+            'message'   => 'Random phpunit Test Message',
+            'send_from' => $this->char_id,
+            'send_to'   => $this->char_id_2,
+            'unread'    => 1,
+            'type'      => 0
+        ];
     }
 
-    public function testMessageCanInstantiate(){
+    function tearDown() {
+        TestAccountCreateAndDestroy::destroy();
+    }
+
+    public function testMessageCanInstantiate() {
         $mess = new Message();
         $this->assertTrue($mess instanceof Message);
     }
 
-    public function testMessageCanBeSent(){
-        $text = 'This is some kind of random message';
-        $char = new Player($this->char_id);
-        $mess = Message::create(['send_from'=>$char->id(), 'send_to'=>$this->char_id_2, 'message'=>$text, 'type'=>0]);
-        $this->assertEquals($text, $mess->message);
+    public function testMessageCanBeSent() {
+        $mess = Message::create($this->messageData);
+        $this->assertEquals($this->messageData['message'], $mess->message);
     }
 
-    public function testMessageCanBeReceived(){
-        $text = 'This is some kind of random message';
-        $char = new Player($this->char_id);
-        $mess = Message::create(['send_from'=>$char->id(), 'send_to'=>$this->char_id_2, 'message'=>$text, 'type'=>0]);
-        $messages = Message::findByReceiver(new Player($this->char_id_2))->all();
+    public function testMessageCanBeReceived() {
+        $mess = Message::create($this->messageData);
         $first_message = Message::find($mess->id());
-        $this->assertEquals($text, $first_message->message);
+        $this->assertEquals($this->messageData['message'], $first_message->message);
     }
 
-    public function testMessageCanBeSentToGroup(){
-        $text = 'Text of a group message to clan or whatever';
-        $char = new Player($this->char_id);
-        $mess = Message::create(['send_from'=>$char->id(), 'send_to'=>$this->char_id_2, 'message'=>$text, 'type'=>1]);
+    public function testMessageCanBeSentToGroup() {
+        $this->messageData['type'] = 1;
+
+        $mess = Message::create($this->messageData);
+        $messages = Message::findByReceiver(new Player($this->char_id_2), 1);
+        $first_message = $messages->first();
+
         $this->assertTrue($mess instanceof Message);
-        $messages = Message::findByReceiver(new Player($this->char_id_2), $type=1);
         $this->assertGreaterThan(0, count($messages), 'Message array should have some elements');
-        $first_message = $messages->first();
         $this->assertTrue($first_message instanceof Message);
-        $this->assertEquals($text, $first_message->message);
+        $this->assertEquals($this->messageData['message'], $first_message->message);
     }
 
-    public function testMessageHasARobustSender(){
-        $rec = new Player($this->char_id);
-        Message::create(['send_from'=>$rec->id(), 'send_to'=>$this->char_id_2, 'message'=>'Random phpunit test message of some content', 'type'=>0]);
-        $messages = Message::findByReceiver(new Player($this->char_id_2), $type=0, $limit=1000, $offset=0);
-        $this->assertGreaterThan(0, count($messages), 'Collection has no results found');
+    public function testMessageHasARobustSender() {
+        Message::create($this->messageData);
+        $messages = Message::findByReceiver(new Player($this->char_id_2), 0, 1000, 0);
         $first_message = $messages->first();
+
+        $this->assertGreaterThan(0, count($messages), 'Collection has no results found');
         $this->assertTrue($first_message instanceof Message, 'First message not a valid message model');
         $this->assertNotEmpty($first_message->sender);
         $this->assertGreaterThan(0, strlen($first_message->sender));
     }
 
-    public function testCreateMessageViaMassAssignment(){
-    	$mess = Message::create(['message'=>'Random phpunit test message', 'send_to'=>$this->char_id, 
-                'send_from'=>$this->char_id_2, 'unread'=>1]);
-    	$text = 'Updated phpunit test message';
-    	$mess->message = $text;
-    	$mess->save(); // Save the newly updated message
-    	$id = $mess->id();
-    	$retrieved_message = Message::find($id);
-    	$retrieved_text = $retrieved_message->message;
-    	$retrieved_message->delete();
-    	$this->assertEquals($text, $retrieved_text);
+    public function testCreateMessageViaMassAssignment() {
+        $this->messageData['send_to']   = $this->char_id;
+        $this->messageData['send_from'] = $this->char_id_2;
+
+        $mess = Message::create($this->messageData);
+
+        $text = 'Updated phpunit test message';
+        $mess->message = $text;
+        $mess->save(); // Save the newly updated message
+
+        $retrieved_message = Message::find($mess->id());
+        $retrieved_text = $retrieved_message->message;
+        $retrieved_message->delete();
+        $this->assertEquals($text, $retrieved_text);
     }
 
-    public function testFindPrivateMessagesForACertainChar(){
-        $i = 4;
-        while($i--){
-            Message::create(['message'=>'Random phpunit test message'.$i, 'send_to'=>$this->char_id, 
-                'send_from'=>$this->char_id_2, 'unread'=>1]);
+    public function testFindPrivateMessagesForACertainChar() {
+        $messageCount = 4;
+
+        $this->messageData['send_to']   = $this->char_id;
+        $this->messageData['send_from'] = $this->char_id_2;
+
+        for ($count = 0; $count < $messageCount; $count++) {
+            $this->messageData['message'] = 'Random phpunit test message'.$count;
+            Message::create($this->messageData);
         }
+
         $char = new Player($this->char_id);
+
         $messages = Message::findByReceiver($char)->all();
-        $this->assertEquals(4, count($messages));
-        Message::deleteByReceiver($char, $type=0);
+        $this->assertEquals($messageCount, count($messages));
+
+        Message::deleteByReceiver($char, 0);
         $this->assertEquals(0, Message::countByReceiver($char));
     }
-
 }
-
