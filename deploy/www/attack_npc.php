235578a6b61ede063f7e1fbb9cea73bfba3d5772
Roy Ronalds <tchalvakspam@gmail.com>
2015-12-28 16:33:15 -0500

    Attack_npc.php: Script removed.

--- deploy/www/attack_npc.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Deprecated script for ferretting out continued bad url usages.
-error_log('Deprecated linking to attack_npc url performed from referrer: '.$_SERVER['HTTP_REFERER']);
-$query = $_SERVER['QUERY_STRING'];
-$new_url = 'npc.php?'.$query;
-permanent_redirect($new_url);
