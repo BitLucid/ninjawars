11fa1c5146558f1dc34eaba5d64139351f223d1c
Al Vazquez <al.vazquez@gmail.com>
2015-12-31 12:19:17 -0500

    Move kill funcs from commands to lib_player

--- deploy/lib/control/lib_player.php
+++ deploy/lib/control/lib_player.php
@@ -446,3 +446,94 @@ function changeLevel($who, $amount) {
 
 	return getLevel($who);
 }
+
+// Takes in a character id and adds kills to that character.
+function addKills($who, $amount) {
+    $amount = (int)abs($amount);
+    update_levelling_log($who, $amount);
+    return change_kills($who, $amount);
+}
+
+function subtractKills($who, $amount) {
+    $amount = (int)abs($amount);
+    update_levelling_log($who, -1*($amount));
+    return change_kills($who, -1*($amount));
+}
+
+function get_kills($char_id) {
+    return query_item(
+        "SELECT kills FROM players WHERE player_id = :player_id",
+        [
+            ':player_id' => [$char_id, PDO::PARAM_INT]
+        ]
+    );
+}
+
+// Change the kills amount of a char, and levels them up when necessary.
+function change_kills($char_id, $amount, $auto_level_check=true) {
+    $amount = (int)$amount;
+
+    if (abs($amount) > 0) {
+        // Ignore changes that amount to zero.
+        if ($amount > 0 && $auto_level_check) {
+            // For positive kill changes, check whether levelling occurs.
+            level_up_if_possible($char_id, true);
+        }
+
+        $query = <<<EOT
+UPDATE players
+SET kills = kills +
+CASE WHEN kills + :amount1 < 0 THEN kills*(-1) ELSE :amount2 END
+WHERE player_id = :player_id
+EOT;
+
+        query($query,
+            [
+                ':amount1'   => [$amount, PDO::PARAM_INT],
+                ':amount2'   => [$amount, PDO::PARAM_INT],
+                ':player_id' => $char_id
+            ]
+        );
+    }
+
+    return get_kills($char_id);
+}
+
+// Update the levelling log with the increased kills.
+function update_levelling_log($who, $amount) {
+    // TODO: This should be deprecated once we have only upwards kills_total increases, but for now I'm just refactoring.
+    DatabaseConnection::getInstance();
+
+    $amount = (int)$amount;
+
+    if ($amount == 0) {
+        return;
+    } else if ($amount > 0) {
+        $record_check = '>';
+        $add = false;
+    } else {
+        $record_check = '<';
+        $add = true;
+    }
+
+    // *** UPDATE THE KILLS LOG ***
+    $statement = DatabaseConnection::$pdo->prepare(
+        "SELECT * FROM levelling_log WHERE _player_id = :player AND killsdate = now() AND killpoints $record_check 0 LIMIT 1");
+    //Check for an existing record of either negative or positive types.
+    $statement->bindValue(':player', $who);
+    $statement->execute();
+
+    $notYetANewDay = $statement->fetch();  //positive if todays record already exists
+    if ($notYetANewDay != NULL) {
+        // If an entry already exists, update it.
+        $statement = DatabaseConnection::$pdo->prepare("UPDATE levelling_log SET killpoints = killpoints + :amount WHERE _player_id = :player AND killsdate = now() AND killpoints $record_check 0");  //increase killpoints
+    } else {
+        $statement = DatabaseConnection::$pdo->prepare(
+            "INSERT INTO levelling_log (_player_id, killpoints, levelling, killsdate) VALUES (:player, :amount, '0', now())");
+        //create a new record for today
+    }
+
+    $statement->bindValue(':amount', $amount);
+    $statement->bindValue(':player', $who);
+    $statement->execute();
+}
