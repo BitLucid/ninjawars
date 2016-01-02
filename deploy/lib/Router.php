c78cc64575eed74719057f8c9f3a2b6ec0f4b656
Al Vazquez <al.vazquez@gmail.com>
2016-01-02 16:09:31 -0500

    Move routing logic to Router, gut front-controller

--- deploy/lib/Router.php
+++ deploy/lib/Router.php
@@ -1,7 +1,34 @@
 <?php
 namespace app\Core;
 
+require_once(LIB_ROOT."control/lib_inventory.php");
+require_once(CORE.'control/ClanController.php');
+require_once(CORE."control/ShopController.php");
+require_once(CORE."control/CasinoController.php");
+require_once(CORE.'control/WorkController.php');
+require_once(CORE.'control/MessagesController.php');
+require_once(CORE.'control/ShrineController.php');
+
+use app\Controller\ShopController;
+use app\Controller\ClanController;
+use app\Controller\CasinoController;
+use app\Controller\WorkController;
+use app\Controller\MessagesController;
+use app\Controller\ShrineController;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+/**
+ * Router/front-controller for NinjaWars
+ *
+ * By default, routes of the form /controller/action are mapped to
+ * Controller->action(). Overrides are defined here in the $routes member.
+ */
 class Router {
+    /*
+     * Adding a default entry helps for when the action is not found or not
+     * provided, otherwise a 404 will occur.
+     */
     public static $routes = [
         'clan' => [
             'new'     => 'create',
@@ -32,7 +59,151 @@ class Router {
         ],
     ];
 
-    public static function all() {
-        return self::$routes;
+    /**
+     * Breaks the request URI into an array of route segments
+     *
+     * @param Request The request object being routed
+     * @return array Each segment of the route
+     *
+     * @note
+     * If there is only 1 route segment and the special input parameter
+     * "command" is set in the request, the value of "command" will be returned
+     * in as the second element of the returned array
+     *
+     * @todo remove the call to in() and replace with something from request
+     * @todo stop supporting ?command=action
+     */
+    public static function parseRoute($p_request) {
+        $pathInfo = $p_request->getPathInfo();
+
+        // split the requested path by slash
+        $routeSegments = explode('/', trim($pathInfo, '/'));
+
+        // if there are 2 route segments use the second one
+        if (!isset($routeSegments[1])) {
+            $routeSegments[1] = (string)in('command');
+        }
+
+        if (empty($routeSegments[0])) {
+            $routeSegments[0] = 'index.php';
+        }
+
+        return $routeSegments;
+    }
+
+    /**
+     * Takes a string and returns a fully qualified controller classname
+     *
+     * @param string $p_main The token to turn into a classname
+     * @return string A fully qualified controller classname
+     */
+    public static function buildClassName($p_main) {
+        return "app\\Controller\\".ucfirst($p_main)."Controller";
+    }
+
+    /**
+     * Removes a trailing ".php" from the inputted string
+     *
+     * @param string $p_main
+     * @return string
+     */
+    public static function sanitizeMainRoute($p_main) {
+        if (stripos($p_main, '.php') === (strlen($p_main) - 4)) {
+            $p_main = substr($p_main, 0, -4);
+        }
+
+        return $p_main;
+    }
+
+    /**
+     * Runs the requested route and returns the ViewSpec to render
+     *
+     * Given a main route, this function instantiates the necessary controller
+     * and calls the necessary method on it, returning the result. It also
+     * handles the error condition when the requested route is not allowed to
+     * be executed by the requestor.
+     *
+     * @param string $p_main The main route segment to execute
+     * @param string $p_command The command to execute on the main route
+     * @return array The ViewSpec to render
+     * @throws RuntimeException No controller could be found for $p_main
+     * @throws RuntimeException No public method could be found for $p_command
+     */
+    public static function execute($p_main, $p_command) {
+        $mainRoute = self::sanitizeMainRoute($p_main);
+
+        // dynamically define the controller classname
+        $controllerClass = self::buildClassName($mainRoute);
+
+        if (!class_exists($controllerClass)) {
+            throw new \RuntimeException();
+        }
+
+        $controller = new $controllerClass();
+
+        /*
+         * if the action requested is a named method on the controller class, call
+         * it. Otherwise, look up the action in the routes array. If it's not there
+         * try the default route. If none specified, throw.
+         */
+        if (method_exists($controller, $p_command)) {
+            $action = $p_command;
+        } else if (isset(self::$routes[$mainRoute][$p_command])) {
+            $action = self::$routes[$mainRoute][$p_command];
+        } else if (isset(self::$routes[$mainRoute]['default'])) {
+            $action = self::$routes[$mainRoute]['default'];
+        } else {
+            throw new \RuntimeException();
+        }
+
+        $priv  = $controllerClass::PRIV;
+        $alive = $controllerClass::ALIVE;
+
+        if ($error = init($priv, $alive)) {
+            return [
+                'template' => 'error.tpl',
+                'title'    => 'There is an obstacle to your progress...',
+                'parts'    => ['error' => $error],
+                'options'  => [],
+            ];
+        } else {
+            return $controller->$action();
+        }
+    }
+
+    /**
+     * The primary method used by the front-controller script to run the app
+     *
+     * @param Request The request from the web server
+     * @return none
+     *
+     * @note
+     * This method generates output
+     */
+    public static function route($p_request) {
+        $routeSegments = self::parseRoute($p_request);
+
+        $mainRoute = array_shift($routeSegments);
+        $command = array_shift($routeSegments);
+
+        if (
+            (stripos($mainRoute, '.php') === (strlen($mainRoute) - 4)) &&
+            (file_exists($mainRoute) && realpath($mainRoute) === getcwd().'/'.$mainRoute)
+        ) {
+            include($mainRoute);
+        } else {
+            $response = self::execute($mainRoute, $command);
+
+            if ($response instanceof RedirectResponse) {
+                $response->send();
+            } else {
+                display_page(
+                    $response['template'],
+                    $response['title'],
+                    $response['parts'],
+                    $response['options']
+                );
+            }
+        }
     }
 }
--- deploy/www/front-controller.php
+++ deploy/www/front-controller.php
@@ -1,100 +1,13 @@
 <?php
 require_once(CORE.'Router.php');
-require_once(LIB_ROOT."control/lib_inventory.php");
-require_once(CORE.'control/ClanController.php');
-require_once(CORE."control/ShopController.php");
-require_once(CORE."control/CasinoController.php");
-require_once(CORE.'control/WorkController.php');
-require_once(CORE.'control/MessagesController.php');
-require_once(CORE.'control/ShrineController.php');
 
 use app\Core\Router;
 
-use app\Controller\ShopController;
-use app\Controller\ClanController;
-use app\Controller\CasinoController;
-use app\Controller\WorkController;
-use app\Controller\MessagesController;
-use app\Controller\ShrineController;
-
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 
-/*
- * this is for custom routes. The default is to map /controller/action to
- * Controller->action() but if you wish to map it to Controller->action2() then
- * put an entry here. Adding a default entry helps for when the action is not
- * found or not provided, otherwise an error probably occurs.
- */
-$routes = Router::$routes;
-
-// get the request information to parse the route
-$request = Request::createFromGlobals();
-$pathInfo = $request->getPathInfo();
-
-// split the requested path by slash
-$routeSegments = explode('/', trim($pathInfo, '/'));
-
-if ($routeSegments[0] === '') {
-	include('index.php');
-	exit();
-} else if (stripos($routeSegments[0], '.php') === (strlen($routeSegments[0]) - 4)) {
-	if (file_exists($routeSegments[0]) && realpath($routeSegments[0]) === __DIR__.'/'.$routeSegments[0]) {
-		include($routeSegments[0]);
-		exit();
-	}
-
-	$routeSegments[0] = substr($routeSegments[0], 0, -4);
-}
-
-// dynamically define the controller classname
-$controllerClass = "app\\Controller\\".ucfirst($routeSegments[0])."Controller";
-
-if (!class_exists($controllerClass) || !isset($routes[$routeSegments[0]])) {
+try {
+	// get the request information to parse the route
+	Router::route(Request::createFromGlobals());
+} catch (RuntimeException $e) {
 	include('404.php');
-	exit();
-}
-
-// if there are 2 route segments use the second one, else use the command param
-if (isset($routeSegments[1])) {
-	$command = $routeSegments[1];
-} else {
-	$command = (string)in('command');
-}
-
-$controller = new $controllerClass();
-
-$priv  = $controllerClass::PRIV;
-$alive = $controllerClass::ALIVE;
-
-if ($error = init($priv, $alive)) {
-	display_error($error);
-} else {
-	/*
-	 * if the action requested is a named method on the controller class, call
-	 * it. Otherwise, look up the action in the routes array. If it's not there
-	 * try the default route. If none specified, throw.
-	 */
-	if (method_exists($controller, $command)) {
-		$action = $command;
-	} else if (isset($routes[$routeSegments[0]][$command])) {
-		$action = $routes[$routeSegments[0]][$command];
-	} else if (isset($routes[$routeSegments[0]]['default'])) {
-		$action = $routes[$routeSegments[0]]['default'];
-	} else {
-		throw new RuntimeException('Requested route not found.');
-	}
-
-	$response = $controller->$action();
-
-	if($response instanceof RedirectResponse){
-		$response->send();
-	} else {
-		display_page(
-			$response['template'],
-			$response['title'],
-			$response['parts'],
-			$response['options']
-		);
-	}
 }
