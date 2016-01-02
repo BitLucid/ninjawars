00737dc5facad9bafc7325dbc0df3c3542d21bda
Roy Ronalds <tchalvakspam@gmail.com>
2016-01-02 05:28:46 -0500

    Clan.php and Shop.php: Fake pretty urls.

--- deploy/www/shop.php
+++ deploy/www/shop.php
@@ -1,24 +1,3 @@
 <?php
-require_once(LIB_ROOT."control/lib_inventory.php");
-require_once(LIB_ROOT."control/ShopController.php");
-
-use app\Controller\ShopController;
-
-if ($error = init(ShopController::PRIV, ShopController::ALIVE)) {
-	display_error($error);
-} else {
-	$shop = new ShopController();
-
-	if (in('command') === 'purchase') {
-		$response = $shop->buy();
-	} else {
-		$response = $shop->index();
-	}
-
-	display_page(
-		$response['template'],
-		$response['title'],
-		$response['parts'],
-		$response['options']
-	);
-} // End of no display error.
+$_SERVER['REQUEST_URI'] = '/clan';
+require(ROOT.'www/front-controller.php');
