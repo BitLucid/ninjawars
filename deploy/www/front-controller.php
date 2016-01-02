30692bd5ad346f3a9c0392f966eaafc1a0adcf79
Al Vazquez <al.vazquez@gmail.com>
2016-01-01 22:35:32 -0500

    Comment the front-controller

--- deploy/www/front-controller.php
+++ deploy/www/front-controller.php
@@ -1,14 +1,20 @@
 <?php
 require_once(LIB_ROOT.'control/lib_clan.php');
-require_once(CORE.'control/ClanController.php');
 require_once(LIB_ROOT."control/lib_inventory.php");
-require_once(LIB_ROOT."control/ShopController.php");
+require_once(CORE.'control/ClanController.php');
+require_once(CORE."control/ShopController.php");
 
 use app\Controller\ShopController;
 use app\Controller\ClanController;
 
 use Symfony\Component\HttpFoundation\Request;
 
+/*
+ * this is for custom routes. The default is to map /controller/action to
+ * Controller->action() but if you wish to map it to Controller->action2() then
+ * put an entry here. Adding a default entry helps for when the action is not
+ * found or not provided, otherwise an error probably occurs.
+ */
 $routes = [
 	'clan' => [
 		'new'     => 'create',
@@ -20,13 +26,17 @@ $routes = [
 	],
 ];
 
+// get the request information to parse the route
 $request = Request::createFromGlobals();
 $pathInfo = $request->getPathInfo();
 
+// split the requested path by slash
 $routeSegments = split('/', trim($pathInfo, '/'));
 
+// dynamically define the controller classname
 $controllerClass = "app\\Controller\\".ucfirst($routeSegments[0])."Controller";
 
+// if there are 2 route segments use the second one, else use the command param
 if (isset($routeSegments[1])) {
 	$command = $routeSegments[1];
 } else {
@@ -40,22 +50,28 @@ $alive = $controllerClass::ALIVE;
 
 if ($error = init($priv, $alive)) {
 	display_error($error);
-	die();
-}
-
-if (method_exists($controller, $command)) {
-	$action = $command;
-} else if (isset($routes[$routeSegments[0]][$command])) {
-	$action = $routes[$routeSegments[0]][$command];
 } else {
-	$action = $routes[$routeSegments[0]]['default'];
+	/*
+	 * if the action requested is a named method on the controller class, call
+	 * it. Otherwise, look up the action in the routes array. If it's not there
+	 * try the default route. If none specified, throw.
+	 */
+	if (method_exists($controller, $command)) {
+		$action = $command;
+	} else if (isset($routes[$routeSegments[0]][$command])) {
+		$action = $routes[$routeSegments[0]][$command];
+	} else if (isset($routes[$routeSegments[0]]['default'])) {
+		$action = $routes[$routeSegments[0]]['default'];
+	} else {
+		throw new RuntimeException('Requested route not found.');
+	}
+
+	$response = $controller->$action();
+
+	display_page(
+		$response['template'],
+		$response['title'],
+		$response['parts'],
+		$response['options']
+	);
 }
-
-$response = $controller->$action();
-
-display_page(
-	$response['template'],
-	$response['title'],
-	$response['parts'],
-	$response['options']
-);
