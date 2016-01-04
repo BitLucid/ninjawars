998bd6a2eb29614eedff52084738cad667acb15b
Al Vazquez <al.vazquez@gmail.com>
2016-01-03 19:19:05 -0500

    Cleanup DoshinController

--- deploy/lib/control/DoshinController.php
+++ deploy/lib/control/DoshinController.php
@@ -5,173 +5,188 @@ namespace NinjaWars\core\control;
  * Handles all user requests for the in-game Doshin Office
  */
 class DoshinController { //extends controller
-	const ALIVE = true;
-	const PRIV  = false;
+    const ALIVE          = true;
+    const PRIV           = false;
+    const MAX_BOUNTY     = 5000;
+    const MIN_BRIBE      = 0;
+    const DOSHIN_CUT     = .8;
+    const SAFE_WEALTH    = 1000;
+    const RICH_REDUCTION = .7;
 
-	protected $sessionData = [];
+    protected $sessionData = [];
 
-	/**
-	 * Gathers data from session and makes it available to internal methods
-	 *
-	 * @return DoshinController
-	 */
-	public function __construct() {
-		$this->sessionData = [
-			'username' => self_name(),
-			'char_id'  => self_char_id(),
-		];
-	}
+    /**
+     * Gathers data from session and makes it available to internal methods
+     *
+     * @return DoshinController
+     */
+    public function __construct() {
+        $this->sessionData = [
+            'username' => self_name(),
+            'char_id'  => self_char_id(),
+        ];
+    }
 
-	/**
-	 * Displays the initial Doshin Office view
-	 *
-	 * @param target String (Optional) Pre-load the bounty form with the specified target
-	 * @return Array
-	 */
-	public function index() {
-		$target = in('target');
+    /**
+     * Displays the initial Doshin Office view
+     *
+     * @param target String (Optional) Pre-load the bounty form with the specified target
+     * @return Array
+     */
+    public function index() {
+        $target = in('target');
 
-		return $this->render(
-			[
-				'quickstat' => true,
-				'location'  => 0,
-				'error'     => 0,
-				'command'   => 'index',
-				'target'    => $target,
-			]
-		);
-	}
+        return $this->render(
+            [
+                'quickstat' => true,
+                'location'  => 0,
+                'error'     => 0,
+                'command'   => 'index',
+                'target'    => $target,
+            ]
+        );
+    }
 
-	/**
-	 * Command for the current user to offer their money as bounty on another player
-	 *
-	 * @param target String The username of the player to offer a bounty on
-	 * @param amount int The amount of gold to spend on offering the bounty
-	 * @return Array
-	 *
-	 * @TODO simplify the conditional branching
-	 */
-	public function offerBounty() {
-		$target    = in('target');
-		$target_id = get_char_id($target); // Will be the enemy to put the bounty on.
-		$amount    = intval(in('amount'));
-		$amount_in = $amount;
-		$error     = 0;
-		$quickstat = false;
-		$success   = false;
+    /**
+     * Command for the current user to offer their money as bounty on another player
+     *
+     * @param target String The username of the player to offer a bounty on
+     * @param amount int The amount of gold to spend on offering the bounty
+     * @return Array
+     *
+     * @TODO simplify the conditional branching
+     */
+    public function offerBounty() {
+        $target    = in('target');
+        $target_id = get_char_id($target); // Will be the enemy to put the bounty on.
+        $amount    = intval(in('amount'));
+        $amount_in = $amount;
+        $error     = 0;
+        $quickstat = false;
+        $success   = false;
 
-		if (!$target_id) {
-			$error = 1;
-		} else { // Target existed.
-			$target_bounty = getBounty($target_id);
+        if (!$target_id) { // Test that target exists
+            $error = 1;
+        } else {
+            $target_bounty = getBounty($target_id);
 
-			if ($target_bounty < 5000) {
-				if ($amount > 0) {
-					if (($target_bounty + $amount) > 5000) {
-						$amount = (5000 - $target_bounty);
-					}
+            // Cap possible bounty amount
+            if (($target_bounty + $amount) > self::MAX_BOUNTY) {
+                $amount = (self::MAX_BOUNTY - $target_bounty);
+            }
 
-					if (get_gold($this->sessionData['char_id']) >= $amount) {
-						addBounty($target_id, $amount); // Add the bounty to the person being bountied upon.  How the hell did this break?
+            if (get_gold($this->sessionData['char_id']) < $amount) {
+                $error = 2;
+            }
 
-						subtract_gold($this->sessionData['char_id'], $amount);
-						send_event($this->sessionData['char_id'], get_char_id($target), $this->sessionData['username']." has offered $amount gold in reward for your head!");
+            if ($amount <= 0) {
+                $error = 3;
+            }
 
-						$success = true;
-						$quickstat = 'player';
-					} else {
-						$error = 2;
-					}
-				} else {
-					$error = 3;
-				}
-			} else {
-				$error = 4;
-			}
-		}
+            if ($target_bounty >= 5000) {
+                $error = 4;
+            }
+        }
 
-		return $this->render(
-			[
-				'error'     => $error,
-				'success'   => $success,
-				'quickstat' => $quickstat,
-				'amount_in' => $amount_in,
-				'amount'    => $amount,
-				'command'   => 'offer',
-				'location'  => 0,
-				'target'    => $target,
-			]
-		);
-	}
+        if ($error === 0) {
+            addBounty($target_id, $amount); // Add the bounty to the person being bountied upon.  How the hell did this break?
 
-	/**
-	 * Command for a user to reduce their bounty by paying their own gold
-	 *
-	 * @param bribe int The amount to spend on reducing bounty
-	 * @return Array
-	 */
-	public function bribe() {
-		$bribe = intval(in('bribe'));
-		$error = 0;
-		$quickstat = false;
+            subtract_gold($this->sessionData['char_id'], $amount);
+            send_event($this->sessionData['char_id'], $target_id, $this->sessionData['username']." has offered $amount gold in reward for your head!");
 
-		if ($bribe <= get_gold($this->sessionData['char_id']) && $bribe > 0) {
-			subtract_gold($this->sessionData['char_id'], $bribe);
-			subtractBounty($this->sessionData['char_id'], ($bribe/2));
-			$location = 1;
+            $success = true;
+            $quickstat = 'player';
+        }
 
-			$quickstat = 'player';
-		} else if ($bribe < 0) {
-			// Was a bug, now the doshin beats you up!  Yay!
-			if (get_gold($this->sessionData['char_id']) > 1000) { //  *** If they have more than 1000 gold, their bounty will be mostly removed by this event.
-				$bountyReduction = (getBounty($this->sessionData['char_id']) * .7);
-				subtractBounty($this->sessionData['char_id'], $bountyReduction);
-			}
+        return $this->render(
+            [
+                'error'     => $error,
+                'success'   => $success,
+                'quickstat' => $quickstat,
+                'amount_in' => $amount_in,
+                'amount'    => $amount,
+                'command'   => 'offer',
+                'location'  => 0,
+                'target'    => $target,
+            ]
+        );
+    }
 
-			subtract_gold($this->sessionData['char_id'], floor(get_gold($this->sessionData['char_id']) *.8));  //Takes away 80% of the players gold.
+    /**
+     * Command for a user to reduce their bounty by paying their own gold
+     *
+     * @param bribe int The amount to spend on reducing bounty
+     * @return Array
+     */
+    public function bribe() {
+        $bribe = intval(in('bribe'));
+        $error = 0;
+        $quickstat = false;
 
-			$location = 2;
+        if ($bribe <= get_gold($this->sessionData['char_id']) && $bribe > 0) {
+            subtract_gold($this->sessionData['char_id'], $bribe);
+            subtractBounty($this->sessionData['char_id'], ($bribe/2));
+            $location = 1;
+            $quickstat = 'player';
+        } else if ($bribe < self::MIN_BRIBE) {
+            $this->doshinAttack($this->SessionData['char_id']);
+            $location = 2;
+            $quickstat = 'player';
+        } else {
+            $location = 0;
+            $error = 5;
+        }
 
-			$quickstat = 'player';
-		} else {
-			$location = 0;
-			$error = 5;
-		}
+        return $this->render(
+            [
+                'error'     => $error,
+                'quickstat' => $quickstat,
+                'location'  => $location,
+                'command'   => 'bribe',
+            ]
+        );
+    }
 
-		return $this->render(
-			[
-				'error'     => $error,
-				'quickstat' => $quickstat,
-				'location'  => $location,
-				'command'   => 'bribe',
-			]
-		);
-	}
+    /**
+     * Was a bug, now the doshin beats you up! Yay!
+     *
+     * @note
+     * If player has enough gold, their bounty will be mostly removed
+     * by this event.
+     */
+    private function doshinAttack($p_characterId) {
+        if (get_gold($p_characterId) > self::SAFE_WEALTH) {
+            $bountyReduction = (getBounty($p_characterId) * self::RICH_REDUCTION);
+            subtractBounty($p_characterId, $bountyReduction);
+        }
 
-	/**
-	 * Returns a view spec hash for rendering a template
-	 *
-	 * @param p_data Array Hash of variables to pass to the view
-	 * @return Array
-	 */
-	private function render($p_data) {
-		$myBounty = getBounty($this->sessionData['char_id']);
+        subtract_gold($p_characterId, floor(get_gold($p_characterId) * self::DOSHIN_CUT));
+    }
 
-		// Pulling the bounties.
-		$data = query_array("SELECT player_id, uname, bounty, class_name AS class, level, clan_id, clan_name 
-			FROM players JOIN class ON class_id = _class_id LEFT JOIN clan_player ON player_id = _player_id 
-			LEFT JOIN clan ON clan_id = _clan_id WHERE bounty > 0 AND active = 1 and health > 0 ORDER BY bounty DESC");
+    /**
+     * Returns a view spec hash for rendering a template
+     *
+     * @param p_data Array Hash of variables to pass to the view
+     * @return Array
+     */
+    private function render($p_data) {
+        $myBounty = getBounty($this->sessionData['char_id']);
 
-		$p_data['data'] = $data;
-		$p_data['myBounty'] = $myBounty;
+        // Pulling the bounties.
+        $data = query_array("SELECT player_id, uname, bounty, class_name AS class, level, clan_id, clan_name 
+            FROM players JOIN class ON class_id = _class_id LEFT JOIN clan_player ON player_id = _player_id 
+            LEFT JOIN clan ON clan_id = _clan_id WHERE bounty > 0 AND active = 1 and health > 0 ORDER BY bounty DESC");
 
-		return [
-			'template' => 'doshin.tpl',
-			'title'    => 'Doshin Office',
-			'parts'    => $p_data,
-			'options'  => [
-				'quickstat' => $p_data['quickstat'],
-			],
-		];
-	}
+        $p_data['data'] = $data;
+        $p_data['myBounty'] = $myBounty;
+
+        return [
+            'template' => 'doshin.tpl',
+            'title'    => 'Doshin Office',
+            'parts'    => $p_data,
+            'options'  => [
+                'quickstat' => $p_data['quickstat'],
+            ],
+        ];
+    }
 }
