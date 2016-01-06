77ea405cfbab5d8fffa75b4be35ab942085a11aa
Al Vazquez <al.vazquez@gmail.com>
2016-01-05 22:55:51 -0500

    Tighten the ratchet on www scripts. Improve ratchet clarity.

--- deploy/tests/unit/RatchetTest.php
+++ deploy/tests/unit/RatchetTest.php
@@ -1,25 +1,38 @@
 <?php
 /**
  * Ratchets for improving codebase simplicity/quality
-**/
-
+ */
 class RatchetTest extends PHPUnit_Framework_TestCase {
-
-	const MAX_WWW_SCRIPTS = 37;
+	const MAX_WWW_SCRIPTS = 26;
+    const MAX_WWW_SCRIPTS_RATCHET = .8;
 	const MAX_NINJAMASTER_SCRIPTS = 3;
 
 	function before() {
 	}
 
 	public function testWwwScriptsLimit(){
-		$it = new RegexIterator(new DirectoryIterator(ROOT.'www/'), "/\\.php\$/i");
-		$this->assertLessThanOrEqual(static::MAX_WWW_SCRIPTS, iterator_count($it));
-		$this->assertGreaterThanOrEqual(round(static::MAX_WWW_SCRIPTS*0.80), iterator_count($it)); // Change ratchet if this trips
+		$scriptList = new RegexIterator(new DirectoryIterator(ROOT.'www/'), "/\\.php\$/i");
+        $scriptCount = iterator_count($scriptList);
+
+		$this->assertLessThanOrEqual(static::MAX_WWW_SCRIPTS, $scriptCount);
+
+        // RATCHET
+        $this->assertGreaterThanOrEqual(
+            round(static::MAX_WWW_SCRIPTS*static::MAX_WWW_SCRIPTS_RATCHET), $scriptCount,
+            "Reduce MAX_WWW_SCRIPTS to $scriptCount to tighten the ratchet"
+        );
 	}
 
 	public function testNinjamasterScriptsLimit(){
-		$it = new RegexIterator(new DirectoryIterator(ROOT.'www/ninjamaster/'), "/\\.php\$/i");
-		$this->assertLessThanOrEqual(static::MAX_NINJAMASTER_SCRIPTS, iterator_count($it));
-		$this->assertGreaterThanOrEqual(static::MAX_NINJAMASTER_SCRIPTS, iterator_count($it)); // Change ratchet if this trips
+		$scriptList = new RegexIterator(new DirectoryIterator(ROOT.'www/ninjamaster/'), "/\\.php\$/i");
+        $scriptCount = iterator_count($scriptList);
+
+		$this->assertLessThanOrEqual(static::MAX_NINJAMASTER_SCRIPTS, $scriptCount);
+
+        // RATCHET
+        $this->assertGreaterThanOrEqual(
+            static::MAX_NINJAMASTER_SCRIPTS, $scriptCount,
+            "Reduce MAX_NINJAMASTER_SCRIPTS to $scriptCount to tighten the ratchet"
+        );
 	}
 }
