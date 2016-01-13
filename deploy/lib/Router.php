64cb88f87e8df446d55cbd6c558d67d843cf0b1e
Al Vazquez <al.vazquez@gmail.com>
2016-01-12 21:39:08 -0500

    Add comments and reduce method lengths in Router

--- deploy/lib/Router.php
+++ deploy/lib/Router.php
@@ -10,14 +10,70 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
  * Controller->action(). Overrides are defined here in the $routes member.
  */
 class Router {
-    const CONTROLLER_NS   = 'NinjaWars\core\control';
+    const CONTROLLER_NS   = 'NinjaWars\core\control'; /// Namespace for controllers
     const DEFAULT_ACTION  = 'index';
     const DEFAULT_COMMAND = 'default';
     const DEFAULT_ROUTE   = 'index.php';
     const COMMAND_PARAM   = 'command';
 
+    /** Set during bootstapping by the file routes.php. Routes are defined as a
+     * 3-level nested-array with level-one keys being the controller name,
+     * level-two keys being metadata, and level-three keys being action names.
+     * Action names are mapped to public methods on the controller class.
+     */
     public static $routes;
+
+    /** Set during bootstrapping by the file routes.php. Mappings between the
+     * controller portion of a route and a real controller name can be used to
+     * make nicer routes or to support historical URLs.
+     */
     public static $controllerAliases;
+
+    /**
+     * Given a request, includes a file or runs a controller action
+     *
+     * @param Request The request from the web server
+     * @return none
+     *
+     * @note
+     * This method generates output
+     *
+     * @todo remove the second isServableFile block when an index controller exists
+     */
+    public static function route($p_request) {
+        // split the requested path by slash
+        $routeSegments = explode('/', trim($p_request->getPathInfo(), '/'));
+
+        $mainRoute = current($routeSegments); // get 1st part of route
+
+        if (self::isServableFile($mainRoute)) { // serve existing files
+            include($mainRoute);
+        } else { // attempt to serve controller actions
+            $routeSegments = self::parseRoute($p_request);
+            $mainRoute = array_shift($routeSegments);
+
+            /** Because we don't have an index controller, we serve index.php
+             * as a file. Because index.php is the default route, we need this
+             * repetition of code because parseRoute translates / to index.php
+             */
+            if (self::isServableFile($mainRoute)) {
+                include($mainRoute);
+            } else {
+                if (isset(self::$routes[$mainRoute]) && self::$routes[$mainRoute]['type'] === 'simple') {
+                    $response = self::serveSimpleRoute($mainRoute);
+                } else {
+                    $command   = array_shift($routeSegments);
+                    $response  = self::execute($mainRoute, $command);
+                }
+
+                if ($response instanceof RedirectResponse) {
+                    $response->send();
+                } else {
+                    self::render($response);
+                }
+            }
+        }
+    }
 
     /**
      * Breaks the request URI into an array of route segments
@@ -26,28 +82,34 @@ class Router {
      * @return array Each segment of the route
      *
      * @note
+     * If there is no path in the request, this will return an array with the
+     * default route for the application
+     *
+     * @note
      * If there is only 1 route segment and the special input parameter
-     * "command" is set in the request, the value of "command" will be returned
-     * in as the second element of the returned array
+     * "command" is set in the request, the value of "command" will be used
+     * as the second element of the return value. If there is no command
+     * parameter, the default action for the application will be used
      *
      * @todo remove the call to in() and replace with something from request
      * @todo stop supporting ?command=action
      */
-    public static function parseRoute($p_routeSegments, $p_request) {
-        $p_request = $p_request;
+    public static function parseRoute($p_request) {
+        // split the requested path by slash
+        $routeSegments = explode('/', trim($p_request->getPathInfo(), '/'));
 
-        if (empty($p_routeSegments[0])) {
+        if (empty($routeSegments[0])) { // Route is just forward-slash
             $mainRoute = self::DEFAULT_ROUTE;
-        } else {
+        } else { // determine the canonical route being requested
             $mainRoute = self::translateRoute(
-                self::sanitizeRoute($p_routeSegments[0])
+                self::sanitizeRoute($routeSegments[0])
             );
         }
 
-        // if there are 2 route segments use the second one
+        // if there are 2 route segments use the second one as the command
         if (isset($p_routeSegments[1]) && !empty($p_routeSegments[1])) {
             $command = $p_routeSegments[1];
-        } else {
+        } else { // without a 2nd route segment, look for command in the input
             $command = (string)in(self::COMMAND_PARAM);
         }
 
@@ -93,11 +155,8 @@ class Router {
      * @return string
      */
     public static function translateRoute($p_main) {
-        if (isset(self::$controllerAliases[$p_main])) {
-            $p_main = self::$controllerAliases[$p_main];
-        }
-
-        return $p_main;
+        return (isset(self::$controllerAliases[$p_main]) ?
+            self::$controllerAliases[$p_main] : $p_main);
     }
 
     /**
@@ -113,27 +172,30 @@ class Router {
      * @return array The ViewSpec to render
      * @throws RuntimeException No controller could be found for $p_main
      * @throws RuntimeException No public method could be found for $p_command
+     * @todo Throw a specific exception when class not found
+     * @todo Throw a specific exception when the command requested is not found
+     * @todo Abstract out the rendering of the error screen
      */
     public static function execute($p_main, $p_command) {
         // dynamically define the controller classname
         $controllerClass = self::buildClassName($p_main);
 
-        if (!class_exists($controllerClass)) {
+        if (!class_exists($controllerClass)) { // ensure class requested exists
             throw new \RuntimeException();
         }
 
         $controller = new $controllerClass();
 
-        /*
-         * if the action requested is a named method on the controller class, call
-         * it. Otherwise, look up the action in the routes array. If it's not there
-         * try the default route. If none specified, throw.
+        /* if the action requested is a named method on the controller class,
+         * call it. Otherwise, look up the action in the routes map. If it's
+         * not there try the default route. If none specified, throw.
          */
         if (method_exists($controller, $p_command)) {
             $action = $p_command;
         } else if (isset(self::$routes[$p_main]['actions'][$p_command])) {
             $action = self::$routes[$p_main]['actions'][$p_command];
 
+            // If the action in the routes map still doesn't exist, throw
             if (!method_exists($controller, $action)) {
                 throw new \RuntimeException();
             }
@@ -141,6 +203,15 @@ class Router {
             throw new \RuntimeException();
         }
 
+        /* This conditional is a holdover from old times when controllers were
+         * actually directly servable procedural scripts (ah the old days!)
+         * If a page required you to be alive or logged in, init() checked if
+         * you were (among a bunch of other things, usually dropping variables
+         * in the global namespace) and if you were not, returned an error.
+         * Now, controllers do this at a class level, which is mostly nonsense
+         * and requires that the router directly serve an error page.
+         * @TODO this whole thing should be factored out.
+         */
         if ($error = init($controllerClass::PRIV, $controllerClass::ALIVE)) {
             return [
                 'template' => 'error.tpl',
@@ -153,59 +224,45 @@ class Router {
         }
     }
 
+    /**
+     * Determines if the route requested is actually a file in the web root
+     *
+     * @param string $p_route The route requested by the user
+     * @return boolean
+     */
     public static function isServableFile($p_route) {
-        return (file_exists($p_route) && realpath($p_route) === getcwd().'/'.$p_route);
+        return (is_file($p_route) && realpath($p_route) === getcwd().'/'.$p_route);
     }
 
     /**
-     * The primary method used by the front-controller script to run the app
+     * Generates a viewspec for a route served by a view without a controller
      *
-     * @param Request The request from the web server
-     * @return none
+     * @param string $p_mainRoute The 1st-level route requested by the user
+     * @return Viewspec The data needed to render the view on from the route
+     */
+    public static function serveSimpleRoute($p_mainRoute) {
+        return [
+            'template' => "$p_mainRoute.tpl",
+            'title'    => self::$routes[$p_mainRoute]['title'],
+            'parts'    => [],
+            'options'  => false,
+        ];
+    }
+
+    /**
+     * Renders the view and sends it to the client
      *
+     * @param Array $p_viewSpec The data needed to render a view
+     * @return void
      * @note
      * This method generates output
      */
-    public static function route($p_request) {
-        $pathInfo = $p_request->getPathInfo();
-
-        // split the requested path by slash
-        $routeSegments = explode('/', trim($pathInfo, '/'));
-
-        $mainRoute = current($routeSegments);
-
-        if (self::isServableFile($mainRoute)) {
-            include($mainRoute);
-        } else {
-            $routeSegments = self::parseRoute($routeSegments, $p_request);
-            $mainRoute = array_shift($routeSegments);
-
-            if (self::isServableFile($mainRoute)) {
-                include($mainRoute);
-            } else {
-                if (isset(self::$routes[$mainRoute]) && self::$routes[$mainRoute]['type'] === 'simple') {
-                    $response = [
-                        'template' => "$mainRoute.tpl",
-                        'title'    => self::$routes[$mainRoute]['title'],
-                        'parts'    => [],
-                        'options'  => false,
-                    ];
-                } else {
-                    $command   = array_shift($routeSegments);
-                    $response  = self::execute($mainRoute, $command);
-                }
-
-                if ($response instanceof RedirectResponse) {
-                    $response->send();
-                } else {
-                    display_page(
-                        $response['template'],
-                        $response['title'],
-                        $response['parts'],
-                        $response['options']
-                    );
-                }
-            }
-        }
+    public static function render($p_viewSpec) {
+        display_page(
+            $p_viewSpec['template'],
+            $p_viewSpec['title'],
+            $p_viewSpec['parts'],
+            $p_viewSpec['options']
+        );
     }
 }
