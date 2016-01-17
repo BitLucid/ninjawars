5df3fec1d3f66f64af9d83e6cfbd35a6c31b8fe1
Al Vazquez <al.vazquez@gmail.com>
2016-01-17 18:06:26 -0500

    Add basic unit tests for AccountController

---
+++ deploy/tests/unit/AccountControllerUnitTest.php
@@ -0,0 +1,54 @@
+<?php
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Storage\MockArraySessionStorage;
+use NinjaWars\core\environment\RequestWrapper;
+use NinjaWars\core\control\SessionFactory;
+use NinjaWars\core\control\AccountController;
+
+class AccountControllerUnitTest extends PHPUnit_Framework_TestCase {
+    private $controller;
+
+    public function __construct() {
+        $this->controller = new AccountController();
+    }
+
+	protected function setUp() {
+		SessionFactory::init(new MockArraySessionStorage());
+
+		$get = [
+			'command' => 'change'
+		];
+
+        $request = new Request($get);
+
+        RequestWrapper::inject($request); // Pass a request to be used by tests
+    }
+
+	protected function tearDown() {
+        RequestWrapper::destroy();
+    }
+
+    public function testIndex() {
+        $response = $this->controller->index();
+
+        $this->assertArrayHasKey('template', $response);
+    }
+
+    public function testShowChangeEmailForm() {
+        $response = $this->controller->showChangeEmailForm();
+
+        $this->assertArrayHasKey('template', $response);
+    }
+
+    public function testShowChangePasswordForm() {
+        $response = $this->controller->showChangePasswordForm();
+
+        $this->assertArrayHasKey('template', $response);
+    }
+
+    public function testDeleteAccountConfirmation() {
+        $response = $this->controller->deleteAccountConfirmation();
+
+        $this->assertArrayHasKey('template', $response);
+    }
+}
