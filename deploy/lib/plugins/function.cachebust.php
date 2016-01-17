e10f69ac47c5038537821c838de0e211d032d699
Al Vazquez <al.vazquez@gmail.com>
2016-01-17 14:26:20 -0500

    Bring cachebust plugin up to standards
    1. Remove unused template parameter
    2. Use the ROOT constant instead of $_SERVER['DOCUMENT_ROOT']
    3. Add @param and @return docblock declarations

--- deploy/lib/plugins/function.cachebust.php
+++ deploy/lib/plugins/function.cachebust.php
@@ -1,9 +1,13 @@
 <?php
 /**
  * Smarty plugin to add a timestamp to static files for cachebusting
+ *
+ * @param Array $p_params Dictionary provided by Smarty
+ * @return String
  */
-function smarty_function_cachebust($p_params, &$p_tpl) {
-    $file = $_SERVER['DOCUMENT_ROOT']."/$p_params[file]";
+function smarty_function_cachebust($p_params) {
+    $file = ROOT."/www/$p_params[file]";
+
     if (is_file($file)) {
         $mtime = filemtime($file);
         $pathParts = pathinfo($p_params['file']);
