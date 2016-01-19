8fbc1acf26dfb3aa84340c4b0ab30a32e29a61ab
Roy Ronalds <tchalvakspam@gmail.com>
2016-01-18 19:08:12 -0500

    Functional Tests: Better fail messages.
    mimic phpunit somewhat by outputting . chars for passing tests.

--- deploy/tests/functional/routing_tests.py
+++ deploy/tests/functional/routing_tests.py
@@ -3,6 +3,12 @@ import requests
 
 ROOT_URL = "http://nw.local/"
 
+def assert_true(outcome, fail_message=''):
+    if outcome == True:
+        print('.', end='')
+    else:
+        print(fail_message)
+
 def status_code(url):
     try:
         r = requests.head(url)
@@ -13,7 +19,7 @@ def status_code(url):
 
 def test_basic():
     res = status_code("http://stackoverflow.com");
-    print(res == 200)
+    assert_true(res == 200)
 
 
 
@@ -21,12 +27,12 @@ def test_routing():
     urls = []
 
 def test_urls_should_200():
-    urls = ['', 'shop', 'clan', 'shop.php', 'clan.php', 'shop/', 'shop/index', 'shop/buy', 'work.php', 'list.php', 'map.php'];
-    [print(200 == status_code(ROOT_URL+url)) for url in urls]
+    urls = ['', 'staff.php', 'events.php', 'skills.php', 'inventory.php', 'enemies.php', 'list.php', 'clan.php', 'map.php', 'shop.php', 'work.php', 'doshin_office.php', 'clan.php?command=list', 'shop', 'clan', 'shop/', 'shop/index', 'shop/buy'];
+    [assert_true(200 == status_code(ROOT_URL+url), 'Url did not 200: ['+url+']') for url in urls]
 
 def test_urls_should_404():
     urls = ['thisshould404', 'shoppinginthesudan', 'js/doesnotexist.js', 'shop/willneverexist', 'shopbobby\'-tables']
-    [print(404 == status_code(ROOT_URL+url)) for url in urls]
+    [assert_true(404 == status_code(ROOT_URL+url), 'Url did not 404: ['+url+']') for url in urls]
 
 def run_all_tests():
     print('Starting routing tests, the ROOT_URL is'+ROOT_URL)
