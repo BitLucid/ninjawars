13829e58b39ce9838c916f555ac23d4361688741
Al Vazquez <al.vazquez@gmail.com>
2016-01-18 21:04:41 -0500

    Add basic unit tests for Router

---
+++ deploy/tests/unit/RouterUnitTest.php
@@ -0,0 +1,82 @@
+<?php
+use NinjaWars\core\Router;
+use Symfony\Component\HttpFoundation\Request;
+
+class RouterUnitTest extends PHPUnit_Framework_TestCase {
+    public function testParseRouteSlash() {
+        $request = Request::create('/', 'GET', []);
+        $result = Router::parseRoute($request);
+        $this->assertInternalType('array', $result);
+        $this->assertGreaterThan(1, count($result));
+        $this->assertContains('index', $result[0]);
+    }
+
+    public function testParseRouteControllerDefault() {
+        $request = Request::create('/shop/', 'GET', []);
+        $result = Router::parseRoute($request);
+        $this->assertInternalType('array', $result);
+        $this->assertGreaterThan(1, count($result));
+    }
+
+    public function testParseRouteControllerCommand() {
+        $request = Request::create('/shop/buy', 'GET', []);
+        $result = Router::parseRoute($request);
+        $this->assertInternalType('array', $result);
+        $this->assertGreaterThan(1, count($result));
+    }
+
+    public function testParseRouteControllerAlternateDefault() {
+        $request = Request::create('/clan/', 'GET', []);
+        $result = Router::parseRoute($request);
+        $this->assertInternalType('array', $result);
+        $this->assertGreaterThan(1, count($result));
+    }
+
+    public function testBuildClassname() {
+        $this->assertEquals(
+            Router::buildClassname('junk'),
+            'NinjaWars\core\control\JunkController'
+        );
+    }
+
+    public function testSanitizeRoutePHP() {
+        $this->assertEquals(Router::sanitizeRoute('shop.php'), 'shop');
+    }
+
+    public function testIsServableFilePositive() {
+        $dir = getcwd();
+        chdir(ROOT.'/www/');
+        $this->assertTrue(Router::isServableFile('front-controller.php'));
+        chdir($dir);
+    }
+
+    public function testIsServableFileNegative() {
+        $dir = getcwd();
+        chdir(ROOT.'/www/');
+        $this->assertFalse(Router::isServableFile('back-controller.php'));
+        chdir($dir);
+    }
+
+    public function testServeSimpleRoute() {
+        $testValue = 'staff';
+        $response = Router::serveSImpleRoute($testValue);
+        $this->assertArrayHasKey('template', $response);
+        $this->assertEquals($response['template'], $testValue.'.tpl');
+    }
+
+    public function testExecuteBadClassname() {
+        $this->setExpectedException('\RuntimeException');
+        Router::execute('junkola', '');
+    }
+
+    public function testExecuteBadCommand() {
+        $this->setExpectedException('\RuntimeException');
+        Router::execute('shop', 'junkola');
+    }
+
+    public function testRouteBadClassname() {
+        $this->setExpectedException('\RuntimeException');
+        $request = Request::create('/junkola/', 'GET', []);
+        Router::route($request);
+    }
+}
