c14f78f7330085212ff0f232efd4133e827cc18b
Roy Ronalds <tchalvakspam@gmail.com>
2016-01-24 08:25:10 -0500

    Events: Controllerified.
    Default url for events becomes simply /events/ . Fixes #395.

---
+++ deploy/lib/control/EventsController.php
@@ -0,0 +1,42 @@
+<?php
+namespace NinjaWars\core\control;
+
+use \Player as Player;
+
+
+
+/**
+ * Handle the listing of events
+ */
+class EventsController { 
+    const ALIVE          = false;
+    const PRIV           = true;
+
+    public function __construct(){
+    }
+
+    /**
+     * Display the combat/action events and mark them as read when displayed.
+     */
+    public function index(){
+    	$char = new Player(self_char_id());
+		$events = get_events($char->id(), 300);
+
+		// Check for clan to use it in the nav tabs.
+		$has_clan  = (bool)get_clan_by_player_id($char->id());
+
+		read_events($char->id()); // mark events as viewed.
+
+		$template = 'events.tpl';
+		$title = 'Events';
+		$parts = ['events'=>$events, 'has_clan'=>$has_clan, 'char'=>$char];
+		$options = ['quickstat' => 'player'];
+		return [
+			'title'=>$title,
+			'template'=>$template,
+			'parts'=>$parts,
+			'options'=>$options
+			];
+    }
+
+}
