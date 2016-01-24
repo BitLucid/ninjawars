58598aefce2163b423fd6278dd88ef04d1dfe183
Roy Ronalds <tchalvakspam@gmail.com>
2016-01-24 14:14:21 -0500

    Create a cryptography lib

---
+++ deploy/tests/unit/crypto_test.php
@@ -0,0 +1,21 @@
+<?php
+class TestCrypto extends PHPUnit_Framework_TestCase {
+    public function testRandomStringDoesntEverMatch() {
+        $this->assertNotEmpty(make_random_string());
+        $i = 30;
+
+        while ($i--) {
+            $this->assertNotEquals(make_random_string(), make_random_string());
+        }
+    }
+
+    public function testNonceDoesntEverMatch() {
+        $nonce = nonce();
+        $this->assertNotEmpty(nonce());
+        $i = 30;
+
+        while ($i--) {
+            $this->assertNotEquals(nonce(), nonce());
+        }
+    }
+}
