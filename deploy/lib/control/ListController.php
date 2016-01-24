0074bdc3b82368f3defa26fd14c32c2e557fe812
Roy Ronalds <tchalvakspam@gmail.com>
2016-01-24 08:47:58 -0500

    List: Initial controller creation.
    Fixes #393. Almost.

--- deploy/lib/control/ListController.php
+++ deploy/lib/control/ListController.php
@@ -1,16 +1,28 @@
 <?php
+namespace NinjaWars\core\control;
+
 use NinjaWars\core\data\DatabaseConnection;
 use NinjaWars\core\control\SessionFactory;
-
-$private = false;
-$alive   = false;
-
-if ($error = init($private, $alive)) {
-	display_error($error);
-} else {
+use \Player as Player;
 
 require_once(LIB_ROOT."control/lib_player_list.php");
 require_once(LIB_ROOT."control/lib_player.php");
+
+/**
+ * Display the ninja list as a whole
+ */
+class ListController { 
+    const ALIVE          = false;
+    const PRIV           = false;
+
+    public function __construct(){
+    }
+
+    /**
+     * Get the ninja list and display it
+     */
+    public function index(){
+
 
 DatabaseConnection::getInstance();
 
@@ -33,7 +45,7 @@ $rank         = get_rank($char->id());
 
 $dead_count = query_item("SELECT count(player_id) FROM rankings WHERE alive = false");
 
-$page 		 = in('page');
+$page        = in('page');
 
 if (!$searched && $hide_setting != $hide) { $session->set('hide_dead', $hide); } // Save the toggled state for later.
 
@@ -50,22 +62,22 @@ $where_clauses = array(); // Array to add where clauses to.
 $queryParams = array();
 
 if ($searched) {
-	$view_type = 'searched';
+    $view_type = 'searched';
 
-	if (strlen($searched) == 1) {
-		$where_clauses[] = " (rankings.uname ilike :param".count($queryParams)." || '%') ";
-		$queryParams[] = $searched;
-	} else if (!$list_by_rank) {
-		$where_clauses[] = " (rankings.uname ilike :param".count($queryParams)." || '%') ";
-		$queryParams[] = $searched;
-	}
+    if (strlen($searched) == 1) {
+        $where_clauses[] = " (rankings.uname ilike :param".count($queryParams)." || '%') ";
+        $queryParams[] = $searched;
+    } else if (!$list_by_rank) {
+        $where_clauses[] = " (rankings.uname ilike :param".count($queryParams)." || '%') ";
+        $queryParams[] = $searched;
+    }
 
-	if ($hide == 'dead') {
-		$where_clause[] = " alive = true";
-	}
+    if ($hide == 'dead') {
+        $where_clause[] = " alive = true";
+    }
 }
 else if ($hide == 'dead') {
-	$where_clauses[] = " alive";
+    $where_clauses[] = " alive";
 }
 
 $query_count     = "SELECT count(player_id) FROM rankings ".(count($where_clauses)? "WHERE ".implode($where_clauses, ' AND ') : "");
@@ -73,8 +85,8 @@ $count_statement = DatabaseConnection::$pdo->prepare($query_count);
 
 $where_clauses[] = " active = 1"; // Filter out inactives when not dealing with rankings, which is filtered separately.
 
-for ($i = 0;$i < count($queryParams); $i++) {	// *** Reformulate if queryParams gets to be more than 3 or for items
-	$count_statement->bindValue(':param'.$i, $queryParams[$i]);
+for ($i = 0;$i < count($queryParams); $i++) {   // *** Reformulate if queryParams gets to be more than 3 or for items
+    $count_statement->bindValue(':param'.$i, $queryParams[$i]);
 }
 
 $count_statement->execute();
@@ -90,11 +102,11 @@ $totalrows = $count_statement->fetchColumn();
 // limit value = ($page * $record_limit) - $record_limit;
 
 if ($searched && $list_by_rank) {
-	$page = ceil(substr($searched, 1) / $record_limit);
+    $page = ceil(substr($searched, 1) / $record_limit);
 } else if ($page == "searched") {
-	$page = in('page', 1);
+    $page = in('page', 1);
 } else {
-	$page = ($page < 1 ? 1 : $page); // Prevent the page number from going negative.
+    $page = ($page < 1 ? 1 : $page); // Prevent the page number from going negative.
 }
 
 $numofpages = ceil($totalrows / $record_limit);
@@ -102,13 +114,13 @@ $limitvalue = (int) max(0, ($page * $record_limit) - $record_limit);
 
 // Get the ninja information to create the lists.
 $sel = "SELECT rank_id, rankings.uname, class.class_name as class, class.identity as class_identity, class.theme as class_theme, rankings.level, rankings.alive, rankings.days, clan_player._clan_id AS clan_id, clan.clan_name, players.player_id
-	FROM rankings LEFT JOIN clan_player ON player_id = _player_id LEFT JOIN clan ON clan_id = _clan_id JOIN players on rankings.player_id = players.player_id JOIN class on class.class_id = players._class_id ".(count($where_clauses)? " WHERE active = 1 AND ".implode($where_clauses, ' AND ') : "")." ORDER BY rank_id ASC, player_id ASC
-	LIMIT :limit OFFSET :offset";
+    FROM rankings LEFT JOIN clan_player ON player_id = _player_id LEFT JOIN clan ON clan_id = _clan_id JOIN players on rankings.player_id = players.player_id JOIN class on class.class_id = players._class_id ".(count($where_clauses)? " WHERE active = 1 AND ".implode($where_clauses, ' AND ') : "")." ORDER BY rank_id ASC, player_id ASC
+    LIMIT :limit OFFSET :offset";
 
 $ninja_info = DatabaseConnection::$pdo->prepare($sel);
 
-for ($i = 0;$i < count($queryParams); $i++) {	// *** Reformulate if queryParams gets to be more than 3 or for items
-	$ninja_info->bindValue(':param'.$i, $queryParams[$i]);
+for ($i = 0;$i < count($queryParams); $i++) {   // *** Reformulate if queryParams gets to be more than 3 or for items
+    $ninja_info->bindValue(':param'.$i, $queryParams[$i]);
 }
 
 $ninja_info->bindValue(':limit', $record_limit);
@@ -118,9 +130,9 @@ $ninja_info->execute();
 $last_page = (($totalrows - ($record_limit * $page)) > 0);
 
 if (!$searched) { // Will not display active ninja on a search page.
-	$active_ninjas = get_active_players(5, $alive_only); // get  the currently active ninjas
+    $active_ninjas = get_active_players(5, $alive_only); // get  the currently active ninjas
 } else {
-	$active_ninjas = null;
+    $active_ninjas = null;
 }
 
 // Format each of the player rows, then just pass 'em to the template.
@@ -130,19 +142,24 @@ $player_rows = '';
 $ninja_rows = array();
 
 while ($a_player = $ninja_info->fetch()) {
-	$ninja_rows[] = format_ninja_row($a_player);
-	$ninja_rows[$ninja_count]['odd_or_even'] = (($ninja_count+1) % 2 ? "odd" : "even");
-	$ninja_count++;
+    $ninja_rows[] = format_ninja_row($a_player);
+    $ninja_rows[$ninja_count]['odd_or_even'] = (($ninja_count+1) % 2 ? "odd" : "even");
+    $ninja_count++;
 }
 
 $parts = get_certain_vars(get_defined_vars(), $whitelist=array('ninja_rows', 'active_ninjas'));
 
-display_page(
-	'list.tpl'	// *** Main Template ***
-	, 'Ninja List'		// *** Page Title ***
-	, $parts			// *** Page Variables ***
-	, array(			// *** Page Options ***
-		'quickstat' => true
-	)
-);
+    $options = ['quickstat'=>true];
+    $title = 'Ninja List';
+    $template = 'list.tpl';
+
+    return [
+        'title'=>$title,
+        'template'=>$template,
+        'parts'=>$parts,
+        'options'=>$options,
+        ];
+    }
+
+
 }
