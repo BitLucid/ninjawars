26265b12c577b6d363a4ee780c3fb97f4a944db4
Roy Ronalds <tchalvakspam@gmail.com>
2016-01-25 08:24:10 -0500

    List: Sanity refactor.

--- deploy/lib/control/ListController.php
+++ deploy/lib/control/ListController.php
@@ -1,7 +1,6 @@
 <?php
 namespace NinjaWars\core\control;
 
-use NinjaWars\core\data\DatabaseConnection;
 use NinjaWars\core\control\SessionFactory;
 use \Player as Player;
 
@@ -22,32 +21,29 @@ class ListController {
      * Get the ninja list and display it
      */
     public function index(){
-
-
-DatabaseConnection::getInstance();
-
 $session      = SessionFactory::getSession();
 $char = new Player(self_char_id());
 $username     = $char->name();
 $char_id      = $char->id();
 $searched     = in('searched', null, 'no filter'); // Don't filter the search setting.
 $list_by_rank = ($searched && substr_compare($searched, '#', 0, 1) === 0); // Whether the search is by rank.
-
 $hide_setting = (!$searched && $session->has('hide_dead') ? $session->get('hide_dead') : 'dead'); // Defaults to hiding dead via session.
 $hide         = ($searched ? 'none' : in('hide', $hide_setting)); // search override > get setting > session setting
-
 $alive_only   = ($hide == 'dead');
 $page         = in('page', 1); // Page will get changed down below.
+$view_type    = in('view_type');
+$page        = in('page');
+$rank         = get_rank($char->id());
 $alive_count  = 0;
 $record_limit = 20; // *** The number of players that gets shown per page.
-$view_type    = in('view_type');
-$rank         = get_rank($char->id());
 
 $dead_count = query_item("SELECT count(player_id) FROM rankings WHERE alive = false");
 
-$page        = in('page');
 
-if (!$searched && $hide_setting != $hide) { $session->set('hide_dead', $hide); } // Save the toggled state for later.
+
+if (!$searched && $hide_setting != $hide) { // Save the toggled state for later.
+    $session->set('hide_dead', $hide); 
+} 
 
 // Display the clear search and create the where clause for searching.
 
@@ -55,23 +51,17 @@ if (!$searched && $hide_setting != $hide) { $session->set('hide_dead', $hide); }
 // If unless showing dead, check that health is > 0, or alive = true from the ranking.
 // Otherwise, no searching was done, so the score
 
-$where_clauses = array(); // Array to add where clauses to.
-
-
+$where_clauses = []; // Array to add where clauses to.
 // Select some players from the ranking.
-$queryParams = array();
+$params = [];
 
 if ($searched) {
     $view_type = 'searched';
 
-    if (strlen($searched) == 1) {
-        $where_clauses[] = " (rankings.uname ilike :param".count($queryParams)." || '%') ";
-        $queryParams[] = $searched;
-    } else if (!$list_by_rank) {
-        $where_clauses[] = " (rankings.uname ilike :param".count($queryParams)." || '%') ";
-        $queryParams[] = $searched;
+    if (strlen($searched) == 1 || !$list_by_rank) {
+        $where_clauses[] = " (rankings.uname ilike :searched || '%') ";
+        $params[':searched'] = $searched;
     }
-
     if ($hide == 'dead') {
         $where_clause[] = " alive = true";
     }
@@ -80,19 +70,16 @@ else if ($hide == 'dead') {
     $where_clauses[] = " alive";
 }
 
-$query_count     = "SELECT count(player_id) FROM rankings ".(count($where_clauses)? "WHERE ".implode($where_clauses, ' AND ') : "");
-$count_statement = DatabaseConnection::$pdo->prepare($query_count);
+$query_count     = "SELECT count(player_id) FROM rankings "
+    .(count($where_clauses)? "WHERE ".implode($where_clauses, ' AND ') : "");
 
-$where_clauses[] = " active = 1"; // Filter out inactives when not dealing with rankings, which is filtered separately.
-
-for ($i = 0;$i < count($queryParams); $i++) {   // *** Reformulate if queryParams gets to be more than 3 or for items
-    $count_statement->bindValue(':param'.$i, $queryParams[$i]);
-}
-
-$count_statement->execute();
-$totalrows = $count_statement->fetchColumn();
+$totalrows = query_item($query_count, $params);
 
 
+$where_clauses[] = " active = 1"; 
+// The rankings view automatically filters out inactives, but we have to do it manually when dealing directly with players table.
+
+// ************************ Pagination **************************
 // Determine the current page spot navigated to.
 // If searching, use the page between
 // If no specific rank was requested, use the viewer's rank
@@ -111,40 +98,35 @@ if ($searched && $list_by_rank) {
 
 $numofpages = ceil($totalrows / $record_limit);
 $limitvalue = (int) max(0, ($page * $record_limit) - $record_limit);
+$last_page = (($totalrows - ($record_limit * $page)) > 0);
 
 // Get the ninja information to create the lists.
 $sel = "SELECT rank_id, rankings.uname, class.class_name as class, class.identity as class_identity, class.theme as class_theme, rankings.level, rankings.alive, rankings.days, clan_player._clan_id AS clan_id, clan.clan_name, players.player_id
     FROM rankings LEFT JOIN clan_player ON player_id = _player_id LEFT JOIN clan ON clan_id = _clan_id JOIN players on rankings.player_id = players.player_id JOIN class on class.class_id = players._class_id ".(count($where_clauses)? " WHERE active = 1 AND ".implode($where_clauses, ' AND ') : "")." ORDER BY rank_id ASC, player_id ASC
     LIMIT :limit OFFSET :offset";
 
-$ninja_info = DatabaseConnection::$pdo->prepare($sel);
+$params[':limit'] = $record_limit;
+$params[':offset'] = $limitvalue;
 
-for ($i = 0;$i < count($queryParams); $i++) {   // *** Reformulate if queryParams gets to be more than 3 or for items
-    $ninja_info->bindValue(':param'.$i, $queryParams[$i]);
-}
+$ninja_infos = query($sel, $params);
 
-$ninja_info->bindValue(':limit', $record_limit);
-$ninja_info->bindValue(':offset', $limitvalue);
-$ninja_info->execute();
 
-$last_page = (($totalrows - ($record_limit * $page)) > 0);
-
-if (!$searched) { // Will not display active ninja on a search page.
-    $active_ninjas = get_active_players(5, $alive_only); // get  the currently active ninjas
-} else {
-    $active_ninjas = null;
-}
 
 // Format each of the player rows, then just pass 'em to the template.
 
 $ninja_count = 0;
-$player_rows = '';
-$ninja_rows = array();
+$ninja_rows = [];
 
-while ($a_player = $ninja_info->fetch()) {
+
+foreach($ninja_infos as $a_player){
     $ninja_rows[] = format_ninja_row($a_player);
     $ninja_rows[$ninja_count]['odd_or_even'] = (($ninja_count+1) % 2 ? "odd" : "even");
     $ninja_count++;
+}
+
+$active_ninjas = null;
+if (!$searched) { // Will not display active ninja on a search page.
+    $active_ninjas = get_active_players(5, $alive_only); // get  the currently active ninjas
 }
 
     $parts = [
@@ -170,6 +152,5 @@ while ($a_player = $ninja_info->fetch()) {
         'options'=>$options,
         ];
     }
-
 
 }
