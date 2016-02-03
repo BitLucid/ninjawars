c35e6a9e6b913b23249934185e2477eebb3211e4
Roy Ronalds <tchalvakspam@gmail.com>
2016-02-03 11:35:18 -0500

    Skills: Test skills DAO more.

--- deploy/tests/integration/model/skill_model_test.php
+++ deploy/tests/integration/model/skill_model_test.php
@@ -1,6 +1,6 @@
 <?php
 require_once(ROOT.'core/control/Skill.php');
-require_once(ROOT.'core/data/SkillDAO.php');
+require_once(ROOT.'core/data/SkillDAO.class.php');
 
 
 use \Skill as Skill;
@@ -16,12 +16,16 @@ class TestSkillObj extends PHPUnit_Framework_TestCase {
 		TestAccountCreateAndDestroy::purge_test_accounts();
     }
 
+    public function testAllSkillsCanBePulled(){
+        $skill_list = new SkillDAO();
+        $this->assertNotEmpty($skill_list->all());
+    }
+
 
     public function testAdminPlayerGetsFireBoltSkill(){
         $skills = new Skill();
         $passfail = $skills->hasSkill('Fire Bolt', 'tchalvak');
         $this->assertTrue($passfail);
-        debug($skills->
     }
 
 }
