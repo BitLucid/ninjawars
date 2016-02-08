e31dc1c9613f47e4f41bc8c67c278b4df56ccf02
Roy Ronalds <tchalvakspam@gmail.com>
2016-02-07 21:26:43 -0500

    Ratchets: Minor code cleanup.

--- deploy/tests/functional/test_ratchets.py
+++ deploy/tests/functional/test_ratchets.py
@@ -18,12 +18,8 @@ class TestRatchets:
     def www_dir(self):
         return self.deploy_dir()+'www/'
 
-    def count_dir_php(self, dir):
+    def count_php_in_dir(self, dir):
         return len(fnmatch.filter(os.listdir(dir), '*.php'))
-        #path, dirs, files = next(os.walk(dir))
-        #file_count = len(files)
-        #return file_count
-        #return len([name for name in os.listdir(dir) if os.path.isfile(name)])
 
     def test_directories_exist(self):
         assert 0 < len(self.www_dir())
@@ -32,36 +28,14 @@ class TestRatchets:
     def test_dir_files(self):
         control_dir = self.deploy_dir()+'lib/control/'
         www_dir  = self.www_dir()
-        assert control_dir and 0 < self.count_dir_php(control_dir)
-        assert www_dir and 0 < self.count_dir_php(www_dir)
+        assert control_dir and 0 < self.count_php_in_dir(control_dir)
+        assert www_dir and 0 < self.count_php_in_dir(www_dir)
         assert control_dir 
         assert control_dir and (
-            TestRatchets.control_php-TestRatchets.plus_minus < self.count_dir_php(control_dir) and
-            TestRatchets.control_php+TestRatchets.plus_minus > self.count_dir_php(control_dir)
+            TestRatchets.control_php-TestRatchets.plus_minus < self.count_php_in_dir(control_dir) and
+            TestRatchets.control_php+TestRatchets.plus_minus > self.count_php_in_dir(control_dir)
             )
         assert www_dir and (
-            TestRatchets.www_php-TestRatchets.plus_minus < self.count_dir_php(www_dir) and
-            TestRatchets.www_php+TestRatchets.plus_minus > self.count_dir_php(www_dir)
+            TestRatchets.www_php-TestRatchets.plus_minus < self.count_php_in_dir(www_dir) and
+            TestRatchets.www_php+TestRatchets.plus_minus > self.count_php_in_dir(www_dir)
             )
-
-
-'''
-t = TestRatchets();
-t.test_directories_exist()
-t.test_dir_files()
-print(t.deploy_dir())
-print(t.www_dir())
-
-# simple version for working with CWD
-print(t.count_directory_files(t.www_dir()))
-print([name for name in os.listdir('.') if os.path.isfile(name)])
-
-# path joining version for other paths
-DIR = '/tmp'
-print(len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))]))
-
-dirname, filename = os.path.split(os.path.abspath(__file__))
-print("running from", dirname)
-print("running towards", )
-print("file is", filename)
-'''
