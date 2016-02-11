1e03d727b28a34727f912d0291cf6f46800b7305
Al Vazquez <al.vazquez@gmail.com>
2016-02-10 23:59:50 -0500

    Add unit test for NWTemplate

---
+++ deploy/tests/unit/NWTemplateUnitTest.php
@@ -0,0 +1,20 @@
+<?php
+require_once(CORE.'/extensions/lib_templates.php');
+use Symfony\Component\HttpFoundation\Session\Storage\MockArraySessionStorage;
+use NinjaWars\core\extensions\SessionFactory;
+
+class NWTemplateUnitTest extends PHPUnit_Framework_TestCase {
+    public function setUp() {
+		SessionFactory::init(new MockArraySessionStorage());
+    }
+
+    public function tearDown() {
+		SessionFactory::annihilate();
+    }
+
+    public function testAssignNull() {
+        $template = new NWTemplate();
+        $result = $template->assignArray(null);
+        $this->assertNull($result);
+    }
+}
