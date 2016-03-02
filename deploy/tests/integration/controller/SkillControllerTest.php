29ab1eeac11345ce59c9054fdecb7e3019aeb75b
Roy Ronalds <tchalvakspam@gmail.com>
2016-03-02 10:14:11 -0500

    Skills Tests: Test using skills on others via url.
    RED

--- deploy/tests/integration/controller/SkillControllerTest.php
+++ deploy/tests/integration/controller/SkillControllerTest.php
@@ -8,35 +8,55 @@ use NinjaWars\core\extensions\SessionFactory;
 class SkillControllerTest extends PHPUnit_Framework_TestCase {
 	public function setUp() {
         $this->char = TestAccountCreateAndDestroy::char();
+        $this->char2 = Player::find(TestAccountCreateAndDestroy::char_id_2());
+        SessionFactory::init(new MockArraySessionStorage());
         $session = SessionFactory::getSession();
         $session->set('player_id', $this->char->id()); // Mock the login.
-        // Mock the post request.
+
         $request = new Request([], []);
         RequestWrapper::inject($request);
-		SessionFactory::init(new MockArraySessionStorage());
 	}
 
 	public function tearDown() {
         $this->char = null;
+        $this->char2 = null;
         TestAccountCreateAndDestroy::destroy();
         RequestWrapper::inject(new Request([]));
         $session = SessionFactory::getSession();
         $session->invalidate();
     }
 
-    public function testSkillIndexDoesNotError() {
-        var_dump(self_char_id());
+    public function testLoggedInSkillsDisplay(){
         $skill = new SkillController();
         $skill_outcome = $skill->index();
         $this->assertNotEmpty($skill_outcome);
+        $this->assertEquals('Your Skills', $skill_outcome['title']);
     }
 
-    public function testLoggedInSkillsDisplay(){
-        var_dump(self_char_id());
+    public function testUseFireboltOnAnotherChar(){
+        $passfail = $this->char->setClass('tiger');
+        $this->assertNull($passfail);
+        $initial_health = $this->char2->health();
+        $request = Request::create('/skill/use/Fire%20Bolt/'.url($this->char2->name()).'/');
+        RequestWrapper::inject($request);
+        $final_defender = Player::find($this->char2->id());
         $skill = new SkillController();
-        $skill_outcome = $skill->index();
-        $this->assertEquals('bob', $skill_outcome['parts']['error']);
+        $skill_outcome = $skill->go();
+        $this->assertLessThan($initial_health, $final_defender->health());
     }
 
+    public function testUseHealOnSelfAsDragon(){
+        $passfail = $this->char->setClass('dragon');
+        $this->assertNull($passfail);
+        $this->char->save();
 
+        $initial_health = $this->char2->health();
+
+        $request = Request::create('/skill/self_use/Heal/');
+        RequestWrapper::inject($request);
+        $final_defender = Player::find($this->char2->id());
+        $skill = new SkillController();
+        $skill_outcome = $skill->selfUse();
+        $this->assertLessThan($initial_health, $final_defender->health());
+    }
 }
