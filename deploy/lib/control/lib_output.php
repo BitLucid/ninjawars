957dc1cc103fff568707cae63e20dfc598c196b0
Al Vazquez <al.vazquez@gmail.com>
2016-03-05 19:24:02 -0500

    Move indefinite article function to InventoryController

--- deploy/lib/control/lib_output.php
+++ deploy/lib/control/lib_output.php
@@ -1,9 +1,6 @@
 <?php
 // See also: the lib_input functions for filter methods.
 
-function get_indefinite_article($p_noun) {
-	return str_replace(' '.$p_noun, '', shell_exec('perl '.LIB_ROOT.'third-party/lingua-a.pl "'.escapeshellcmd($p_noun).'"'));
-}
 // Need to cover out to html, and out to database in here somewhere, I think.
 
 
