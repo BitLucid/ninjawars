bc468207c17d9c801e9b486eddf549daad96a921
Al Vazquez <al.vazquez@gmail.com>
2016-03-13 12:47:27 -0400

    Add integration tests for StatsController

---
+++ deploy/tests/integration/controller/StatsControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Storage\MockArraySessionStorage;
+use NinjaWars\core\environment\RequestWrapper;
+use NinjaWars\core\extensions\SessionFactory;
+use NinjaWars\core\control\StatsController;
+
+class StatsControllerTest extends PHPUnit_Framework_TestCase {
+    private $controller;
+
+    public function __construct() {
+        $this->controller = new StatsController();
+    }
+
+	protected function setUp() {
+		SessionFactory::init(new MockArraySessionStorage());
+        $char_id = TestAccountCreateAndDestroy::create_testing_account();
+		SessionFactory::getSession()->set('player_id', $char_id);
+    }
+
+	protected function tearDown() {
+        RequestWrapper::destroy();
+        $session = SessionFactory::getSession();
+        $session->invalidate();
+    }
+
+    public function testIndex() {
+        $response = $this->controller->index();
+
+        $this->assertArrayHasKey('template', $response);
+    }
+}
