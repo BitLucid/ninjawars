44350303964c5deff9e716c7a5eb92fd8f7e2f7c
Roy Ronalds <tchalvakspam@gmail.com>
2016-03-17 23:19:15 -0400

    Items: A variety of more pixel item icons.

---
+++ deploy/db/migrations/20160318_more_item_icons.sql
@@ -0,0 +1,4 @@
+update item set image = 'ginseng_root.png' where item_internal_name = 'ginsengroot';
+update item set image = 'tiger_salve.png' where item_internal_name = 'tigersalve';
+update item set image = 'smoke_bomb.png' where item_internal_name = 'smokebomb';
+update item set image = 'sake.png' where item_internal_name = 'sake';
