1d60efb1ed77bbbf0ca2f37e9209bfcd1496d87b
Roy Ronalds <tchalvakspam@gmail.com>
2016-03-17 23:40:55 -0400

    Items: Add mirror_shard icon.

--- deploy/db/migrations/20160318_more_item_icons.sql
+++ deploy/db/migrations/20160318_more_item_icons.sql
@@ -2,3 +2,4 @@ update item set image = 'ginseng_root.png' where item_internal_name = 'ginsengro
 update item set image = 'tiger_salve.png' where item_internal_name = 'tigersalve';
 update item set image = 'smoke_bomb.png' where item_internal_name = 'smokebomb';
 update item set image = 'sake.png' where item_internal_name = 'sake';
+update item set image = 'mirror_shard.png' where item_internal_name = 'mirror';
