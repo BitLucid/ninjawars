d3f58395dca8aec6e5e4fe8fefe32547e474cb84
Roy Ronalds <tchalvakspam@gmail.com>
2016-03-19 16:22:11 -0400

    Confirm.php: Moved to AssistanceController, WIP.

--- deploy/lib/control/AssistanceController.php
+++ deploy/lib/control/AssistanceController.php
@@ -3,6 +3,7 @@
 namespace NinjaWars\core\control;
 
 use \Nmail;
+use NinjaWars\core\data\DatabaseConnection;
 
 /**
  * Give assistance to players and pre-player-users
@@ -123,10 +124,61 @@ class AssistanceController{
             'username'=>$username,
             ];
         return [
-            'template'=>'account_issues.tpl',
-            'title'=>'Account Problems',
+            'template'=>'assistance.tpl',
+            'title'=>'Account Assistance',
             'parts'=>$parts,
             'options'=>['quickstat'=>false]
             ];
     }
+
+    /**
+     * Handle account email confirmation
+     */
+    public function confirm(){
+        $admin_override_pass       = 'WeAllowIt'; // Just a weak passphrase for simply confirming players.
+        $admin_override_request    = in('admin_override');
+        $acceptable_admin_override = ($admin_override_pass === $admin_override_request);
+        $confirm                   = in('confirm');
+        $aid                       = positive_int(in('aid'));
+
+        $data = query_row('
+            SELECT player_id, uname, accounts.verification_number as verification_number,
+            CASE WHEN active = 1 THEN 1 ELSE 0 END AS active, accounts.active_email,
+            CASE WHEN accounts.confirmed = 1 THEN 1 ELSE 0 END as confirmed, status, member, days, players.created_date
+            FROM accounts JOIN account_players ON _account_id = account_id JOIN players ON _player_id = player_id
+            WHERE account_id = :acctId', array(':acctId'=>$aid));
+
+        if (rco($data)) {
+            $check     = $data['verification_number'];
+            $confirmed = $data['confirmed'];
+            $active    = $data['active'];
+            $username  = $data['uname'];
+        } else {
+            $active    =
+            $check     =
+            $confirmed =
+            $username  = null;
+        }
+
+        $confirmation_confirmed = false;
+
+        if ($confirmed != 1 && (($check && $confirm && $confirm == $check) || $acceptable_admin_override)) {
+            // Confirmation number matches whats in the database and neither is null, or the admin override was met.
+            query('UPDATE accounts SET operational = true, confirmed=1 WHERE account_id = :accountID', array(':accountID'=>$aid));
+
+            $statement = DatabaseConnection::$pdo->prepare('UPDATE players SET active = 1 WHERE player_id in (SELECT _player_id FROM account_players WHERE _account_id = :accountID)');
+            $statement->bindValue(':accountID', $aid);
+            $statement->execute();  // todo - test for success
+
+            $confirmation_confirmed = true;
+        }
+
+        return [
+            'template'=>'assistance.confirm.tpl',
+            'title'=>'Account Confirmation',
+            'parts'=>get_certain_vars(get_defined_vars()),
+            'options'=>['quickstat'=>false]
+            ];
+    }
+
 }
