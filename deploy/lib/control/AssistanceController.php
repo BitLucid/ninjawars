2c2236f8c04951a9678915608a1fe5f75b2dce5c
Roy Ronalds <tchalvakspam@gmail.com>
2016-03-19 22:53:44 -0400

    AssistanceController: Modify spacing and lines.

--- deploy/lib/control/AssistanceController.php
+++ deploy/lib/control/AssistanceController.php
@@ -6,77 +6,74 @@ use \Nmail;
 use NinjaWars\core\data\DatabaseConnection;
 
 /**
- * Give assistance to players and pre-player-users
+ * Give assistance to players and proto-players who anonymous users
  */
 class AssistanceController{
     const PRIV          = false;
     const ALIVE         = false;
 
-        /**
-         * Determines the user information for a certain email.
-         */
-        private function userHavingEmail($email) {
-            $data = query_row('SELECT uname, level, accounts.confirmed, accounts.verification_number, accounts.active_email, account_id, CASE WHEN active::bool THEN 1 ELSE 0 END AS active
-                from accounts LEFT JOIN account_players ON account_id = _account_id LEFT JOIN players on _player_id = player_id 
-                WHERE active_email = :email limit 1;',
-                    array(':email'=>$email));
-            return $data;
-        }
+    /**
+     * Determines the user information for a certain email.
+     */
+    private function userHavingEmail($email) {
+        $data = query_row('SELECT uname, level, account_id, accounts.confirmed, 
+            accounts.verification_number, accounts.active_email,
+            CASE WHEN active::bool THEN 1 ELSE 0 END AS active
+            from accounts LEFT JOIN account_players ON account_id = _account_id 
+            LEFT JOIN players on _player_id = player_id 
+            WHERE trim(lower(active_email)) = trim(lower(:email)) limit 1;',
+                array(':email'=>$email));
+        return $data;
+    }
 
-        /**
-         * Sends an email for the user's account data.
-         */
-        private function sendAccountEmail($email, $data) {
-            $_from = array(SYSTEM_EMAIL=>SYSTEM_EMAIL_NAME);
+    /**
+     * Sends an email for the user's account data.
+     */
+    private function sendAccountEmail($email, $data) {
+        $_from = array(SYSTEM_EMAIL=>SYSTEM_EMAIL_NAME);
+        /* additional headers */
+        $_to = array("$email"=>$data['uname']);
+        $_subject = 'NinjaWars Account Info Request';
+        $_body = render_template('email.assistance.account.tpl', [
+                'lost_uname'   => $data['uname'],
+                'active_email' => $data['active_email'],
+                'confirmed'  => $data['confirmed'],
+                'level' => $data['level']
+            ]
+        );
 
-            /* additional headers */
-            $_to = array("$email"=>$data['uname']);
-            $_subject = 'NinjaWars Account Info Request';
-            $_body = render_template('email.assistance.account.tpl', [
-                    'lost_uname'   => $data['uname'],
-                    'active_email' => $data['active_email'],
-                    'confirmed'  => $data['confirmed'],
-                    'level' => $data['level']
-                ]
-            );
+        $mail_obj = new Nmail($_to, $_subject, $_body, $_from);
+        // *** Set the custom replyto email. ***
+        $mail_obj->setReplyTo(array(SUPPORT_EMAIL=>SUPPORT_EMAIL_NAME));
+        return $mail_obj->send();
+    }
 
-            $mail_obj = new Nmail($_to, $_subject, $_body, $_from);
-
-            // *** Set the custom replyto email. ***
-            $mail_obj->setReplyTo(array(SUPPORT_EMAIL=>SUPPORT_EMAIL_NAME));
-
-            return $mail_obj->send();
-        }
-
-        /**
-         * Sends the account confirmation email.
-         */
-        private function sendConfirmationEmail($email, $data) {
-            $lost_confirm = $data['verification_number'];
-            $lost_uname   = $data['uname'];
-
-            $_from = array(SYSTEM_EMAIL=>SYSTEM_EMAIL_NAME);
-            $_to = array("$email"=>$data['uname']);
-            $_subject = "NinjaWars Account Confirmation Info";
-            $_body = render_template('email.assistance.confirmation.tpl', array(
-                    'lost_uname'     => $lost_uname
-                    , 'lost_confirm' => $lost_confirm
-                    , 'account_id'   => $data['account_id']
-                )
-            );
-
-            $mail_obj = new Nmail($_to, $_subject, $_body, $_from);
-            $mail_obj->setReplyTo(array(SUPPORT_EMAIL=>SUPPORT_EMAIL_NAME));
-
-            return $mail_obj->send();
-        }
+    /**
+     * Sends the account confirmation email.
+     */
+    private function sendConfirmationEmail($email, $data) {
+        $lost_confirm = $data['verification_number'];
+        $lost_uname   = $data['uname'];
+        $_from = array(SYSTEM_EMAIL=>SYSTEM_EMAIL_NAME);
+        $_to = array("$email"=>$data['uname']);
+        $_subject = "NinjaWars Account Confirmation Info";
+        $_body = render_template('email.assistance.confirmation.tpl', array(
+                'lost_uname'     => $lost_uname
+                , 'lost_confirm' => $lost_confirm
+                , 'account_id'   => $data['account_id']
+            )
+        );
+        $mail_obj = new Nmail($_to, $_subject, $_body, $_from);
+        $mail_obj->setReplyTo(array(SUPPORT_EMAIL=>SUPPORT_EMAIL_NAME));
+        return $mail_obj->send();
+    }
 
 
     /**
      * Display the assistance options to users.
      */
     public function index(){
-        $email = in('email', null, 'sanitize_to_email'); // The default filter allows standard emails.
+        $email = in('email', null, 'sanitize_to_email');
         $password_request = in('password_request');
         $confirmation_request = in('confirmation_request');
 
@@ -91,20 +88,18 @@ class AssistanceController{
         } else if ($email) {
             $data = $this->userHavingEmail($email);
             $username = $data['uname'];
-
             if (!$data['uname']) {
                 $error = 'nouser';
             } elseif ($password_request && $data['active']) {
                 $sent = $this->sendAccountEmail($email, $data);
-
                 $attemptedToSendEmail = true;
             } else {
                 // Confirmation request.
                 if ($data['confirmed']) {
                     $error = 'alreadyconfirmed';
                 } else {
-                    $attemptedToSendEmail = true;
                     $sent = $this->sendConfirmationEmail($email, $data);
+                    $attemptedToSendEmail = true;
                 }
             }
         }
@@ -112,9 +107,6 @@ class AssistanceController{
         if ($attemptedToSendEmail && !$sent) {
             $error = 'emailfail';
         }
-
-        $body_classes = 'account-issues';
-
 
         $parts = [
             'data'=>$data,
@@ -127,7 +119,7 @@ class AssistanceController{
             'template'=>'assistance.tpl',
             'title'=>'Account Assistance',
             'parts'=>$parts,
-            'options'=>['quickstat'=>false]
+            'options'=>['quickstat'=>false, 'body_classes'=>'account-issues']
             ];
     }
 
@@ -142,10 +134,14 @@ class AssistanceController{
         $aid                       = positive_int(in('aid'));
 
         $data = query_row('
-            SELECT player_id, uname, accounts.verification_number as verification_number,
-            CASE WHEN active = 1 THEN 1 ELSE 0 END AS active, accounts.active_email,
-            CASE WHEN accounts.confirmed = 1 THEN 1 ELSE 0 END as confirmed, status, member, days, players.created_date
-            FROM accounts JOIN account_players ON _account_id = account_id JOIN players ON _player_id = player_id
+            SELECT player_id, uname, 
+            accounts.verification_number as verification_number,
+            CASE WHEN active = 1 THEN 1 ELSE 0 END AS active, 
+            accounts.active_email,
+            CASE WHEN accounts.confirmed = 1 THEN 1 ELSE 0 END as confirmed, 
+            status, member, days, players.created_date
+            FROM accounts JOIN account_players ON _account_id = account_id 
+            JOIN players ON _player_id = player_id
             WHERE account_id = :acctId', array(':acctId'=>$aid));
 
         if (rco($data)) {
@@ -162,11 +158,18 @@ class AssistanceController{
 
         $confirmation_confirmed = false;
 
-        if ($confirmed != 1 && (($check && $confirm && $confirm == $check) || $acceptable_admin_override)) {
-            // Confirmation number matches whats in the database and neither is null, or the admin override was met.
-            query('UPDATE accounts SET operational = true, confirmed=1 WHERE account_id = :accountID', array(':accountID'=>$aid));
+        if ($confirmed != 1 && 
+            (($check && $confirm && $confirm == $check) 
+                || $acceptable_admin_override)) {
+            // Confirmation number not null and matches
+            // or the admin override was met.
+            query('UPDATE accounts SET operational = true, confirmed=1 
+                WHERE account_id = :accountID', array(':accountID'=>$aid));
 
-            $statement = DatabaseConnection::$pdo->prepare('UPDATE players SET active = 1 WHERE player_id in (SELECT _player_id FROM account_players WHERE _account_id = :accountID)');
+            $statement = DatabaseConnection::$pdo->prepare(
+                'UPDATE players SET active = 1 WHERE player_id in 
+                (SELECT _player_id FROM account_players 
+                    WHERE _account_id = :accountID)');
             $statement->bindValue(':accountID', $aid);
             $statement->execute();  // todo - test for success
 
