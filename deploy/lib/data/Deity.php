599f5dc3b555cc4190683ddb54480536460407f3
Al Vazquez <al.vazquez@gmail.com>
2016-04-09 15:05:20 -0400

    Move deity functions to Deity

--- deploy/lib/data/Deity.php
+++ deploy/lib/data/Deity.php
@@ -11,6 +11,7 @@ use NinjaWars\core\data\GameLog;
  */
 class Deity {
     const VICIOUS_KILLER_STAT = 4; // the ID of the vicious killer stat
+    const MIDNIGHT_HEAL_SKILL = 5; // the ID of the midnight heal skill
 
     /**
      * @param int $minutes Minute interval of tick.
@@ -60,7 +61,7 @@ class Deity {
             'major_revive_percent' => MAJOR_REVIVE_PERCENT,
         ];
 
-        list($revived, $dead_count) = revive_players($params);
+        list($revived, $dead_count) = self::revivePlayers($params);
 
         $rand = rand(1, DEITY_LOG_CHANCE_DIVISOR);
 
@@ -111,7 +112,7 @@ class Deity {
 
         // *** HEAL Characters a certain amount ***
 
-        heal_characters(); // Just use the defaults, function does not return anything at the moment.
+        self::healCharacters(); // Just use the defaults, function does not return anything at the moment.
 
         // **************
         // Visual output:
@@ -151,8 +152,8 @@ class Deity {
         // *** HEAL ***
         $s = DatabaseConnection::$pdo->prepare(
             "UPDATE players SET health = numeric_smaller(health+:regen_rate, :max_heal) ".
-                 "WHERE health BETWEEN 1 AND :max_heal2 AND NOT ".
-                 "CAST(status&:poison AS boolean)"
+            "WHERE health BETWEEN 1 AND :max_heal2 AND NOT ".
+            "CAST(status&:poison AS boolean)"
         );
         $s->bindValue(':max_heal', HEALTH_REGEN_THRESHOLD);
         $s->bindValue(':max_heal2', HEALTH_REGEN_THRESHOLD);
@@ -208,7 +209,7 @@ class Deity {
         DatabaseConnection::getInstance();
         DatabaseConnection::$pdo->query('BEGIN TRANSACTION');
         $affected_rows = [];
-        $affected_rows['Increase Days Of Players'] = update_days();
+        $affected_rows['Increase Days Of Players'] = self::updateDays();
 
         $status_removal = DatabaseConnection::$pdo->query("UPDATE players SET status = 0");  // Hmmm, gets rid of all status effects, we may want to make that not have that limitation, some day.
         $affected_rows['Statuses Removed'] = $status_removal->rowCount();
@@ -221,53 +222,53 @@ class Deity {
             $update->bindValue(':viciousKiller', $killer);
             $update->bindValue(':viciousKillerStat', self::VICIOUS_KILLER_STAT);
             $update->execute();
-        }
+    }
 
-        //Nightly Unconfirm old players script settings.
-        $unconfirmed = self::unconfirmOlderPlayersOverMinimums($keep_players_until_over_the_number, $days_players_have_to_be_older_than_to_be_unconfirmed, $maximum_players_to_unconfirm, $just_testing=false);
-        assert($unconfirmed < $maximum_players_to_unconfirm+1);
+    //Nightly Unconfirm old players script settings.
+    $unconfirmed = self::unconfirmOlderPlayersOverMinimums($keep_players_until_over_the_number, $days_players_have_to_be_older_than_to_be_unconfirmed, $maximum_players_to_unconfirm, $just_testing=false);
+    assert($unconfirmed < $maximum_players_to_unconfirm+1);
 
-        $affected_rows['Players Unconfirmed'] = ($unconfirmed === false ? 'Under the Minimum number of players' : $unconfirmed);
+    $affected_rows['Players Unconfirmed'] = ($unconfirmed === false ? 'Under the Minimum number of players' : $unconfirmed);
 
-        // Delete from inventory where owner is unconfirmed or non-existent.
-        $deleted_items = DatabaseConnection::$pdo->query("DELETE FROM inventory WHERE owner IN (SELECT owner FROM inventory LEFT JOIN players ON owner = player_id WHERE uname IS NULL GROUP BY owner)");
-        $affected_rows['deleted items'] = $deleted_items->rowCount();
+    // Delete from inventory where owner is unconfirmed or non-existent.
+    $deleted_items = DatabaseConnection::$pdo->query("DELETE FROM inventory WHERE owner IN (SELECT owner FROM inventory LEFT JOIN players ON owner = player_id WHERE uname IS NULL GROUP BY owner)");
+    $affected_rows['deleted items'] = $deleted_items->rowCount();
 
-        $deleted_items = DatabaseConnection::$pdo->query("delete from levelling_log where killsdate < (now() - interval '2 months')");
-        $affected_rows['deleted levelling_logs'] = $deleted_items->rowCount();
+    $deleted_items = DatabaseConnection::$pdo->query("delete from levelling_log where killsdate < (now() - interval '2 months')");
+    $affected_rows['deleted levelling_logs'] = $deleted_items->rowCount();
 
-        $deleted_mail = Messages::deleteOldMessages();
-        $deleted_events = Events::deleteOldEvents();
-        $affected_rows['Old Messages Deletion'] = $deleted_mail;
-        $affected_rows['Old Events Deletion'] = $deleted_events;
+    $deleted_mail = Messages::deleteOldMessages();
+    $deleted_events = Events::deleteOldEvents();
+    $affected_rows['Old Messages Deletion'] = $deleted_mail;
+    $affected_rows['Old Events Deletion'] = $deleted_events;
 
-        $level_log_delete = DatabaseConnection::$pdo->query("delete from levelling_log where killsdate < now()- interval '3 months'");
-        $affected_rows['levelling log deletion'] = $level_log_delete->rowCount(); // Keep only the last 3 months of logs.
+    $level_log_delete = DatabaseConnection::$pdo->query("delete from levelling_log where killsdate < now()- interval '3 months'");
+    $affected_rows['levelling log deletion'] = $level_log_delete->rowCount(); // Keep only the last 3 months of logs.
 
-        $duel_log_delete = DatabaseConnection::$pdo->query("delete from dueling_log where date != cast(now() AS date) AND date != cast(now() AS date)-1"); // Keep only the last two days of duels.
-        $affected_rows['dueling log deletion'] = $duel_log_delete->rowCount();
+    $duel_log_delete = DatabaseConnection::$pdo->query("delete from dueling_log where date != cast(now() AS date) AND date != cast(now() AS date)-1"); // Keep only the last two days of duels.
+    $affected_rows['dueling log deletion'] = $duel_log_delete->rowCount();
 
-        $level_1_delete = DatabaseConnection::$pdo->query("delete from players where active = 0 and level = 1 and created_date < (now() - interval '5 days')"); // Delete old level 1's.
-        DatabaseConnection::$pdo->query('COMMIT');
-        $affected_rows['old level 1 players deletion'] = $level_1_delete->rowCount();
+    $level_1_delete = DatabaseConnection::$pdo->query("delete from players where active = 0 and level = 1 and created_date < (now() - interval '5 days')"); // Delete old level 1's.
+    DatabaseConnection::$pdo->query('COMMIT');
+    $affected_rows['old level 1 players deletion'] = $level_1_delete->rowCount();
 
 
-        $logMessage .= "DEITY_NIGHTLY: Deity reset occurred at server date/time: ".date('l jS \of F Y h:i:s A').".\n";
-        $logMessage .= 'DEITY_NIGHTLY: Mail deleted: ('.$affected_rows['Old Messages Deletion'].")\n";
-        $logMessage .= "DEITY_NIGHTLY: Items: ".$affected_rows['deleted items']."\n";
-        $logMessage .= 'DEITY_NIGHTLY: Players unconfirmed: ('.$unconfirmed.").  30 is the current default maximum.\n";
-        $logMessage .= "DEITY_NIGHTLY: Chats deleted (if a deletion value is returned): $deleted\n";
+    $logMessage .= "DEITY_NIGHTLY: Deity reset occurred at server date/time: ".date('l jS \of F Y h:i:s A').".\n";
+    $logMessage .= 'DEITY_NIGHTLY: Mail deleted: ('.$affected_rows['Old Messages Deletion'].")\n";
+    $logMessage .= "DEITY_NIGHTLY: Items: ".$affected_rows['deleted items']."\n";
+    $logMessage .= 'DEITY_NIGHTLY: Players unconfirmed: ('.$unconfirmed.").  30 is the current default maximum.\n";
+    $logMessage .= "DEITY_NIGHTLY: Chats deleted (if a deletion value is returned): $deleted\n";
 
-        // **************
-        // Visual output:
+    // **************
+    // Visual output:
 
-        foreach ($affected_rows AS $loopKey => $loopRowResult) {
-            $logMessage .= "DEITY_NIGHTLY: Result type: $loopKey yeilded result: $loopRowResult\n";
-        }
+    foreach ($affected_rows AS $loopKey => $loopRowResult) {
+        $logMessage .= "DEITY_NIGHTLY: Result type: $loopKey yeilded result: $loopRowResult\n";
+    }
 
-        $logMessage .= "DEITY_NIGHTLY ENDING: ---- ".date(DATE_RFC1036)." ---- \n";
+    $logMessage .= "DEITY_NIGHTLY ENDING: ---- ".date(DATE_RFC1036)." ---- \n";
 
-        self::logStuff($logMessage);
+    self::logStuff($logMessage);
 
     }
 
@@ -291,26 +292,180 @@ class Deity {
         if ($current_players < $keep_players) {
             // *** If we're under the minimum, don't inactivate anyone.
             return false;
+    }
+
+    // *** Don't unconfirm anyone below the minimum floor.
+    $unconfirm_days_over = max($unconfirm_days_over, $minimum_days);
+
+    // Unconfirm at a maximum of 20 players at a time.
+    $unconfirm_within_limits = "UPDATE players
+        SET active = :active
+        WHERE players.player_id
+        IN (
+            SELECT player_id FROM players
+            WHERE active = 1
+            AND days > :age
+            ORDER BY player_id DESC	LIMIT :max)";
+$update = DatabaseConnection::$pdo->prepare($unconfirm_within_limits);
+$update->bindValue(':active', $change_confirm_to);
+$update->bindValue(':age', intval($unconfirm_days_over));
+$update->bindValue(':max', $max_to_unconfirm);
+$update->execute();
+
+return $update->rowCount();
+    }
+    /**
+     */
+    private static function updateDays() {
+        DatabaseConnection::getInstance();
+        $players = DatabaseConnection::$pdo->query("UPDATE players SET days = days+1");
+        return $players->rowCount();
+    }
+
+    /**
+     * Take all characters, and heal them one step closer to their maximum base.
+     */
+    private static function healCharacters($basic=8, $with_level=true){
+        $maximum_heal = Player::maxHealthByLevel(3);
+    /*
+    Goal:  Faster regen for higher level.
+    See the balance sheet:
+    https://docs.google.com/spreadsheet/ccc?pli=1&key=0AkoUgtBBP00HdGs0Tmk4bC10TXN0SUJYXzdYMVpFZFE#gid=0
+     */
+        $max_hp = Player::maxHealthByLevel(MAX_PLAYER_LEVEL);
+
+
+        $level_add = '+ cast(floor(level/10) AS int)';
+        if(!$with_level){
+            $level_add = '';
+        }
+        // Take level, divide by 10, throw away remainder, and add ten for every whole tenth level.
+        // e.g. 99 / 10 = 9.9 floored = 9 * 10 = 90
+        $level_limit = 'cast((floor(level /10) * 10) AS int)';
+        // Add an amount
+        $s = DatabaseConnection::$pdo->prepare(
+            "UPDATE players SET health = numeric_smaller(
+                (health+:basic ".$level_add."),
+        cast((:max_heal + ".$level_limit.") AS int))
+        WHERE health > 0 AND NOT cast(status&:poison AS bool) AND health < (:max_heal2 + ".$level_limit.")");
+        // Heal a character that is alive, and isn't at their level max yet.
+        $s->bindValue(':basic', $basic);
+        $s->bindValue(':max_heal', $maximum_heal);
+        $s->bindValue(':max_heal2', $maximum_heal);
+        $s->bindValue(':poison', POISON);
+        $s->execute();
+        DatabaseConnection::$pdo->query('COMMIT');
+        // Higher levels now heal faster.
+        // Higher levels should now also heal to a larger maximum, level dependent.
+        // e.g. level 100 gets +100 in how many hitpoints they'll heal up to,
+        // level 99 gets +90 in how many hitpoints they'll heal up to.
+    }
+
+    /**
+     * Revive up to a small max in minor hours, and a stable percent on major hours.
+     * Defaults
+     * sample_use: revive_players(array('just_testing'=>true));
+     * @param array('minor_revive_to'=>100, 'major_revive_percent'=>5,
+     *      'just_testing'=>false)
+     */
+    private static function revivePlayers($params=array()) {
+        // Previous min/max was 2-4% always, ~3000 players, so 60-120 each time.
+
+        $minor_revive_to      = (isset($params['minor_revive_to']) ? $params['minor_revive_to'] : 100); // minor_revive_to, default 100
+        $major_revive_percent = (isset($params['major_revive_percent']) ? $params['major_revive_percent'] : 5); // major_revive_percent, default 5%
+        $just_testing         = isset($params['just_testing']);
+        $major_hour           = 3; // Hour for the major revive.
+
+        /*
+         * General idea should be:
+         * 1: revive to 100
+         * 2: revive to 100 (probably 0)
+         * 3: revive 150, (250 total) to a max of 80% of total, ~2500.
+         * 4: revive to 100 (almost certainly no more)
+         * 5: revive to 100 (almost certainly no more)
+         * 6: revive 150, (400 total) to a max of 80% of total, ~2500
+         * 7: ...etc.
+         */
+
+        // Determine the total dead (& active).
+        $sel_dead = DatabaseConnection::$pdo->query('SELECT count(*) FROM players WHERE health < 1 AND active = 1');
+        $dead_count = $sel_dead->fetchColumn();
+
+        // If none dead, return false.
+        if (!$dead_count) {
+            return array(0, 0);
         }
 
-        // *** Don't unconfirm anyone below the minimum floor.
-        $unconfirm_days_over = max($unconfirm_days_over, $minimum_days);
+        // Determine the total active.
+        $sel_total_active = DatabaseConnection::$pdo->query('SELECT count(*) FROM players WHERE active = 1');
+        $total_active = $sel_total_active->fetchColumn();
 
-        // Unconfirm at a maximum of 20 players at a time.
-        $unconfirm_within_limits = "UPDATE players
-            SET active = :active
-            WHERE players.player_id
-            IN (
-                SELECT player_id FROM players
-                WHERE active = 1
-                AND days > :age
-                ORDER BY player_id DESC	LIMIT :max)";
-        $update = DatabaseConnection::$pdo->prepare($unconfirm_within_limits);
-        $update->bindValue(':active', $change_confirm_to);
-        $update->bindValue(':age', intval($unconfirm_days_over));
-        $update->bindValue(':max', $max_to_unconfirm);
+        // Calc the total alive.
+        $total_alive = ($total_active - $dead_count);
+
+        // Determine major or minor based on the hour.
+        $sel_current_time = DatabaseConnection::$pdo->query("SELECT amount FROM time WHERE time_label = 'hours'");
+        $current_time = $sel_current_time->fetchColumn();
+        assert(is_numeric($current_time));
+
+        $major = (($current_time % $major_hour) == 0);
+
+        // If minor, and total_alive is more than minor_revive_to-1, return 0/total.
+        if (!$major) { // minor
+            if ($total_alive > ($minor_revive_to-1)) { // minor_revive_to already met.
+                return array(0, $dead_count);
+            } else {  // else revive minor_revive_to - total_alive.
+                $revive_amount = floor($minor_revive_to - $total_alive);
+            }
+        } else { // major.
+            $percent_int = floor(($major_revive_percent/100)*$total_active);
+
+            if ($dead_count < $percent_int) {
+                // If major, and total_dead is less than target_num (major_revive_percent*total, floored)
+                // just revive those that are dead.
+                $revive_amount = $dead_count;
+            } else {
+                // Else revive target_num (major_revive_percent*total, floored)
+                $revive_amount = $percent_int;
+            }
+        }
+
+        assert(isset($revive_amount));
+        assert(isset($current_time));
+        assert(isset($just_testing));
+        assert(isset($dead_count));
+        assert(isset($major));
+        // Actually perform the revive on those integers.
+        // Use the order by clause to determine who revives, by time, days and then by level, using the limit set previously.
+        //select uname, player_id, level,floor(($major_revive_percent/100)*$total_active) days, resurrection_time from players where active = 1 AND health < 1 ORDER BY abs(8 - resurrection_time) asc, level desc, days asc
+        $select = 'SELECT player_id FROM players WHERE active = 1 AND health < 1 '.
+            ' ORDER BY abs(:time - resurrection_time) ASC, level DESC, days ASC LIMIT :amount';
+
+        $up_revive_players= 'UPDATE players SET status = 0 ';
+
+        if (!$just_testing) {
+            $up_revive_players .= ', health =
+                CASE WHEN level >= coalesce(class_skill_level, skill_level)
+                    THEN (150+(level*3))
+                    ELSE (100+(level*3)) END
+                        FROM (SELECT * FROM skill LEFT JOIN class_skill ON skill_id = _skill_id WHERE skill_id = :midnightHeal)
+                        AS class_skill ';
+        }
+
+        $up_revive_players .= ' WHERE player_id IN ('.$select.') ';
+
+        if (!$just_testing) {
+            $up_revive_players .= ' AND coalesce(class_skill._class_id, players._class_id) = players._class_id';
+        }
+
+        $update = DatabaseConnection::$pdo->prepare($up_revive_players);
+        $update->bindValue(':amount', intval($revive_amount));
+        $update->bindValue(':time', intval($current_time));
+        $update->bindValue(':midnightHeal', self::MIDNIGHT_HEAL_SKILL);
         $update->execute();
+        $truly_revived = $update->rowCount();
 
-        return $update->rowCount();
+        // Return the 'revived/total' actually revived.
+        return array($truly_revived, $dead_count);
     }
 }
