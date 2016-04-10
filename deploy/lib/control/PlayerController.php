8c6067eadfd5c911ed4b7d6f9236e7151d173a68
Al Vazquez <al.vazquez@gmail.com>
2016-04-10 11:20:06 -0400

    Simplify PlayerController::index(), reduce variable count

--- deploy/lib/control/PlayerController.php
+++ deploy/lib/control/PlayerController.php
@@ -17,26 +17,8 @@ class PlayerController extends AbstractController {
     const ALIVE = false;
 
     public function index() {
-        $target    = $player = first_value(in('ninja'), in('player'), in('find'), in('target'));
+        $target    = first_value(in('ninja'), in('player'), in('find'), in('target'));
         $target_id = first_value(in('target_id'), in('player_id'));
-
-        $account               = null;
-        $attack_error          = null;
-        $char_info             = null;
-        $clan                  = null;
-        $combat_skills         = null;
-        $display_clan_options  = null;
-        $gravatar_url          = null;
-        $items                 = null;
-        $kills_today           = null;
-        $player_info           = null;
-        $rank_spot             = null;
-        $same_clan             = false;
-        $self                  = null;
-        $status_list           = null;
-        $targeted_skills       = null;
-        $viewed_name_for_title = null;
-        $viewing_player_obj    = null;
 
         if ($target_id) {
             $target_player_obj = Player::find($target_id);
@@ -44,136 +26,91 @@ class PlayerController extends AbstractController {
             $target_player_obj = Player::findByName($target);
         }
 
-        if ($target_player_obj !== null) {
-            $viewed_name_for_title = $target_player_obj->name();
-        }
-
         if ($target_player_obj === null) {
-            $template = 'no-player.tpl';
-            $parts    = array();
+            $template              = 'no-player.tpl';
+            $viewed_name_for_title = null;
+            $parts                 = array();
         } else {
-            $account = Account::findByChar($target_player_obj);
-            $player_info = $target_player_obj->as_array(); // Pull the info out of the object.
+            $attack_allowed        = false;
+            $attack_error          = 'You must become a ninja first.';
+            $clan                  = ClanFactory::clanOfMember($target_player_obj->id());
+            $combat_skills         = null;
+            $communication_section = '';
+            $display_clan_options  = false;
+            $items                 = null;
+            $player_clan_section   = '';
+            $same_clan             = false;
+            $self                  = false;
+            $set_bounty_section    = '';
+            $targeted_skills       = null;
+            $template              = 'player.tpl';
+            $viewed_name_for_title = $target_player_obj->name();
+            $viewers_clan          = null;
+            $viewing_player_obj    = Player::find(SessionFactory::getSession()->get('player_id'));
 
-            if (!$player_info) {
-                $template = 'no-player.tpl';
-                $parts    = array();
-            } else {
-                $viewing_player_obj = Player::find(SessionFactory::getSession()->get('player_id'));
+            $kills_today = query_item(
+                'SELECT sum(killpoints) FROM levelling_log WHERE _player_id = :player_id AND killsdate = CURRENT_DATE AND killpoints > 0',
+                [':player_id'=>$target_player_obj->id()]
+            );
 
-                // Record whether this is a self-viewing.
-                $self = ($viewing_player_obj && $viewing_player_obj->id() === $target_player_obj->id());
+            $rank_spot = query_item(
+                'SELECT rank_id FROM rankings WHERE player_id = :player_id limit 1',
+                [':player_id'=>$target_player_obj->id()]
+            );
 
-                if ($viewing_player_obj !== null) {
-                    $char_info = $viewing_player_obj->dataWithClan();
-                    $char_id   = $viewing_player_obj->id();
-                    $username  = $viewing_player_obj->name();
-                } else {
-                    $char_info = [];
-                }
+            if ($viewing_player_obj !== null) {
+                $viewers_clan   = ClanFactory::clanOfMember($viewing_player_obj);
+                $self           = ($viewing_player_obj->id() === $target_player_obj->id());
+                $params         = ['required_turns'=>0, 'ignores_stealth'=>true];
+                $AttackLegal    = new AttackLegal($viewing_player_obj, $target_player_obj, $params);
+                $attack_allowed = $AttackLegal->check(false);
+                $attack_error   = $AttackLegal->getError();
 
-                $player    = $target = $player_info['uname']; // reset the target and target_id vars.
-                $target_id = $player_info['player_id'];
-
-                // Get the player's kills for this date.
-                $kills_today = query_item('select sum(killpoints) from levelling_log where _player_id = :player_id and killsdate = CURRENT_DATE and killpoints > 0', array(':player_id'=>$target_id));
-
-                $viewers_clan       = ($viewing_player_obj !== null ? ClanFactory::clanOfMember($viewing_player_obj) : null);
-
-                // Attack Legal section
-                $params          = array('required_turns'=>0, 'ignores_stealth'=>true); // 0 for unstealth.
-                $attack_error = 'You must become a ninja first.';
-                $attack_allowed = false;
-                if (null !== $viewing_player_obj) {
-                    $AttackLegal     = new AttackLegal($viewing_player_obj, $target_player_obj, $params);
-                    $attack_allowed  = $AttackLegal->check(false);
-                    $attack_error    = $AttackLegal->getError();
-                }
-
-                $sel_rank_spot = "SELECT rank_id FROM rankings WHERE player_id = :char_id limit 1";
-                $rank_spot = query_item($sel_rank_spot, array(':char_id'=>$player_info['player_id']));
-
-                // Display the player info.
-                $status_list          = Player::getStatusList($player);
-                $gurl = $gravatar_url = $target_player_obj->avatarUrl();
-
-                if ($viewing_player_obj !== null && !$attack_error && !$self) { // They're not dead or otherwise unattackable.
-                    // Attack or Duel
+                if (!$attack_error && !$self) { // They're not dead or otherwise unattackable.
+                    // Pull the items and some necessary data about them.
+                    $inventory = new Inventory($viewing_player_obj);
+                    $items     = $inventory->counts();
 
                     $skillDAO = new SkillDAO();
 
-                    $is_admin = false;
-                    if($viewing_player_obj){
-                        $is_admin = $viewing_player_obj->isAdmin();
-                    }
-
-                    if(!$is_admin){
-                        $combat_skills = $skillDAO->getSkillsByTypeAndClass($viewing_player_obj->_class_id, 'combat', $viewing_player_obj->level);
+                    if (!$viewing_player_obj->isAdmin()) {
+                        $combat_skills   = $skillDAO->getSkillsByTypeAndClass($viewing_player_obj->_class_id, 'combat', $viewing_player_obj->level);
                         $targeted_skills = $skillDAO->getSkillsByTypeAndClass($viewing_player_obj->_class_id, 'targeted', $viewing_player_obj->level);
                     } else {
-                        $combat_skills = $skillDAO->all('combat');
+                        $combat_skills   = $skillDAO->all('combat');
                         $targeted_skills = $skillDAO->all('targeted');
-                    }
-
-                    // Pull the items and some necessary data about them.
-                    $inventory = new Inventory($viewing_player_obj);
-                    $items = $inventory->counts();
-
-                }	// End of the there-was-no-attack-error section
-
-                $set_bounty_section    = '';
-                $communication_section = '';
-                $player_clan_section   = '';
-
-                $clan = ClanFactory::clanOfMember($player_info['player_id']);
-
-                // Player clan and clan members
-
-                if ($clan) {
-                    $viewer_clan  = ($viewing_player_obj ? ClanFactory::clanOfMember($viewing_player_obj) : null);
-                    $clan_id      = $clan->getID();
-                    $clan_name    = $clan->getName();
-
-                    if ($viewer_clan) {
-                        $same_clan = ($clan->getID() == $viewer_clan->getID());
-                        $display_clan_options = $viewing_player_obj && !$self && $same_clan && $viewing_player_obj->isClanLeader();
-                    } else {
-                        $same_clan = $display_clan_options = false;
                     }
                 }
 
-                // Send the info to the template.
-
-                $template = 'player.tpl';
-                $combat_skills = ($combat_skills ? $combat_skills->fetchAll() : null);
-
-                $parts = [
-                    'char_info'            => $char_info,
-                    'viewing_player_obj'   => $viewing_player_obj,
-                    'target_player_obj'    => $target_player_obj,
-                    'combat_skills'        => $combat_skills,
-                    'targeted_skills'      => $targeted_skills,
-                    'player_info'          => $player_info,
-                    'self'                 => $self,
-                    'rank_spot'            => $rank_spot,
-                    'kills_today'          => $kills_today,
-                    'gravatar_url'         => $gravatar_url,
-                    'status_list'          => $status_list,
-                    'clan'                 => $clan,
-                    'items'                => $items,
-                    'account'              => $account,
-                    'same_clan'            => $same_clan,
-                    'display_clan_options' => $display_clan_options,
-                    'attack_error'         => $attack_error,
-                ];
+                if ($clan && $viewers_clan) {
+                    $same_clan            = ($clan->getID() == $viewers_clan->getID());
+                    $display_clan_options = (!$self && $same_clan && $viewing_player_obj->isClanLeader());
+                }
             }
+
+            $parts = [
+                'viewing_player_obj'   => $viewing_player_obj,
+                'target_player_obj'    => $target_player_obj,
+                'combat_skills'        => $combat_skills,
+                'targeted_skills'      => $targeted_skills,
+                'self'                 => $self,
+                'rank_spot'            => $rank_spot,
+                'kills_today'          => $kills_today,
+                'status_list'          => Player::getStatusList($target_player_obj->id()),
+                'clan'                 => $clan,
+                'items'                => $items,
+                'account'              => Account::findByChar($target_player_obj),
+                'same_clan'            => $same_clan,
+                'display_clan_options' => $display_clan_options,
+                'attack_error'         => $attack_error,
+            ];
         }
 
         $parts['authenticated'] = SessionFactory::getSession()->get('authenticated', false);
 
         return [
             'template' => $template,
-            'title'    => 'Ninja'.($viewed_name_for_title? ": $viewed_name_for_title" : ' Profile'),
+            'title'    => 'Ninja'.($viewed_name_for_title ? ": $viewed_name_for_title" : ' Profile'),
             'parts'    => $parts,
             'options'  => [
                 'quickstat' => 'player',
