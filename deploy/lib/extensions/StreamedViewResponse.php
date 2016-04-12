09e391567228a1b9c30f2dd45935e847cccdd3cd
Al Vazquez <al.vazquez@gmail.com>
2016-04-11 22:09:54 -0400

    Add view spec properties to StreamedViewReponse to support testing

--- deploy/lib/extensions/StreamedViewResponse.php
+++ deploy/lib/extensions/StreamedViewResponse.php
@@ -6,8 +6,18 @@ use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 class StreamedViewResponse extends StreamedResponse {
+    private $title;
+    private $template;
+    private $data;
+    private $options;
+
     public function __construct($title, $template, $data = [], $options = [], $headers = []) {
         parent::__construct();
+
+        $this->title    = $title;
+        $this->template = $template;
+        $this->data     = $data;
+        $this->options  = $options;
 
         $this->setCallback(function() use ($title, $template, $data, $options) {
             $view = new NWTemplate();
