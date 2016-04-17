5f7b8dacdf59328912a3f2394841451d8466fffd
Al Vazquez <al.vazquez@gmail.com>
2016-04-17 13:42:49 -0400

    Use StreamedViewResponse in RumorController

--- deploy/lib/control/RumorController.php
+++ deploy/lib/control/RumorController.php
@@ -3,21 +3,16 @@ namespace NinjaWars\core\control;
 
 use NinjaWars\core\control\AbstractController;
 use NinjaWars\core\data\DatabaseConnection;
+use NinjaWars\core\extensions\StreamedViewResponse;
 
 /**
  * Handles the rumors and info displayed by the bathhouse.
  */
 class RumorController extends AbstractController {
-    const ALIVE          = false;
-    const PRIV           = false;
+    const ALIVE = false;
+    const PRIV  = false;
 
-    /**
-     * 
-     */
-    public function __construct() {
-    }
-
-    private function duels(){
+    private function duels() {
         return query_array("SELECT dueling_log.*, attackers.player_id AS attacker_id, defenders.player_id AS defender_id FROM dueling_log JOIN players AS attackers ON attackers.uname = attacker JOIN players AS defenders ON defender = defenders.uname ORDER BY id DESC LIMIT 500");
     }
 
@@ -25,24 +20,15 @@ class RumorController extends AbstractController {
         return $this->membershipAndCombatStats();
     }
 
-    public function index(){
-        $stats          = $this->stats();
-        $vicious_killer = $stats['vicious_killer'];
-        $duels = $this->duels();
-
-
-
-
-        return [
-            'template'=>'duel.tpl',
-            'title'   =>'Bath House',
-            'parts'   =>[
-                    'stats'=>$stats,
-                    'vicious_killer'=>$vicious_killer,
-                    'duels'=>$duels,
-                ],
-            'options' =>['quickstat'=>false]
+    public function index() {
+        $stats           = $this->stats();
+        $parts           = [
+            'stats'          => $stats,
+            'vicious_killer' => $stats['vicious_killer'],
+            'duels'          => $this->duels(),
         ];
+
+        return new StreamedViewResponse('Bath House', 'duel.tpl', $parts, ['quickstat'=>false]);
     }
 
     /**
@@ -64,6 +50,7 @@ class RumorController extends AbstractController {
         $stats['players_online'] = $peopleOnline->fetchColumn();
 
         $stats['active_chars'] = query_item("SELECT count(*) FROM ppl_online WHERE member = true AND activity > (now() - CAST('15 minutes' AS interval))");
+
         return $stats;
     }
 }
--- deploy/tests/integration/controller/RumorController.php
+++ deploy/tests/integration/controller/RumorController.php
@@ -4,7 +4,10 @@ use NinjaWars\core\control\RumorController;
 class RumorControllerTest extends PHPUnit_Framework_TestCase {
     public function testIndex() {
         $controller = new RumorController();
-        $result = $controller->index();
-        $this->assertEquals('duel.tpl', $result['template']);
+        $response = $controller->index();
+        $reflection = new \ReflectionProperty(get_class($response), 'template');
+        $reflection->setAccessible(true);
+        $response_template = $reflection->getValue($response);
+        $this->assertEquals('duel.tpl', $response_template);
     }
 }
