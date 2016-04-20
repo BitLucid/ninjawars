649aecba825afdf2e749d9fd0db65710eb872171
Al Vazquez <al.vazquez@gmail.com>
2016-04-19 23:11:36 -0400

    Move update_activity_log to GameLog, inject request and session

--- deploy/www/front-controller.php
+++ deploy/www/front-controller.php
@@ -5,12 +5,17 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use NinjaWars\core\RouteNotFoundException;
 use NinjaWars\core\Router;
+use NinjaWars\core\environment\RequestWrapper;
+use NinjaWars\core\data\GameLog;
+use NinjaWars\core\extensions\SessionFactory;
 
 // setup our runtime environment
 require_once(LIB_ROOT.'environment/bootstrap.php');
 
 try {
-    update_activity_info(); // Updates the activity of the page viewer in the database.
+    // Update the activity of the page viewer in the database.
+    RequestWrapper::init();
+    GameLog::updateActivityInfo(RequestWrapper::$request, SessionFactory::getSession());
 
     // get the request information to parse the route
     $response = Router::route(Request::createFromGlobals());
