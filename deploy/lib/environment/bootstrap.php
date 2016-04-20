e78beb4bcd9d451d0941bf3b5d2cd2073376a009
Al Vazquez <al.vazquez@gmail.com>
2016-04-19 23:22:30 -0400

    Remove cleanup.inc, move contents to bootstrap.php

--- deploy/lib/environment/bootstrap.php
+++ deploy/lib/environment/bootstrap.php
@@ -19,7 +19,19 @@ if ($dbg === 'on') {
     $_COOKIE['debug'] == false;
 
 }
+
 if (defined('TRAP_ERRORS') && TRAP_ERRORS) {
     set_exception_handler(['NWError', 'exceptionHandler']);
     set_error_handler(['NWError', 'errorHandler'], E_USER_ERROR);
+}
+
+if (defined('PROFILE') && PROFILE) {
+    $mtime = microtime();
+    $mtime = explode(" ",$mtime);
+    $mtime = $mtime[1] + $mtime[0];
+    $totaltime = ($mtime - $__starttime);
+    $totalmemory = memory_get_peak_usage(true);
+    $unit=array('b','kb','mb','gb','tb','pb');
+    $totalmemory = @round($totalmemory/pow(1024,($i=floor(log($totalmemory,1024)))),2).' '.$unit[$i];
+    error_log('PROFILE - Script: '.$_SERVER["SCRIPT_NAME"]." - Time: $totaltime - Mem: $totalmemory");
 }
