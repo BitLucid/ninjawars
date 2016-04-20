2d6a327ffa281726936001f618c4468cb98eaf8e
Al Vazquez <al.vazquez@gmail.com>
2016-04-19 23:31:02 -0400

    Move database.php from data/ to environment/

--- deploy/lib/base.inc.php
+++ deploy/lib/base.inc.php
@@ -23,11 +23,10 @@ require_once(SERVER_ROOT.'routes.php');
 
 // Includes that actually actively modify settings.
 require_once(LIB_ROOT.'environment/status_defines.php'); // Status constant definitions.
-require_once(SERVER_ROOT.'npc-list.php'); // must not be before status defines
 require_once(LIB_ROOT.'environment/global_error_handling.php');
-
+require_once(LIB_ROOT.'environment/database.php'); // Eloquent database connection
 require_once(TEMPLATE_LIBRARY_PATH);
 require_once(LIB_ROOT.'control/lib_helpers.php');
-require_once(LIB_ROOT.'data/database.php'); // Eloquent database connection
 require_once(LIB_ROOT.'data/lib_db.php');
 require_once(LIB_ROOT.'extensions/Nmail.class.php');
+require_once(SERVER_ROOT.'npc-list.php'); // must not be before status defines
