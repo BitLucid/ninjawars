c7757086b139ca247142ce040e0b26612722d03c
Al Vazquez <al.vazquez@gmail.com>
2016-04-19 22:55:18 -0400

    Remove in() completely

--- deploy/lib/control/lib_helpers.php
+++ deploy/lib/control/lib_helpers.php
@@ -1,24 +1,4 @@
 <?php
-use NinjaWars\core\environment\RequestWrapper;
-use Symfony\Component\HttpFoundation\Request;
-use NinjaWars\core\Filter;
-
-/**
- * Input function that by default LEAVES INPUT COMPLETELY UNFILTERED
- * To not filter some input, you have to explicitly pass in null for the third parameter,
- * e.g. in('some_url_parameter', null, null)
- */
-function in($var_name, $default_val=null, $filter_callback=null) {
-	$result = RequestWrapper::getPostOrGet($var_name, $default_val);
-
-	// Check that the filter function sent in exists.
-	if ($filter_callback) {
-		$result = Filter::$filter_callback($result);
-	}
-
-    return $result;
-}
-
 function debug($val) {
     if (DEBUG) {
     	$vals = func_get_args();
