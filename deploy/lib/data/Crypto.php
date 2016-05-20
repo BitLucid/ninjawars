7ff87954dea1002f86777e08c6b221d8f9ad7b4f
Roy Ronalds <tchalvakspam@gmail.com>
2016-05-20 16:44:16 -0400

    Crypto: Incorporate random_compat for simple nonce
    generation.  WIP due to composer version issues.

--- deploy/lib/data/Crypto.php
+++ deploy/lib/data/Crypto.php
@@ -14,25 +14,7 @@ class Crypto{
      * @return String
      */
     public static function nonce() {
-        // Fast hashing the random str only to make it a usable/passable nonce
-        return hash('sha512', static::make_random_string());
-    }
-
-    /**
-     * Seed random string for nonce
-     *
-     * @param int $bits Desired size of output
-     * @return String
-     */
-    public static function make_random_string($bits = 256) {
-        $bytes = ceil($bits / 8);
-
-        $return = '';
-        for ($i = 0; $i < $bytes; $i++) {
-            $return .= chr(mt_rand(0, 255));
-
-        }
-        return $return;
+        return bin2hex(random_bytes(32));
     }
 }
 
