a4d73b8d483de74c156a7089c4f820a29b5a1b25
Roy R <tchalvakspam@gmail.com>
2016-06-01 17:48:55 -0400

    Crypto: Incorporate random_compat for simple nonce (#824)
    * Crypto: Incorporate random_compat for simple nonce
    
    generation.  WIP due to composer version issues.
    
    * CryptoTest: Remove make_random_string as redundant.

--- deploy/tests/unit/CryptoTest.php
+++ deploy/tests/unit/CryptoTest.php
@@ -3,14 +3,6 @@
 use NinjaWars\core\data\Crypto;
 
 class CryptoTest extends PHPUnit_Framework_TestCase {
-    public function testRandomStringDoesntEverMatch() {
-        $this->assertNotEmpty(Crypto::make_random_string());
-        $i = 30;
-
-        while ($i--) {
-            $this->assertNotEquals(Crypto::make_random_string(), Crypto::make_random_string());
-        }
-    }
 
     public function testNonceDoesntEverMatch() {
         $this->assertNotEmpty(Crypto::nonce());
