57cbbd81c125006ce49d7dbd7002e80b39d39210
Roy Ronalds <tchalvakspam@gmail.com>
2017-12-26 13:00:06 -0500

    Casino.js: Use strict.

--- deploy/www/js/casino.js
+++ deploy/www/js/casino.js
@@ -1,6 +1,12 @@
+/* Simple defaults for the casino page */
+/*jshint browser: true, white: true, plusplus: true*/
+/*jslint browser: true, white: true, plusplus: true*/
+/*global $, NW */
 $(function(){
+	'use strict'
+	//Get stored bet
     $("#bet").val(NW.storage.appState.get("bet", 1));
-
+    // Store latest bet
     $("#coin_flip").submit(function() {
         NW.storage.appState.set("bet", $("#bet").val());
         return true;
