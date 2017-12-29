7e9a179491a6bd38aac1a6059cb870f0f73e5b8f
Roy Ronalds <tchalvakspam@gmail.com>
2017-12-29 11:17:18 -0500

    Intro: Prevent relayout, hide faq area faster.

--- deploy/www/js/intro.js
+++ deploy/www/js/intro.js
@@ -4,12 +4,15 @@ $(function () {
     $('.not-user').hide();
   }
   var show_faqs = false; // Set faqs hidden by default.
+  document.getElementById('faqs').style.visibility = 'hidden'; // Hide fast to avoid layout flash
   var showfaqsLink = $('#show-faqs');
   var faqsArea = $('#faqs');
   if(!show_faqs){
     faqsArea.hide(); // To avoid flashing of content hide early on.
+    showfaqsLink.show();
   } else {
-    showfaqsLink.hide(); // Otherwise, hide the show-hide link.
+    showfaqsLink.hide();
+    faqsArea.show();
   }
   showfaqsLink.click(function(event){
     faqsArea.slideToggle('slow');
@@ -17,17 +20,19 @@ $(function () {
     return false;
   });
 
-  // Fade the link colors in gradually, one at a time.
+  // Fade the colors for the links in gradually to slowly introduce the concepts.
+  var cssLinkify = {color:'steelBlue'};
   $('#later-progression a')
       .each(function(secs, element){
       setTimeout(function (){
-          $(element).css({'color':'steelBlue'});
-      }, 1000*(secs+1)*5);
+          $(element).css(cssLinkify);
+      }, 1000*(secs+1)*1);
   });
-  // Finally, fade in the join link last.
+  // Finally, accentuate the join link after a while.
+  var cssAccent = {color:'steelBlue', 'font-size':'1.5em'};
   $('#join-link').each(function(index, element){
       setTimeout(function (){
-          $(element).css({'color':'steelBlue', 'font-size':'1.5em'});
+          $(element).css(cssAccent);
       }, 1000*26);
   });
 });
