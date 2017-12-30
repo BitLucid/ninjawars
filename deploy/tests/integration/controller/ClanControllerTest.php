e7c1f0c5c579bcb69782ebe7e145fddba154f746
Roy Ronalds <tchalvakspam@gmail.com>
2017-12-29 21:35:14 -0500

    ClanControllerTest: Fix inject of clan_id.

--- deploy/tests/integration/controller/ClanControllerTest.php
+++ deploy/tests/integration/controller/ClanControllerTest.php
@@ -185,7 +185,8 @@ class ClanControllerTest extends NWTest {
         $this->m_dependencies['session']->set('player_id', $char_id_2);
 
         // try to leave
-        $request = Request::create('/clan/leave', 'GET', []);
+        $clan_id = query('select clan_id from clan limit 1');
+        $request = Request::create('/clan/join', 'GET', ['clan_id'=>1]);
         RequestWrapper::inject($request);
         $response = $this->controller->join($this->m_dependencies);
 
@@ -201,6 +202,8 @@ class ClanControllerTest extends NWTest {
 
         // switch session to new character
 		$this->m_dependencies['session']->set('player_id', $char_id_2);
+
+
 
         // try to leave
         $request = Request::create('/clan/leave', 'GET', []);
