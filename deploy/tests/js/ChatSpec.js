739632e0db1fb87faa176167eed300200a3a17e8
Roy Ronalds <tchalvakspam@gmail.com>
2017-12-30 09:18:19 -0500

    Js tests: Add ChatSpec.js, DisagreementSpec.js, IntroSpec.js.

---
+++ deploy/tests/js/ChatSpec.js
@@ -0,0 +1,24 @@
+"use strict"; // Strict checking.
+
+describe('chat', function() {
+  describe('Introductory page testing context', function() {
+    beforeEach(function() {
+    });
+
+    afterEach(function() {
+    });
+
+    it('should be able to initialize without failing', function() {
+      // Running the file should work without error
+    });
+
+    it('should have initialized the Chat', function() {
+      // Running the file should work without error
+      expect(Chat).toBeDefined();
+      expect(getDomainName).toBeDefined();
+      expect(chatApiDomain).toBeDefined();
+      expect(config).toBeDefined();
+      expect(Chat).toBeDefined();
+    });
+  });
+});
