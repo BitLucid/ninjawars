617e8bf829a261fba29ecdcda0f1aa49b730550f
Roy Ronalds <tchalvakspam@gmail.com>
2018-07-21 15:41:03 -0400

    Update sanitation script.

--- deploy/sql/sanitize_to_fixtures.sql
+++ deploy/sql/sanitize_to_fixtures.sql
@@ -4,22 +4,23 @@ delete from players where player_id not in(select player_id from players where u
 update accounts set phash = crypt('test', gen_salt('bf', 10));
 update players set days = days/500, active = 1;  -- activate anyone who is left
 update players set goals = '', beliefs = '';
-update players set email = '' where email != 'tchalvakspam@gmail.com';
+update players set email = '' where email not like 'tchal%' or email not like '%spam%';
 update accounts set active_email = 'test'||account_id||'@example.com', account_identity = 'test'||account_id||'@example.com' where active_email not like 'tchal%';
 update accounts set last_ip = concat('33.', trunc(random()*55 + 1), '.', trunc(random()*250 + 1), '.33');
 update clan set clan_name = 'clan_fixture_test'||clan_id;
 update clan set description = 'fixtures_test';
 update clan set clan_founder = 'Tchalvak';
 update past_stats set stat_result = 'Tchalvak' where stat_result::text != '0';
-truncate login_attempts;
-truncate password_reset_requests;
-truncate chat;
-truncate player_rank;
-truncate dueling_log;
-truncate levelling_log;
-truncate messages;
-truncate enemies;
-truncate events;
-truncate inventory;
-truncate settings;
+truncate login_attempts RESTART IDENTITY;
+truncate password_reset_requests RESTART IDENTITY;
+truncate chat RESTART IDENTITY;
+truncate player_rank RESTART IDENTITY;
+truncate dueling_log RESTART IDENTITY;
+truncate levelling_log RESTART IDENTITY;
+truncate messages RESTART IDENTITY;
+truncate enemies RESTART IDENTITY;
+truncate events RESTART IDENTITY;
+truncate inventory RESTART IDENTITY;
+truncate settings RESTART IDENTITY;
+truncate events RESTART IDENTITY;
 -- May have to run tick atomic script after this
