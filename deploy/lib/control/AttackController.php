7a5e913e75f0796876749a6d30720eb9c66a8d68
Roy Ronalds <tchalvakspam@gmail.com>
2018-07-21 16:49:44 -0400

    Fix attackController setting player_id...
    on stealthed attackers to zero, on a variable that didn't exist anyway.

--- deploy/lib/control/AttackController.php
+++ deploy/lib/control/AttackController.php
@@ -12,6 +12,9 @@ use NinjaWars\core\data\Event;
 use NinjaWars\core\extensions\StreamedViewResponse;
 use NinjaWars\core\environment\RequestWrapper;
 
+/**
+ * Handles the displaying and processing of the attack page
+ */
 class AttackController extends AbstractController {
     const ALIVE                  = true;
     const PRIV                   = true;
@@ -24,7 +27,7 @@ class AttackController extends AbstractController {
     const EVEN_MATCH_ROUND_COUNT = 4;
 
     /**
-     * @return Response
+     * @return StreamedViewResponse
      */
     public function index(Container $p_dependencies) {
         $request = RequestWrapper::$request;
@@ -195,10 +198,10 @@ class AttackController extends AbstractController {
 
             $reporting_victor = $victor;
 
+            // @todo This mis-use of attacking needs to be refactored.
             if ($victor->hasStatus(STEALTH)) {
                 $reporting_victor = new Player();
                 $reporting_victor->uname     = 'a stealthed ninja';
-                $reporting_victor->player_id = 0;
             }
 
             $this->lose($loser, $reporting_victor, $loot);
