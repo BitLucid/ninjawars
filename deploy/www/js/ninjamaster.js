ce28e660f6de81db2958cc336ab09fd1211dec59
Roy Ronalds <tchalvakspam@gmail.com>
2021-08-15 20:28:30 -0400

    Ninjamaster: Fix js tests.

--- deploy/www/js/ninjamaster.js
+++ deploy/www/js/ninjamaster.js
@@ -1,51 +1,47 @@
 /* Functions for ninjamaster */
 /*jshint browser: true, white: true, plusplus: true*/
-/*global $, NW */
+/*global $ */
 $(function () {
     'use strict';
 
-    
-
-    $('#shop_form').submit(function () {
-        if (!auth) {
-            return false;
-        } else {
-            NW.storage.appState.set('quantity', $('#quantity').val());
-            return true;
-        }
-    });
 });
 
-function confirmDeactivation(e, charId, callback){
-    const message = 'Are you sure you want to deactivate this player character?'
-    result = window.confirm(message);
-    if(result === true){
+// Check in the ui for an additional confirm of deactivation
+function confirmDeactivation(e, charId, callback) {
+    var message = 'Are you sure you want to deactivate this player character?';
+    var result = window.confirm(message);
+    if (result === true) {
         e.preventDefault();
         e.stopPropagation();
-        deactivateChar(charId, callback)
-        return true
-    }else {
+        deactivateChar(charId, function(){
+            afterDeactivation(charId, callback);
+        });
+        return true;
+    } else {
         e.preventDefault();
         e.stopPropagation();
-        return false
+        return false;
     }
 }
 
 // Hit the api to deactivate the character
 function deactivateChar(charId, callback) {
-        // Deactivate a player character by id
-        return $.getJSON(
-            '/api?type=deactivate_char&data=' +
-                charId +
-                '&jsoncallback=?',
-            callback
-        );
+    // Deactivate a player character by id
+    return $.getJSON(
+        '/api?type=deactivate_char&data=' +
+        charId +
+        '&jsoncallback=?',
+        callback
+    );
 }
 
 // Reload the page afterwards
-function afterDeactivation(data){
-    return ()=> {
+function afterDeactivation(data, callback) {
+    return function() {
         console.log(data);
         location.reload();
+        return callback && callback();
     }
 }
+
+/* exported deactivateChar afterDeactivation confirmDeactivation */
