5b974e0c70af9fce23af15582dd55c8e3fb93dcc
Roy Ronalds <tchalvakspam@gmail.com>
2021-11-05 12:55:08 -0400

    Helpers: Minor change to function arg override.

--- deploy/lib/control/lib_helpers.php
+++ deploy/lib/control/lib_helpers.php
@@ -3,9 +3,9 @@
 function debug($val) {
     if (DEBUG) {
     	$vals = func_get_args();
-    	foreach($vals as $val){
+		foreach ($vals as $fval) {
 		    echo "<pre class='debug' style='font-size:12pt;background-color:white;color:black;position:relative;z-index:10'>";
-		    var_dump($val);
+			var_dump($fval);
 		    echo "</pre>";
         }
     }
