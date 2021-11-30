2b1eb00a4f92fa1ea772ec9943814650357c71a0
Roy Ronalds <roy.ronalds@xa.epicgames.com>
2021-11-30 00:34:45 -0500

    Nw.js better format comment spacing.

--- deploy/www/js/nw.js
+++ deploy/www/js/nw.js
@@ -1,7 +1,7 @@
 /* The main javascript functionality of the site, apart from very page specific behaviors */
-/*jshint browser: true, white: true, plusplus: true*/
-/*jslint browser: true, white: true, plusplus: true*/
-/*global $, jQuery, window, parent, console, Storage */
+/* jshint browser: true, white: true, plusplus: true */
+/* jslint browser: true, white: true, plusplus: true */
+/* global $, jQuery */
 
 // Sections are, in order: SETTINGS | FUNCTIONS | READY
 
@@ -38,12 +38,12 @@ if (
 ) {
   console.log('Reusing existing parent NW object in new page');
   // If the interior page of an iframe, use the already-defined globals from the index.
-  //$ = parent.$;
+  // $ = parent.$;
   NW = parent.NW;
 } else {
   console.log('Creating new NW object');
   // If the page is standalone, define the objects as needed.
-  //$ = jQuery;
+  // $ = jQuery;
   NW = {};
 
   NW.datastore = {};
@@ -67,9 +67,9 @@ $(selector).keyup(function () {
   })();
 
   // Accept a json data array of matches and house them in the interface.
-  /*NW.displayMatches = function(json_matches){
+  /* NW.displayMatches = function(json_matches){
 
-};*/
+}; */
 
   // Get the chars/ids matching a term and then have the callback run.
   NW.charMatch = function (term, limit, callback) {
@@ -85,7 +85,7 @@ $(selector).keyup(function () {
 
   // Update the barstats visuals with incoming data.
   NW.updateBarstats = function (barstats) {
-    //alert('update of barstats visuals reached! Barstats are:'+barstats.health+' '+barstats.max_health+' '+barstats.kills+' '+barstats.next_level+' '+barstats.turns+' '+barstats.kills_percent);
+    // alert('update of barstats visuals reached! Barstats are:'+barstats.health+' '+barstats.max_health+' '+barstats.kills+' '+barstats.next_level+' '+barstats.turns+' '+barstats.kills_percent);
     // Find the barstats container.
     var barstatsContainer = $('#barstats');
     // Find the bars inside that.
@@ -298,10 +298,10 @@ $(selector).keyup(function () {
   NW.updateIndex = function () {
     var messageUpdated = this.updateMessageCount();
     var eventUpdated = this.updateLatestEvent();
-    //var healthUpdated       = this.getAndUpdateHealth(); // health bar.
+    // var healthUpdated       = this.getAndUpdateHealth(); // health bar.
 
     // If any changes to data occurred, return true.
-    var res = !!((messageUpdated || eventUpdated) /*|| healthUpdated*/);
+    var res = !!((messageUpdated || eventUpdated) /* || healthUpdated */);
 
     return res;
   };
@@ -455,7 +455,7 @@ Take in a new datum from the api, compare it's <property name> to the already-in
   };
 
   // Determines the update interval,
-  //increases when feedback == false, rebaselines when feedback == true
+  // increases when feedback == false, rebaselines when feedback == true
   NW.getUpdateInterval = function (feedback) {
     var maxInt = 180;
     var min = 20; // Changes push the interval to this minimum.
@@ -525,7 +525,7 @@ Take in a new datum from the api, compare it's <property name> to the already-in
   NW.storage.appState.get = NW.storageGetter;
 }
 
-/***************************** READY SECTION *******************************/
+/** *************************** READY SECTION ****************************** */
 
 if (g_isIndex || g_isRoot) {
   // This has to be outside of domready for some reason.
@@ -588,4 +588,4 @@ $(function () {
 // html5shiv @rem remysharp.com/html5-enabling-script
 // Dual licensed under the MIT or GPL Version 2 licenses
 // What follows is an ie-only hack /*@ ... @*/
-/*@cc_on(function(a,b){function r(a){var b=-1;while(++b<f)a.createElement(e[b])}if(!window.attachEvent||!b.createStyleSheet||!function(){var a=document.createElement("div");return a.innerHTML="<elem></elem>",a.childNodes.length!==1}())return;a.iepp=a.iepp||{};var c=a.iepp,d=c.html5elements||"abbr|article|aside|audio|canvas|datalist|details|figcaption|figure|footer|header|hgroup|mark|meter|nav|output|progress|section|subline|summary|time|video",e=d.split("|"),f=e.length,g=new RegExp("(^|\\s)("+d+")","gi"),h=new RegExp("<(/*)("+d+")","gi"),i=/^\s*[\{\}]\s*$/,j=new RegExp("(^|[^\\n]*?\\s)("+d+")([^\\n]*)({[\\n\\w\\W]*?})","gi"),k=b.createDocumentFragment(),l=b.documentElement,m=b.getElementsByTagName("script")[0].parentNode,n=b.createElement("body"),o=b.createElement("style"),p=/print|all/,q;c.getCSS=function(a,b){try{if(a+""===undefined)return""}catch(d){return""}var e=-1,f=a.length,g,h=[];while(++e<f){g=a[e];if(g.disabled)continue;b=g.media||b,p.test(b)&&h.push(c.getCSS(g.imports,b),g.cssText),b="all"}return h.join("")},c.parseCSS=function(a){var b=[],c;while((c=j.exec(a))!=null)b.push(((i.exec(c[1])?"\n":c[1])+c[2]+c[3]).replace(g,"$1.iepp-$2")+c[4]);return b.join("\n")},c.writeHTML=function(){var a=-1;q=q||b.body;while(++a<f){var c=b.getElementsByTagName(e[a]),d=c.length,g=-1;while(++g<d)c[g].className.indexOf("iepp-")<0&&(c[g].className+=" iepp-"+e[a])}k.appendChild(q),l.appendChild(n),n.className=q.className,n.id=q.id,n.innerHTML=q.innerHTML.replace(h,"<$1font")},c._beforePrint=function(){if(c.disablePP)return;o.styleSheet.cssText=c.parseCSS(c.getCSS(b.styleSheets,"all")),c.writeHTML()},c.restoreHTML=function(){if(c.disablePP)return;n.swapNode(q)},c._afterPrint=function(){c.restoreHTML(),o.styleSheet.cssText=""},r(b),r(k);if(c.disablePP)return;m.insertBefore(o,m.firstChild),o.media="print",o.className="iepp-printshim",a.attachEvent("onbeforeprint",c._beforePrint),a.attachEvent("onafterprint",c._afterPrint)})(this,document);@*/
+/* @cc_on(function(a,b){function r(a){var b=-1;while(++b<f)a.createElement(e[b])}if(!window.attachEvent||!b.createStyleSheet||!function(){var a=document.createElement("div");return a.innerHTML="<elem></elem>",a.childNodes.length!==1}())return;a.iepp=a.iepp||{};var c=a.iepp,d=c.html5elements||"abbr|article|aside|audio|canvas|datalist|details|figcaption|figure|footer|header|hgroup|mark|meter|nav|output|progress|section|subline|summary|time|video",e=d.split("|"),f=e.length,g=new RegExp("(^|\\s)("+d+")","gi"),h=new RegExp("<(/*)("+d+")","gi"),i=/^\s*[\{\}]\s*$/,j=new RegExp("(^|[^\\n]*?\\s)("+d+")([^\\n]*)({[\\n\\w\\W]*?})","gi"),k=b.createDocumentFragment(),l=b.documentElement,m=b.getElementsByTagName("script")[0].parentNode,n=b.createElement("body"),o=b.createElement("style"),p=/print|all/,q;c.getCSS=function(a,b){try{if(a+""===undefined)return""}catch(d){return""}var e=-1,f=a.length,g,h=[];while(++e<f){g=a[e];if(g.disabled)continue;b=g.media||b,p.test(b)&&h.push(c.getCSS(g.imports,b),g.cssText),b="all"}return h.join("")},c.parseCSS=function(a){var b=[],c;while((c=j.exec(a))!=null)b.push(((i.exec(c[1])?"\n":c[1])+c[2]+c[3]).replace(g,"$1.iepp-$2")+c[4]);return b.join("\n")},c.writeHTML=function(){var a=-1;q=q||b.body;while(++a<f){var c=b.getElementsByTagName(e[a]),d=c.length,g=-1;while(++g<d)c[g].className.indexOf("iepp-")<0&&(c[g].className+=" iepp-"+e[a])}k.appendChild(q),l.appendChild(n),n.className=q.className,n.id=q.id,n.innerHTML=q.innerHTML.replace(h,"<$1font")},c._beforePrint=function(){if(c.disablePP)return;o.styleSheet.cssText=c.parseCSS(c.getCSS(b.styleSheets,"all")),c.writeHTML()},c.restoreHTML=function(){if(c.disablePP)return;n.swapNode(q)},c._afterPrint=function(){c.restoreHTML(),o.styleSheet.cssText=""},r(b),r(k);if(c.disablePP)return;m.insertBefore(o,m.firstChild),o.media="print",o.className="iepp-printshim",a.attachEvent("onbeforeprint",c._beforePrint),a.attachEvent("onafterprint",c._afterPrint)})(this,document);@ */
