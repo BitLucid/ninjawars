8a8bf5a54476a6ad24a1f82d41d7b3c3c8e47835
Roy Ronalds <tchalvakspam@gmail.com>
2022-12-04 17:40:48 -0500

    Generally working disagreement and fight config.

--- deploy/www/js/disagreement.js
+++ deploy/www/js/disagreement.js
@@ -1,7 +1,7 @@
 /* Simple defaults for the player and fight pages, attacking_possible (boolean)
  * and combatSkillsList (json array) are rendered by the server and passed in */
 /* jshint browser: true, white: true, plusplus: true */
-/* global NW, attacking_possible, combatSkillsList
+/* global NW, attacking_possible
  */
 
 /* eslint max-statements: "Warn" */
@@ -10,11 +10,25 @@
 
 // @ts-check
 
-const config = {};
+const initialCombatConfig = {
+  blaze: false,
+  deflect: false,
+  evasion: false,
+  duel: false,
+};
 
-const getD = (key, defaultV) => NW.storage.appState.get(key, defaultV);
+// Helper functions
+const getD = (key) => window && window.localStorage && window.localStorage.getItem(key);
+const setD = (key, value) => window
+  && window.localStorage && window.localStorage.setItem(key, value);
+const saveD = (key, val) => setD(key, JSON.stringify(val));
+const obtainD = (key) => {
+  const intermediary = getD(key);
+  return intermediary ? JSON.parse(intermediary) : undefined;
+};
 
-const setD = (key, value) => NW.storage.appState.set(key, value);
+const isChecked = (key) => Boolean($(`#${key}`).prop('checked'));
+const setChecked = (key, bol) => $(`#${key}`).prop('checked', bol);
 
 const kickClanMember = () => window
   // eslint-disable-next-line no-alert
@@ -24,8 +38,9 @@ const kickClanMember = () => window
 $(() => {
   console.info('Checking for saved combat configurations.');
   //  Pull var as defined in external template
+  // @ts-ignore
   // eslint-disable-next-line camelcase
-  const attackable = typeof attacking_possible !== 'undefined' ? attacking_possible : false;
+  const attackable = attacking_possible || false;
   console.info(
     attackable ? 'Attacking enabled.' : 'No attacking this target',
   );
@@ -35,52 +50,37 @@ $(() => {
     kickClanMember,
   );
 
-  /*
+  if (attackable) {
+    /*
        because some browsers store all values as strings, we need to store
        booleans as string representations of 1 and 0. We then need to get
        the int value upon retrieval
+       Perhaps no longer necessary? -- RR Dec 4 2022
     */
 
-  // @ts-ignore
-  const localCombatSkills = combatSkillsList || undefined;
+    const combatKey = 'combat-config';
 
-  if (attackable) {
-    console.info('combat skills', localCombatSkills);
-    if (!Array.isArray(localCombatSkills)) {
-      console.warn(
-        'Combat_skills settings were not in proper array format',
-      );
-    }
+    const storedCombatConfig = obtainD(combatKey);
+    const finalConfig = {
+      ...initialCombatConfig,
+      ...storedCombatConfig,
+    };
+    setChecked('blaze', finalConfig.blaze);
+    setChecked('deflect', finalConfig.deflect);
+    setChecked('evasion', finalConfig.evasion);
+    setChecked('duel', finalConfig.duel);
+
     // Duel is a special case, non-skill combat choice
-    $('#duel').prop(
-      'checked',
-      Boolean(getD('duel_checked', false)),
-    );
-    $.each(
-      localCombatSkills,
-      (_i, skill) => {
-        $(`#${skill.skill_internal_name}`).prop('checked', Boolean(getD(
-          `${skill.skill_internal_name}_checked`,
-          false,
-        )));
-      },
-    );
 
     $('#attack_player').on('submit', () => {
-      // the unary + operator converts the boolean to an int
-      setD(
-        'duel_checked',
-        Boolean($('#duel').prop('checked')),
-      ); // Duel is special case
-      $.each(
-        localCombatSkills,
-        (i, skill) => {
-          setD(
-            `${skill.skill_internal_name}_checked`,
-            Boolean($(`#${skill.skill_internal_name}`).prop('checked')),
-          );
-        },
-      );
+      const submitFinalConfig = {
+        ...initialCombatConfig,
+        blaze: isChecked('blaze'),
+        deflect: isChecked('deflect'),
+        evasion: isChecked('evasion'),
+        duel: isChecked('duel'),
+      };
+      saveD(combatKey, submitFinalConfig);
 
       return true;
     });
@@ -91,7 +91,7 @@ $(() => {
   if ($(`#item option[value='${lastItemUsed}']`).length) {
     $('#item').val(lastItemUsed);
   } else {
-    $('#item').val($('#item option:first-child').val());
+    $('#item').val($('#item option:first-child').val() || '');
   }
 
   $('#inventory_form').on('submit', () => {
