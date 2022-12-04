2ec78f025b251c5333ed79b5d9c54bf5e184762c
Roy Ronalds <tchalvakspam@gmail.com>
2022-12-04 15:47:38 -0500

    Updates to disagreement setting saving.

--- deploy/www/js/disagreement.js
+++ deploy/www/js/disagreement.js
@@ -1,23 +1,38 @@
-/* Simple defaults for the casino page, attacking_possible (boolean)
+/* Simple defaults for the player and fight pages, attacking_possible (boolean)
  * and combatSkillsList (json array) are rendered by the server and passed in */
 /* jshint browser: true, white: true, plusplus: true */
-/* global NW, attacking_possible, combatSkillsList */
+/* global NW, attacking_possible, combatSkillsList
+ */
 
 /* eslint max-statements: "Warn" */
 /* eslint max-lines: "Warn" */
 /* eslint max-lines-per-function: "Warn" */
 
+// @ts-check
+
+const config = {};
+
+const getD = (key, defaultV) => NW.storage.appState.get(key, defaultV);
+
+const setD = (key, value) => NW.storage.appState.set(key, value);
+
+const kickClanMember = () => window
+  // eslint-disable-next-line no-alert
+  && window.confirm('Are you sure you want to kick this player?');
+
+// eslint-disable-next-line max-statements, max-lines-per-function
 $(() => {
+  console.info('Checking for saved combat configurations.');
   //  Pull var as defined in external template
   // eslint-disable-next-line camelcase
   const attackable = typeof attacking_possible !== 'undefined' ? attacking_possible : false;
   console.info(
     attackable ? 'Attacking enabled.' : 'No attacking this target',
   );
-  // eslint-disable-next-line no-alert
-  $('#kick_form').submit(
-    () => window
-            && window.confirm('Are you sure you want to kick this player?'),
+  // The player field has a kick button option for clan leaders
+  $('#kick_form').on(
+    'submit',
+    kickClanMember,
   );
 
   /*
@@ -25,44 +40,47 @@ $(() => {
        booleans as string representations of 1 and 0. We then need to get
        the int value upon retrieval
     */
+
+  // @ts-ignore
+  const localCombatSkills = combatSkillsList || undefined;
+
   if (attackable) {
-    if (
-      undefined === combatSkillsList
-            || !Array.isArray(combatSkillsList)
-    ) {
+    console.info('combat skills', localCombatSkills);
+    if (!Array.isArray(localCombatSkills)) {
       console.warn(
         'Combat_skills settings were not in proper array format',
       );
     }
-    console.info('combat skills', combatSkillsList);
     // Duel is a special case, non-skill combat choice
     $('#duel').prop(
       'checked',
-      parseInt(NW.storage.appState.get('duel_checked', false), 10),
+      Boolean(getD('duel_checked', false)),
     );
-    $.each(combatSkillsList, (i, skill) => {
-      const checkedOrNot = parseInt(
-        NW.storage.appState.get(
+    $.each(
+      localCombatSkills,
+      (_i, skill) => {
+        $(`#${skill.skill_internal_name}`).prop('checked', Boolean(getD(
           `${skill.skill_internal_name}_checked`,
           false,
-        ),
-        10,
-      );
-      $(`#${skill.skill_internal_name}`).prop('checked', checkedOrNot);
-    });
+        )));
+      },
+    );
 
-    $('#attack_player').submit(() => {
+    $('#attack_player').on('submit', () => {
       // the unary + operator converts the boolean to an int
-      NW.storage.appState.set(
+      setD(
         'duel_checked',
-        +$('#duel').prop('checked'),
+        Boolean($('#duel').prop('checked')),
       ); // Duel is special case
-      $.each(combatSkillsList, (i, skill) => {
-        NW.storage.appState.set(
-          `${skill.skill_internal_name}_checked`,
-          +$(`#${skill.skill_internal_name}`).prop('checked'),
-        );
-      });
+      $.each(
+        localCombatSkills,
+        (i, skill) => {
+          setD(
+            `${skill.skill_internal_name}_checked`,
+            Boolean($(`#${skill.skill_internal_name}`).prop('checked')),
+          );
+        },
+      );
 
       return true;
     });
@@ -76,7 +94,7 @@ $(() => {
     $('#item').val($('#item option:first-child').val());
   }
 
-  $('#inventory_form').submit(() => {
-    NW.storage.appState.set('last_item_used', $('#item').val());
+  $('#inventory_form').on('submit', () => {
+    setD('last_item_used', $('#item').val());
   });
 });
