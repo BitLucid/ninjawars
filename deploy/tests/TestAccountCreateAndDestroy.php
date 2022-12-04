7558d147a737e06ab098128c07702416eeda0438
Roy Ronalds <tchalvakspam@gmail.com>
2022-12-04 12:47:40 -0500

    Linting of testAccountCreateAndDestroy.

--- deploy/tests/TestAccountCreateAndDestroy.php
+++ deploy/tests/TestAccountCreateAndDestroy.php
@@ -6,7 +6,8 @@ use NinjaWars\core\data\Account;
 /**
  * Library for creating and destroying test-only accounts, for use in their various ways in testing.
  */
-class TestAccountCreateAndDestroy {
+class TestAccountCreateAndDestroy
+{
     public static $test_email = 'testphpunit@example.com';
     public static $alt_test_email = 'testphpunit2@example.com';
     public static $test_password = 'password';
@@ -30,19 +31,23 @@ class TestAccountCreateAndDestroy {
      * or account_identity='testphpunit@example.com';
      * @param string $test_ninja_nm Newly created ninja name to delete from
      */
-    public static function purge_test_accounts($test_ninja_nm = null) {
+    public static function purge_test_accounts($test_ninja_nm = null)
+    {
         $test_ninja_name = $test_ninja_nm ? $test_ninja_nm : self::$test_ninja_name;
         $active_email = self::$test_email;
         $alt_active_email = self::$alt_test_email;
 
+
+        // TODO: Throw exceptions on invalid arguments
+
         query(
-            'delete from players where player_id in '.
-            '(select player_id from players '.
-            'left join account_players on _player_id = player_id '.
-            'left join accounts on _account_id = account_id '.
-            'where active_email = :active_email or account_identity= :ae2 '.
+            'delete from players where player_id in ' .
+                '(select player_id from players ' .
+                'left join account_players on _player_id = player_id ' .
+                'left join accounts on _account_id = account_id ' .
+                'where active_email = :active_email or account_identity= :ae2 ' .
                 'or players.uname = :uname or active_email = :alt_active_email ' .
-            'or account_identity = :alt_active_email2)',
+                'or account_identity = :alt_active_email2)',
             [
                 ':active_email'      => $active_email,
                 ':ae2'               => $active_email,
@@ -53,11 +58,11 @@ class TestAccountCreateAndDestroy {
         ); // Delete the players
 
         query(
-            'delete from account_players where _account_id in '.
-            '(select account_id from accounts '.
-            'where active_email = :active_email or account_identity= :ae2 '.
-            'or active_email = :alt_active_email '.
-            'or account_identity = :alt_active_email2)',
+            'delete from account_players where _account_id in ' .
+                '(select account_id from accounts ' .
+                'where active_email = :active_email or account_identity= :ae2 ' .
+                'or active_email = :alt_active_email ' .
+                'or account_identity = :alt_active_email2)',
             [
                 ':active_email'      => $active_email,
                 ':ae2'               => $active_email,
@@ -67,10 +72,10 @@ class TestAccountCreateAndDestroy {
         ); // Delete the account_players linkage.
 
         $query = query(
-            'delete from accounts '.
-            'where active_email = :active_email or account_identity= :ae2 '.
-            'or active_email = :alt_active_email '.
-            'or account_identity = :alt_active_email2',
+            'delete from accounts ' .
+                'where active_email = :active_email or account_identity= :ae2 ' .
+                'or active_email = :alt_active_email ' .
+                'or account_identity = :alt_active_email2',
             [
                 ':active_email'      => $active_email,
                 ':ae2'               => $active_email,
@@ -85,14 +90,16 @@ class TestAccountCreateAndDestroy {
     /**
      * More memorable wrapper to the purge_test_accounts functionality.
      */
-    public static function destroy($test = null) {
+    public static function destroy($test = null)
+    {
         static::purge_test_accounts($test);
     }
 
     /**
      * Create a testing account
      */
-    public static function create_testing_account($confirm = false, $overrides = null) {
+    public static function create_testing_account($confirm = false, $overrides = null)
+    {
         self::purge_test_accounts();
         return self::createAccount($overrides['name'] ?? self::$test_ninja_name, self::$test_email, 'tiger');
     }
@@ -100,11 +107,13 @@ class TestAccountCreateAndDestroy {
     /**
      * Create a separate, second testing account
      */
-    public static function create_alternate_testing_account($confirm = false) {
+    public static function create_alternate_testing_account($confirm = false)
+    {
         return self::createAccount(self::$alt_test_ninja_name, self::$alt_test_email, 'dragon');
     }
 
-    public static function createAccount($ninja_name, $email, $class_identity) {
+    public static function createAccount($ninja_name, $email, $class_identity)
+    {
         $found = Player::findByName($ninja_name);
 
         if ($found) {
@@ -118,7 +127,7 @@ class TestAccountCreateAndDestroy {
 
         $class_id = query_item(
             'SELECT class_id FROM class WHERE identity = :class_identity',
-            [ ':class_identity' => $class_identity ]
+            [':class_identity' => $class_identity]
         );
 
         $ninja = new Player();
@@ -146,7 +155,8 @@ class TestAccountCreateAndDestroy {
     /**
      * Convenience wrapper for the above, but confirms the account and returns the account id.
      */
-    public static function create_complete_test_account_and_return_id() {
+    public static function create_complete_test_account_and_return_id()
+    {
         $player_mock = new Player();
         $player_mock->player_id = self::create_testing_account(true);
         $account = Account::findByChar($player_mock);
@@ -156,32 +166,37 @@ class TestAccountCreateAndDestroy {
     /**
      * Just return a character wholesale
      */
-    public static function char() {
+    public static function char()
+    {
         return Player::find(self::char_id());
     }
 
     /**
      * Return alternate character
      */
-    public static function char_2() {
+    public static function char_2()
+    {
         return Player::find(self::char_id_2());
     }
 
     /**
      * Alias for create_testing_account but clearer.
      */
-    public static function char_id($confirm = false) {
+    public static function char_id($confirm = false)
+    {
         return self::create_testing_account($confirm);
     }
 
-    public static function char_id_2($confirm = false) {
+    public static function char_id_2($confirm = false)
+    {
         return self::create_alternate_testing_account($confirm);
     }
 
     /**
      * Alias to get an account id, to prevent clash, only use in isolation from char_id
      */
-    public static function account_id() {
+    public static function account_id()
+    {
         return self::create_complete_test_account_and_return_id();
     }
 }
