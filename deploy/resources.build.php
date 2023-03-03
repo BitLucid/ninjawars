31726e2f2a2de34ab404618816f286fd203dbe95
Roy Ronalds <tchalvakspam@gmail.com>
2023-03-02 22:30:24 -0500

    Add game log to build script, again.

--- deploy/resources.build.php
+++ deploy/resources.build.php
@@ -13,12 +13,12 @@ define('DATABASE_PASSWORD', "unused_in_build");        // *** The password for t
 define('DATABASE_NAME', "nw");        // *** The name of the database to connect to, nw on dev
 define('OFFLINE', true);                // *** Controls if remote or local resources are used
 define('HASH_ASSET_PATHS', false);            // *** Controls if cachebusting hash strings for assets like images are used
-define('DEBUG', true);					// *** Shorter debugging constant name, set as false on live.
-define('SERVER_ROOT', realpath(__DIR__).'/');		// *** The root deployment directory of the game
+define('DEBUG', true);                    // *** Shorter debugging constant name, set as false on live.
+define('SERVER_ROOT', realpath(__DIR__) . '/');        // *** The root deployment directory of the game
 // Generally for the install purposes the SERVER_ROOT should correspond to /srv/ninjawars/deploy/
-define('WEB_ROOT', "http://localhost:8765/");			// *** The base URL used to access the game, http://www.ninjawars.net on live
-define('ADMIN_EMAIL', "ninjawarsTchalvak@gmail.com");		// *** For logs/emailed errors.
-define('SUPPORT_EMAIL', "ninjawarsTchalvak@gmail.com");	// *** For public questions.
+define('WEB_ROOT', "http://localhost:8765/");            // *** The base URL used to access the game, http://www.ninjawars.net on live
+define('ADMIN_EMAIL', "ninjawarsTchalvak@gmail.com");        // *** For logs/emailed errors.
+define('SUPPORT_EMAIL', "ninjawarsTchalvak@gmail.com");    // *** For public questions.
 define('SUPPORT_EMAIL_NAME', "Ninjawars Tchalvak");
 define('SYSTEM_EMAIL', "noreply@ninjawars.net");
 define('SYSTEM_MESSENGER_EMAIL', "noreply@ninjawars.net");
@@ -31,17 +31,19 @@ define('FACEBOOK_APP_SECRET', 'mooMooIAmACow'); // Secret! string for facebook l
 
 define('TRAP_ERRORS', false); // Whether to use the global error handler & oops page, true on live.
 
-define('TEMPLATE_LIBRARY_PATH', SERVER_ROOT.'vendor/smarty/smarty/libs/Smarty.class.php'); // Path to Smarty 3
+define('TEMPLATE_LIBRARY_PATH', SERVER_ROOT . 'vendor/smarty/smarty/libs/Smarty.class.php'); // Path to Smarty 3
 
 // For location-specific, can-be-dynamic-or-not constants.
-define('COMPILED_TEMPLATE_PATH', SERVER_ROOT.'templates/compiled/'); // *** This folder must have write permissions.
-define('TEMPLATE_CACHING_PATH', SERVER_ROOT.'templates/cache/'); // *** This folder must have write permissions.
-define('LOGS', SERVER_ROOT.'resources/logs/'); // *** For all custom logging
-define('CONNECTION_STRING', 'pgsql:'.(DATABASE_USE_HOST ? 'host='.DATABASE_HOST : '').';dbname='.DATABASE_NAME.';user='.DATABASE_USER.
-    (DATABASE_USE_PORT ? ';port='.DATABASE_PORT : '').(DATABASE_USE_PASSWORD ? ';password='.DATABASE_PASSWORD : ''));
+define('COMPILED_TEMPLATE_PATH', SERVER_ROOT . 'templates/compiled/'); // *** This folder must have write permissions.
+define('TEMPLATE_CACHING_PATH', SERVER_ROOT . 'templates/cache/'); // *** This folder must have write permissions.
+define('LOGS', SERVER_ROOT . 'resources/logs/'); // *** For all custom logging
+define('GAME_LOGS', '/tmp/game_logs/'); // For game logs, like combat logs.
+define('CONNECTION_STRING', 'pgsql:' . (DATABASE_USE_HOST ? 'host=' . DATABASE_HOST : '') . ';dbname=' . DATABASE_NAME . ';user=' . DATABASE_USER .
+    (DATABASE_USE_PORT ? ';port=' . DATABASE_PORT : '') . (DATABASE_USE_PASSWORD ? ';password=' . DATABASE_PASSWORD : ''));
 
 // Can't use constants as arrays until php 7, so static class info instead.
-class Constants {
+class Constants
+{
     public static $trusted_proxies = ['104.130.111.36', '10.189.245.10'];
 }
 
@@ -58,4 +60,4 @@ if (true) {
 }
 
 // Seperate, tracked file for derived constants, that changes as they change.
-require(SERVER_ROOT."derived_constants.php");
+require(SERVER_ROOT . "derived_constants.php");
