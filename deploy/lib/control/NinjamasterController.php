087a6a617ac27850371d48431e06bc51054413f4
Roy Ronalds <tchalvakspam@gmail.com>
2023-09-30 13:45:14 -0400

    Don't trim in nm unless view of char requested.

--- deploy/lib/control/NinjamasterController.php
+++ deploy/lib/control/NinjamasterController.php
@@ -67,7 +67,7 @@ class NinjamasterController extends AbstractController
 
         $initial_char_ids = $request->get('view');
         $char_ids  = $initial_char_ids ? preg_split("/[,\s]+/", $initial_char_ids) : null;
-        $char_name = trim($request->get('char_name'));
+        $char_name = $request->get('char_name') ? trim($request->get('char_name')) : null;
 
         if ($char_name) { // View a target non-self character
             $first_char = Player::findByName($char_name);
