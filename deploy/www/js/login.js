7ddf5dad69c86e239d2f092b8f79d2bf6ca3e9d5
Roy Ronalds <tchalvakspam@gmail.com>
2024-01-18 19:08:54 -0500

    Correctly framebreak on login page.

---
+++ deploy/www/js/login.js
@@ -0,0 +1,27 @@
+/**
+ * General behaviors for the login page.
+ */
+// @ts-check
+
+const { location: lLocation, top: lTop } = window;
+const { location: lFrameLocation } = lTop || {};
+
+/**
+ * Executions on the signup page.
+ */
+(() => {
+  // eslint-disable-next-line no-unused-vars
+  const { log, debug } = console || { log: () => { /** noop */ }, debug: () => { /** noop */ } };
+  debug('iife run on login.js');
+  // // eslint-disable-next-line eqeqeq
+  if (lLocation != lFrameLocation) { // Framebreak
+    if (window.top && window.top.location && window.top.location.href) {
+      window.top.location.href = document.location.href;
+    }
+  }
+  $( // on document ready
+    () => {
+
+    },
+  );
+})();
