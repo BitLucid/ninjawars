83a53c651e1f89879b75b0e24f69e61ce1f7542c
Roy Ronalds <tchalvakspam@gmail.com>
2024-01-28 13:49:20 -0500

    Cleanup empty block of login.js
    since it is currently not in use.

--- deploy/www/js/login.js
+++ deploy/www/js/login.js
@@ -13,15 +13,10 @@ const { location: lFrameLocation } = lTop || {};
   // eslint-disable-next-line no-unused-vars
   const { log, debug } = console || { log: () => { /** noop */ }, debug: () => { /** noop */ } };
   debug('iife run on login.js');
-  // // eslint-disable-next-line eqeqeq
+  // eslint-disable-next-line eqeqeq
   if (lLocation != lFrameLocation) { // Framebreak
     if (window.top && window.top.location && window.top.location.href) {
       window.top.location.href = document.location.href;
     }
   }
-  $( // on document ready
-    () => {
-
-    },
-  );
 })();
